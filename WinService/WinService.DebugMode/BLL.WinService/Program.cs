442ab876d93b32f6e88712c8a90b35b026717735
余小章 <yao-chang.yu@outlook.com>
2017-06-25 13:07:22 +0800

    加入RunInteractive

--- WinService/WinService.DebugMode/BLL.WinService/Program.cs
+++ WinService/WinService.DebugMode/BLL.WinService/Program.cs
@@ -1,25 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
+using System.Reflection;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BLL.WinService
 {
-    static class Program
+    internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        static void Main()
+        ///// <summary>
+        ///// The main entry point for the application.
+        ///// </summary>
+        //static void Main()
+        //{
+        //    ServiceBase[] ServicesToRun;
+        //    ServicesToRun = new ServiceBase[]
+        //    {
+        //        new Service1()
+        //    };
+        //    ServiceBase.Run(ServicesToRun);
+        //}
+
+        private static void Main(string[] args)
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
+            // Run Service
+            ServiceBase[] services =
             {
                 new Service1()
             };
-            ServiceBase.Run(ServicesToRun);
+
+            if (Environment.UserInteractive)
+            {
+                RunInteractive(services);
+            }
+            else
+            {
+                ServiceBase.Run(services);
+            }
+        }
+
+        public static void RunInteractive(ServiceBase[] services)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Install the services in interactive mode.");
+            Console.WriteLine();
+
+            // Get the method to invoke on each service to start it
+            var onStartMethod =
+                typeof(ServiceBase).GetMethod("OnStart", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            // Start services loop
+            foreach (var service in services)
+            {
+                Console.WriteLine("Installing {0} ... ", service.ServiceName);
+                onStartMethod.Invoke(service, new object[] { new string[] { } });
+                Console.WriteLine("Installed {0} ", service.ServiceName);
+
+                Console.WriteLine();
+            }
+
+            // Waiting the end
+            Console.WriteLine("Press a key to uninstall all services...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            // Get the method to invoke on each service to stop it
+            var onStopMethod = typeof(ServiceBase).GetMethod("OnStop", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            // Stop loop
+            foreach (var service in services)
+            {
+                Console.Write("Uninstalling {0} ... ", service.ServiceName);
+                onStopMethod.Invoke(service, null);
+                Console.WriteLine("Uninstalled {0}", service.ServiceName);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("All services are uninstalled.");
+
+            // Waiting a key press to not return to VS directly
+            if (Debugger.IsAttached)
+            {
+                Console.WriteLine();
+                Console.Write("=== Press a key to quit ===");
+                Console.ReadKey();
+            }
         }
     }
 }
