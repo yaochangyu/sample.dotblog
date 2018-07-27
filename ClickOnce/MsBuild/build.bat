f625bf24709552923c406c746977ee0dbc2a24d1
余小章 <yao-chang.yu@outlook.com>
2018-07-27 15:13:27 +0800

    刪除簽章

--- ClickOnce/MsBuild/build.bat
+++ ClickOnce/MsBuild/build.bat
@@ -1,8 +1,9 @@
 @echo off
-SET MSBUILD="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
-::SET MSBUILD="C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe"
+::SET MSBUILD="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+SET MSBUILD="C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe"
 
 SET CARWIN="Simple.Winfrom.BuildClickOnce.sln"
 
-%MSBUILD% %CARWIN% /target:publish /p:platform="any cpu" /p:configuration="debug" /p:VisualStudioVersion="14.0" /p:ApplicationVersion=2018.07.19.1 /p:TargetCulture=zh-TW
+%MSBUILD% %CARWIN% /target:publish /p:MapFileExtensions=true /p:TrustUrlParameters=true /p:UseApplicationTrust=true /p:CreateDesktopShortcut=true /p:BootstrapperEnabled=true /p:IsWebBootstrapper=true /p:InstallFrom=Web /p:UpdateEnabled=false /p:ApplicationVersion=2017.07.27.1 /p:TargetCulture=zh-TW /p:ProductName=ClickOnce開發版 /p:PublisherName=東和鋼鐵 /p:InstallUrl= /p:PublishUrl= 
+
 pause
