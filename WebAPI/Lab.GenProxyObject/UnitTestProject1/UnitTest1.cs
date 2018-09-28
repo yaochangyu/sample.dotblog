149f33f7ca5e9da7a12c580e175891c4ff4d18f0
余小章 <yao-chang.yu@outlook.com>
2018-09-29 00:26:32 +0800

    Add Lab.GenProxyObject

---
+++ WebAPI/Lab.GenProxyObject/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApi.Proxies.Clients;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly ValuesClient _client;
+
+        public UnitTest1()
+        {
+            if (this._client == null)
+            {
+                this._client = new ValuesClient();
+                this._client.HttpClient.BaseAddress = new Uri("http://localhost:56364");
+            }
+        }
+
+        [TestMethod]
+        public void Put_Test()
+        {
+            var responseMessage = this._client.PutAsync(1, "2").Result;
+            var success = responseMessage.StatusCode == HttpStatusCode.NoContent;
+            Assert.AreEqual(true, success);
+        }
+    }
+}
---
+++ WebAPI/Lab.GenProxyObject/WebApplication1/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebApiProxy.Server;
+
+namespace WebApplication1
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.RegisterProxyRoutes();
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
