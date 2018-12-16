09d44ef736c873988c2b4648ccccf1c65fee10ef
余小章 <yao-chang.yu@outlook.com>
2018-12-16 22:32:41 +0800

    基本驗證

---
+++ ModelValidation/Sample.Validation/UnitTestProject1/Contact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace UnitTestProject1
+{
+    public class Contact : IValidatableObject
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string EMail { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; } = DateTime.Parse("2000-01-01");
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            int result = DateTime.Compare(this.StartDate, this.EndDate);
+            if (result < 0)
+            {
+                yield return new ValidationResult("start date must be less than the end date!", new[] {"ConfirmEmail"});
+            }
+        }
+    }
+}
--- ModelValidation/Sample.Validation/UnitTestProject1/UnitTest1.cs
+++ ModelValidation/Sample.Validation/UnitTestProject1/UnitTest1.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject1
@@ -6,8 +11,34 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TryValidateObject_Test()
         {
+            var contact = new Contact
+            {
+                FirstName = "Armin",
+                LastName = "Zia",
+                EMail = "mail"
+            };
+            var context = new ValidationContext(contact, null, null);
+            var errors = new List<ValidationResult>();
+            if (!Validator.TryValidateObject(contact, context, errors, true))
+            {
+                Assert.AreEqual(1, errors.Count);
+
+            }
+        }
+        [TestMethod]
+        public void ValidateObject_Test()
+        {
+            var contact = new Contact
+            {
+                FirstName = "Armin",
+                LastName = "Zia",
+                EMail = "mail"
+            };
+            var context = new ValidationContext(contact, null, null);
+            Action action = () => Validator.ValidateObject(contact, context, true);
+            action.Should().Throw<ValidationException>();
         }
     }
 }
