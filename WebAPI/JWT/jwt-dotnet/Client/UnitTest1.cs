1adc4c37ad8262b829f1b4fe253d220727a6c1ae
余小章 <yao-chang.yu@outlook.com>
2019-01-07 01:17:20 +0800

    Add JWT-dotnet

--- WebAPI/JWT/jwt-dotnet/Client/UnitTest1.cs
+++ WebAPI/JWT/jwt-dotnet/Client/UnitTest1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net;
 using System.Net.Http;
 using Microsoft.Owin.Hosting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,9 +8,9 @@ namespace Client
     [TestClass]
     public class UnitTest1
     {
-        private const string HOST_ADDRESS = "http://localhost:8001";
         private static IDisposable s_webApp;
         private static HttpClient s_client;
+        private const string HOST_ADDRESS = "http://localhost:8001";
 
         [AssemblyInitialize]
         public static void AssemblyInitialize(TestContext context)
@@ -28,16 +27,9 @@ namespace Client
         {
             s_webApp.Dispose();
         }
-
         [TestMethod]
         public void TestMethod1()
         {
-            var url = "api/Default/1";
-            var response = s_client.GetAsync(url).Result;
-            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-
-            var content = response.Content.ReadAsStringAsync().Result;
-            Assert.AreEqual("\"value\"", content);
         }
     }
 }
