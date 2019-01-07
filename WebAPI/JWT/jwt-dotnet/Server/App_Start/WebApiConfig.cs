9f9882ff3480468d1833cb31329fe5353ea9ccf0
余小章 <yao-chang.yu@outlook.com>
2019-01-07 09:46:16 +0800

    OK

--- WebAPI/JWT/jwt-dotnet/Server/App_Start/WebApiConfig.cs
+++ WebAPI/JWT/jwt-dotnet/Server/App_Start/WebApiConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace Server
 {
@@ -10,15 +7,16 @@ namespace Server
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Filters.Add(new JwtAuthorizeAttribute());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+                                       "DefaultApi",
+                                       "api/{controller}/{id}",
+                                       new {id = RouteParameter.Optional}
+                                      );
         }
     }
 }
---
+++ WebAPI/JWT/jwt-dotnet/Server/DateTimeProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using JWT;
+
+namespace Server
+{
+    public sealed class DateTimeProvider : IDateTimeProvider
+    {
+        private DateTime? _now;
+
+        public DateTime? Now
+        {
+            get
+            {
+                if (this._now.HasValue==false)
+                {
+                    return DateTime.UtcNow;
+                }
+                return this._now;
+            }
+            set { this._now = value; }
+        }
+
+        public DateTimeOffset GetNow()
+        {
+            return this.Now.Value;
+        }
+    }
+}
