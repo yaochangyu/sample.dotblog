ec7cffda111193c5c5b0ad2e452c07dcdcda0a99
余小章 <yao-chang.yu@outlook.com>
2019-01-07 09:21:53 +0800

    add project

---
+++ WebAPI/JWT/jwt-dotnet/Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Client")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Client")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9104bb35-639e-4cac-be90-c598bd6b4375")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebAPI/JWT/jwt-dotnet/Client/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Owin.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Client
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static IDisposable s_webApp;
+        private static HttpClient s_client;
+        private const string HOST_ADDRESS = "http://localhost:8001";
+
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            s_webApp = WebApp.Start<Startup>(HOST_ADDRESS);
+            Console.WriteLine("Web API started!");
+            s_client = new HttpClient();
+            s_client.BaseAddress = new Uri(HOST_ADDRESS);
+            Console.WriteLine("HttpClient started!");
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            s_webApp.Dispose();
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
