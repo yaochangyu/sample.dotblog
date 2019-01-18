e43e86a337b4622b9d62ffc5ba7778f0ecefb72b
余小章 <yao-chang.yu@outlook.com>
2019-01-18 10:35:07 +0800

    create web api & add swagger

---
+++ WebAPI/Swagger/Multiple Operation/Server/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Server.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public IHttpActionResult Get(string userName)
+        {
+            return this.Ok();
+        }
+        public IHttpActionResult Get(string userName, string department)
+        {
+            return this.Ok();
+        }
+
+    }
+}
