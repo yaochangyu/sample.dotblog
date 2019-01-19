769be3f548f197db4775ef74fe7052e5680ddf50
余小章 <yao-chang.yu@outlook.com>
2019-01-19 18:30:58 +0800

    add authorize

---
+++ WebAPI/Swagger/Authorization/Server/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using Server.Filters;
+
+namespace Server
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.Filters.Add(new AuthorizeAttribute());
+            config.Filters.Add(new IdentityBasicAuthenticationAttribute());
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                                       "DefaultApi",
+                                       "api/{controller}/{id}",
+                                       new { id = RouteParameter.Optional }
+                                      );
+        }
+    }
+}
