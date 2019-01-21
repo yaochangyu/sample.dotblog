e4369670abbfa7bd4bf18657057ffc2588150827
余小章 <yao-chang.yu@outlook.com>
2019-01-21 13:22:02 +0800

    add jwt authority

---
+++ WebAPI/Swagger/JWT Authorization/Server/JwtAuthorizeAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Security;
+
+namespace Server
+{
+    public class JwtAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            var authorization = actionContext.Request.Headers.Authorization;
+            if (authorization != null && authorization.Scheme.ToLower() == "bearer")
+            {
+                var token = authorization.Parameter;
+                if (JwtManager.TryValidateToken(token, out var principal))
+                {
+                    Thread.CurrentPrincipal = principal;
+                    if (HttpContext.Current != null)
+                    {
+                        HttpContext.Current.User = principal;
+                    }
+
+                    actionContext.Request.GetRequestContext().Principal = principal;
+                }
+
+            }
+            return base.IsAuthorized(actionContext);
+        }
+    }
+}
---
+++ WebAPI/Swagger/JWT Authorization/Server/SwaggerAuthorizeOperationFilter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.Filters;
+using Swashbuckle.Swagger;
+
+namespace Server
+{
+  
+    public class SwaggerAuthorizeOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            var filterPipeline = apiDescription.ActionDescriptor.GetFilterPipeline();
+
+            // check if authorization is required
+            var isAuthorized = filterPipeline
+                               .Select(filterInfo => filterInfo.Instance)
+                               .Any(filter => filter is IAuthorizationFilter);
+
+            // check if anonymous access is allowed
+            var allowAnonymous = apiDescription.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().Any();
+            if (isAuthorized && !allowAnonymous)
+            {
+                if (operation.security == null)
+                {
+                    operation.security = new List<IDictionary<string, IEnumerable<string>>>();
+                }
+
+                var auth = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"jwt", Enumerable.Empty<string>()},
+                    {"basic", Enumerable.Empty<string>()}
+                };
+
+                operation.security.Add(auth);
+
+                //operation.parameters.Add(new Parameter
+                //{
+                //    name = "Authorization",
+                //    @in = "header", // specify that the value will be sent in the header
+                //    description = "Basic U3dhZ2dlcjpUZXN0", // Basic Swagger:Test
+                //    required = true, // parameter required
+                //    type = "string"
+                //});
+            }
+        }
+    }
+}
