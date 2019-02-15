dfd1eb5b7ade82ce6eca386d2253649679916bcb
余小章 <yao-chang.yu@outlook.com>
2019-02-15 18:04:09 +0800

    first add

---
+++ WebAPI/UploadAndDownloadFiles/Client/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using Microsoft.Owin.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Server.Models;
+
+namespace Client
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string HOST_ADDRESS = "http://localhost:9527";
+        private static IDisposable s_webApp;
+        private static HttpClient s_client;
+
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            s_webApp = WebApp.Start<Startup>(HOST_ADDRESS);
+            Console.WriteLine("Web API started!");
+            s_client = new HttpClient();
+            s_client.BaseAddress = new Uri(HOST_ADDRESS);
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            s_webApp.Dispose();
+        }
+
+        [TestMethod]
+        public void Updload_TEST()
+        {
+            var url = "/api/file/upload";
+            var fileNames = new[] {"例外1.txt", "例外2.txt"};
+            using (var content = new MultipartFormDataContent())
+            {
+                foreach (var fileName in fileNames)
+                {
+                    var fileMimeType = MimeMapping.GetMimeMapping(fileName);
+                    var fileBytes = File.ReadAllBytes(fileName);
+                    var fileContent = new ByteArrayContent(fileBytes);
+                    fileContent.Headers.ContentType = new MediaTypeHeaderValue(fileMimeType);
+                    content.Add(fileContent, fileName, fileName);
+                }
+
+                var response = s_client.PostAsync(url, content).Result;
+                response.EnsureSuccessStatusCode();
+                var result = response.Content.ReadAsAsync<UploadResponse>().Result;
+                Assert.IsNotNull(result);
+            }
+        }
+
+        [TestMethod]
+        public void Upload_TEST2()
+        {
+            var url = "/api/file/UploadFormData";
+            var fileName = "例外1.txt";
+            var mimeType = MimeMapping.GetMimeMapping(fileName);
+            using (var content = new MultipartFormDataContent())
+            {
+                FileStream fileStream = new FileStream(fileName, FileMode.Open);
+
+                var streamContent = new StreamContent(fileStream);
+                streamContent.Headers.ContentType = new MediaTypeHeaderValue(mimeType);
+                streamContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+                    {FileName = fileName};
+                content.Add(streamContent, "File", fileName);
+                s_client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("multipart/form-data"));
+                var response = s_client.PostAsync(url, content).Result;
+                response.EnsureSuccessStatusCode();
+                var result = response.Content.ReadAsAsync<UploadResponse>().Result;
+                Assert.IsNotNull(result);
+            }
+        }
+
+        [TestMethod]
+        public void Download_TEST()
+        {
+            var fileName = "例外1.txt";
+            var url = $"api/file/download?fileName={fileName}";
+
+            var responseMessage = s_client.GetAsync(url).Result;
+            var fileBytes = responseMessage.Content.ReadAsByteArrayAsync().Result;
+        }
+    }
+}
---
+++ WebAPI/UploadAndDownloadFiles/Server/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Swagger.Net.Application;
+
+namespace Server
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapHttpRoute(
+                                name: "swagger_root", 
+                                routeTemplate: "", 
+                                defaults: null, 
+                                constraints: null,
+                                handler: new RedirectHandler((message => message.RequestUri.ToString()), "swagger"));
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
