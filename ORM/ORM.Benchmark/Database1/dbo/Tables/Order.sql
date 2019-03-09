a1b6517ad3e63badd45a9148949b3755b3f0fa66
余小章 <yao-chang.yu@outlook.com>
2019-03-09 14:18:57 +0800

    change schema

--- ORM/ORM.Benchmark/Database1/dbo/Tables/Order.sql
+++ ORM/ORM.Benchmark/Database1/dbo/Tables/Order.sql
@@ -4,7 +4,7 @@
     [OrderTime]   DATETIME         NULL,
     [Remark]      NVARCHAR (50)    NULL,
     [SequenceId]  BIGINT           IDENTITY (1, 1) NOT NULL,
-    CONSTRAINT [PK_dbo.Order] PRIMARY KEY CLUSTERED ([Id] ASC), 
-    CONSTRAINT [FK_Order_Employee_id] FOREIGN KEY ([Employee_Id]) REFERENCES [Employee]([Id])
+    CONSTRAINT [PK_dbo.Order] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Order_Employee_id] FOREIGN KEY ([Employee_Id]) REFERENCES [dbo].[Employee] ([Id])
 );
 
