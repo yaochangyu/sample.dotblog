8679dae0a2e13b325c9b572272e0120a55caad76
余小章 <yao-chang.yu@outlook.com>
2019-03-15 15:15:00 +0800

    add project

---
+++ ORM/Linq2Db/Lab.As400.CRUD1/UnitTestProject1/EntityModel/IDENTITY.cs
@@ -0,0 +1,39 @@
+﻿using LinqToDB.Mapping;
+
+namespace UnitTestProject1.EntityModel
+{
+    /// <summary>
+    /// </summary>
+    [Table("IDENTITY")]
+    public class Identity
+    {
+        /// <summary>
+        ///     MEMBER_ID
+        /// </summary>
+        [Column]
+        [PrimaryKey]
+        public int MEMBER_ID { get; set; }
+
+        /// <summary>
+        ///     ACCOUNT
+        /// </summary>
+        [Column]
+        public string ACCOUNT { get; set; }
+
+        /// <summary>
+        ///     PASSWORD
+        /// </summary>
+        [Column]
+        public string PASSWORD { get; set; }
+
+        /// <summary>
+        ///     REMARK
+        /// </summary>
+        [Column]
+        [Nullable]
+        public string REMARK { get; set; }
+
+        [Association(ThisKey = "MEMBER_ID", OtherKey = "ID", CanBeNull = false)]
+        public Member MEMBER { get; set; }
+    }
+}
