38d05342d713d7550437e8b249592a6a1048ecb2
余小章 <yao-chang.yu@outlook.com>
2019-03-27 22:26:10 +0800

    use page object

---
+++ Test/WinAppDriver/Lab.WinAppDriverTest/App.UnitTest/LoginPage.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace App.UnitTest
+{
+    internal class LoginPage
+    {
+        protected internal WindowsDriver<WindowsElement> _driver;
+
+        public LoginPage(WindowsDriver<WindowsElement> driver)
+        {
+            this._driver = driver;
+        }
+
+        // id
+
+        public WindowsElement IdElement => this._driver.FindElementByAccessibilityId("Id_TextBox");
+
+        // password
+
+        public WindowsElement PasswordElement => this._driver.FindElementByAccessibilityId("Password_TextBox");
+
+        // login
+
+        public WindowsElement LoginElement => this._driver.FindElementByAccessibilityId("Login_Button");
+
+        // messagebox
+        public WindowsElement MessageBoxElemnt => this._driver.FindElementByClassName("#32770");
+
+        // ok button
+        public WindowsElement OkElement => this._driver.FindElementByXPath("//Button[@Name='OK']");
+
+        public LoginPage SetId(string id)
+        {
+            this.IdElement.SendKeys(id);
+            return this;
+        }
+
+        public LoginPage SetPassword(string password)
+        {
+            this.PasswordElement.SendKeys(password);
+            return this;
+        }
+
+        public LoginPage ClickLogin()
+        {
+            this.LoginElement.Click();
+            return this;
+        }
+
+        public LoginPage VerifyMessageBoxByName(string expected)
+        {
+            var messageText = this.MessageBoxElemnt.FindElementByXPath($"//Text[@Name='{expected}']").Text;
+            Assert.AreEqual(expected, messageText);
+            return this;
+        }
+
+        public LoginPage VerifyMessageBoxByTitle(string expected)
+        {
+            var messageText = this.MessageBoxElemnt.Text;
+            Assert.AreEqual(expected, messageText);
+            return this;
+        }
+
+        public LoginPage ClickOK()
+        {
+            this.OkElement.Click();
+            return this;
+        }
+    }
+}
---
+++ Test/WinAppDriver/Lab.WinAppDriverTest/App.UnitTest/PageObjectUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+
+namespace App.UnitTest
+{
+    [TestClass]
+    public class PageObjectUnitTest
+    {
+        protected internal static WindowsDriver<WindowsElement> WindowsDriver;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            var projectName = "App";
+            string solutionPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
+            var targetAppPath = Path.Combine(solutionPath, projectName, "bin", "debug", "app.exe");
+
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("app", targetAppPath);
+            //appCapabilities.SetCapability("app", @"C:\Program Files (x86)\Progress\Telerik UI for WinForms R1 2019\Examples\QuickStart\bin\TelerikExamples.exe");
+
+            ;
+            WindowsDriver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appCapabilities);
+        }
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            WindowsDriver.CloseApp();
+            WindowsDriver.Dispose();
+        }
+
+        [TestMethod]
+        public void 輸入帳號密碼_按下登入_預期得到一個彈跳視窗並呈現Hiyao()
+        {
+            var loginPage = new LoginPage(WindowsDriver);
+            loginPage.SetId("yao")
+                     .SetPassword("123456")
+                     .ClickLogin();
+
+            loginPage.VerifyMessageBoxByTitle("Title")
+                     .VerifyMessageBoxByName("Hi~yao");
+
+            loginPage.ClickOK();
+        }
+    }
+
+
+}
