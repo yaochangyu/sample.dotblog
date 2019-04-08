078b3a68911961985d4b0aea77dd2cca6b7b2c20
余小章 <yao-chang.yu@outlook.com>
2019-04-08 11:04:55 +0800

    add for mstest

---
+++ Test/Specflow3/Lab.Specflow3/ForMsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ForMsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ForMsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("57939f81-f207-45c8-a3b1-cfb93794a346")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test/Specflow3/Lab.Specflow3/ForMsTest/計算機Steps.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace ForMsTest
+{
+    [Binding]
+    public class 計算機Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int firstNumber)
+        {
+            ScenarioContext.Current.Set(firstNumber,"firstNumber");
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int secondNumber)
+        {
+            ScenarioContext.Current.Set(secondNumber,"secondNumber");
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            var firstNumber = ScenarioContext.Current.Get<int>("firstNumber");
+            var secondNumber = ScenarioContext.Current.Get<int>("secondNumber");
+            var calculation = new Calculation();
+            var actual = calculation.Add(firstNumber, secondNumber);
+            ScenarioContext.Current.Set(actual,"actual");
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expected)
+        {
+            var actual = ScenarioContext.Current.Get<int>("actual");
+            Assert.AreEqual(expected,actual);
+        }
+    }
+    public class Calculation
+    {
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
