513b040e99087d0007c26447e710ef8bb3fa17f3
余小章 <yao-chang.yu@outlook.com>
2019-05-28 16:15:06 +0800

    加入SQL專案

---
+++ ORM/EF6/Lab.EF6.DynamicDataMask/Lab.EF6.DynamicDataMask.DB/dbo/Tables/Customer.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [ID]         VARCHAR (11)                                         NOT NULL,
+    [Name]       NVARCHAR (10)                                        NULL,
+    [Birthday]   DATE MASKED WITH (FUNCTION = 'default()')            NULL,
+    [Marriage]   CHAR (1) MASKED WITH (FUNCTION = 'default()')        NULL,
+    [Email]      VARCHAR (50)                                         NULL,
+    [Tel]        VARCHAR (20) MASKED WITH (FUNCTION = 'default()')    NULL,
+    [Salary]     NUMERIC (13, 2) MASKED WITH (FUNCTION = 'default()') NULL,
+    [CreditCard] VARCHAR (19)                                         NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
