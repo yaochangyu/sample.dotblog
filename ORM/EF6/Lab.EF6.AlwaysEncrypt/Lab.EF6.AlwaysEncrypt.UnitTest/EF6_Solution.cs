57cff2313dec9b125e8a2ef92c788cf1b5e2f41f
余小章 <yao-chang.yu@outlook.com>
2019-06-03 10:52:48 +0800

    refactory

--- ORM/EF6/Lab.EF6.AlwaysEncrypt/Lab.EF6.AlwaysEncrypt.UnitTest/EF6_Solution.cs
+++ ORM/EF6/Lab.EF6.AlwaysEncrypt/Lab.EF6.AlwaysEncrypt.UnitTest/EF6_Solution.cs
@@ -47,11 +47,11 @@ EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'
             {
                 var employee1 = new Employee
                 {
-                    Id = Guid.NewGuid(),
-                    Name = "小章",
-                    Age = 18,
+                    Id       = Guid.NewGuid(),
+                    Name     = "小章",
+                    Age      = 18,
                     CreateAt = new DateTime(2019, 12, 1),
-                    Identity = new Identity { Account = "yao", Password = "123456" }
+                    Identity = new Identity {Account = "yao", Password = "123456"}
                 };
                 employee1.Orders = new List<Order>
                 {
@@ -95,11 +95,11 @@ EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'
                 dbContext.Configuration.AutoDetectChangesEnabled = false;
                 var employee1 = new Employee
                 {
-                    Id = Guid.NewGuid(),
-                    Name = "小章",
-                    Age = 18,
+                    Id       = Guid.NewGuid(),
+                    Name     = "小章",
+                    Age      = 18,
                     CreateAt = new DateTime(2019, 12, 1),
-                    Identity = new Identity { Account = "yao", Password = "123456" }
+                    Identity = new Identity {Account = "yao", Password = "123456"}
                 };
                 employee1.Orders = new List<Order>
                 {
@@ -122,21 +122,21 @@ EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'
 
                 var employee2 = new Employee
                 {
-                    Id = Guid.NewGuid(),
-                    Name = "小英",
-                    Age = 23,
+                    Id       = Guid.NewGuid(),
+                    Name     = "小英",
+                    Age      = 23,
                     CreateAt = new DateTime(1909, 1, 2),
-                    Identity = new Identity { Account = "James", Password = "123456" }
+                    Identity = new Identity {Account = "James", Password = "123456"}
                 };
 
                 dbContext.Employees.Add(employee2);
                 var employee3 = new Employee
                 {
-                    Id = Guid.NewGuid(),
-                    Name = "小明",
-                    Age = 33,
+                    Id       = Guid.NewGuid(),
+                    Name     = "小明",
+                    Age      = 33,
                     CreateAt = new DateTime(2011, 2, 2),
-                    Identity = new Identity { Account = "JOJO", Password = "123456" }
+                    Identity = new Identity {Account = "JOJO", Password = "123456"}
                 };
                 dbContext.Employees.Add(employee3);
 
@@ -167,7 +167,7 @@ EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'
 
             using (var dbContext = new TestDbContext())
             {
-                dbContext.Configuration.LazyLoadingEnabled = false;
+                dbContext.Configuration.LazyLoadingEnabled   = false;
                 dbContext.Configuration.ProxyCreationEnabled = false;
                 var employeeGroups = dbContext.Employees
                                               .AsNoTracking()
@@ -197,7 +197,7 @@ EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'
             };
             using (var dbContext = new TestDbContext())
             {
-                dbContext.Configuration.LazyLoadingEnabled = false;
+                dbContext.Configuration.LazyLoadingEnabled   = false;
                 dbContext.Configuration.ProxyCreationEnabled = false;
 
                 var employees = dbContext.Employees
@@ -235,7 +235,7 @@ EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'
 
             using (var dbContext = new TestDbContext())
             {
-                dbContext.Configuration.LazyLoadingEnabled = false;
+                dbContext.Configuration.LazyLoadingEnabled   = false;
                 dbContext.Configuration.ProxyCreationEnabled = false;
                 var employees = dbContext.Employees
                                          .SelectMany(o => o.Orders, (employee, order) => new
@@ -243,25 +243,24 @@ EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'
                                              employee.Id,
                                              employee.Age,
                                              employee.Name,
-                                             Order = new { order.Id, order.Price, order.ProductName }
+                                             Order = new {order.Id, order.Price, order.ProductName}
                                          })
                                          .AsNoTracking()
                                          .ToList()
                     ;
 
-                var group = employees.GroupBy(e => new { e.Id, e.Name, e.Age },
+                var group = employees.GroupBy(e => new {e.Id, e.Name, e.Age},
                                               e => e.Order,
-                                              (e, o) => new { e.Id, e.Name, e.Age, Orders = o })
+                                              (e, o) => new {e.Id, e.Name, e.Age, Orders = o})
                                      .ToList()
                     ;
 
                 group.Should()
                      .BeEquivalentTo(expected, option =>
-                                                            {
-                                                                option.WithStrictOrdering();
-                                                                return option;
-                                                            });
-
+                                               {
+                                                   option.WithStrictOrdering();
+                                                   return option;
+                                               });
             }
         }
 
@@ -306,94 +305,16 @@ EXEC sp_MSForEachTable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'
                 {
                     var employee = new EmployeeViewModel
                     {
-                        Id = element.Id,
+                        Id   = element.Id,
                         Name = element.Name,
-                        Age = element.Age.Value
+                        Age  = element.Age.Value
                     };
                     OrderViewModel order = null;
                     if (element.Order != null)
                     {
                         order = new OrderViewModel
                         {
-                            Id = element.Order.Id,
-                            ProductName = element.Order.ProductName
-                        };
-                    }
-
-                    if (!result.ContainsKey(element.Id))
-                    {
-                        result.Add(element.Id, employee);
-                    }
-
-                    if (order != null)
-                    {
-                        result[element.Id].Orders.Add(order);
-                    }
-                }
-            }
-        }
-
-        [TestMethod]
-        public void 無法直接投影集合12()
-        {
-            var expected = new[]
-            {
-                new
-                {
-                    //Name = "小章", Age = 18,
-                    Name = "小章", Age = 18, Orders = new[]
-                    {
-                        new {Price = (decimal) 20.00, ProductName = "滑鼠"},
-                        new {Price = (decimal) 10.00, ProductName = "鍵盤"}
-                    }
-                }
-            };
-
-            using (var dbContext = new TestDbContext())
-            {
-                var employees = (from employee in dbContext.Employees
-                                 join order in dbContext.Orders on employee.Id equals order.Employee_Id into orders
-                                 from order in orders.DefaultIfEmpty()
-                                 select new
-                                 {
-                                     employee.Id,
-                                     employee.Name,
-                                     employee.Age,
-                                     Order = order == null
-                                                 ? new
-                                                 {
-                                                     Id = Guid.Empty,
-                                                     Price = 0m,
-                                                     ProductName = string.Empty
-                                                 }
-                                                 : new
-                                                 {
-                                                     order.Id,
-                                                     order.Price,
-                                                     order.ProductName
-                                                 }
-                                 }).ToList();
-                foreach (var enumerable in employees.GroupBy(k => k.Id, e => e.Order, (k, os) => os))
-                {
-
-                }
-
-                //		employees.Dump();
-                var result = new Dictionary<Guid, EmployeeViewModel>();
-                foreach (var element in employees)
-                {
-                    var employee = new EmployeeViewModel
-                    {
-                        Id = element.Id,
-                        Name = element.Name,
-                        Age = element.Age.Value
-                    };
-                    OrderViewModel order = null;
-                    if (element.Order != null)
-                    {
-                        order = new OrderViewModel
-                        {
-                            Id = element.Order.Id,
+                            Id          = element.Order.Id,
                             ProductName = element.Order.ProductName
                         };
                     }
