e0abea0674616f97428cc139f3ba4e092a973b8f
余小章 <yao-chang.yu@outlook.com>
2019-08-31 11:54:08 +0800

    refactory

---
+++ WinService/Lab.TopshelfService/ConsoleApp1/DoThing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Timers;
+
+namespace ConsoleApp1
+{
+    public class DoThing
+    {
+        private readonly Timer _timer;
+
+        public DoThing()
+        {
+            this._timer         =  new Timer(1000) { AutoReset = true };
+            this._timer.Elapsed += (sender, eventArgs) => Console.WriteLine($"Now Time:{DateTime.Now}");
+        }
+
+        public void Start()
+        {
+            this._timer.Start();
+            Console.WriteLine($"Timer Start");
+        }
+
+        public void Stop()
+        {
+            this._timer.Stop();
+            Console.WriteLine($"Timer Stop");
+        }
+    }
+}
