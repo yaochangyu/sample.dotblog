ef0d3e3bdc124fb589c7a7fcc7439e0a57ee0ea4
余小章 <yao-chang.yu@outlook.com>
2019-12-13 10:33:33 +0800

    add Process.Report

---
+++ TPL/Winform/Progress.Report Update UI/TPL_IProgress/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TPL_IProgress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_SyncStart = new System.Windows.Forms.Button();
+            this.button_AsyncStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_AsyncStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_SyncStart
+            // 
+            this.button_SyncStart.Location = new System.Drawing.Point(12, 12);
+            this.button_SyncStart.Name = "button_SyncStart";
+            this.button_SyncStart.Size = new System.Drawing.Size(75, 23);
+            this.button_SyncStart.TabIndex = 0;
+            this.button_SyncStart.Text = "Sync Start";
+            this.button_SyncStart.UseVisualStyleBackColor = true;
+            this.button_SyncStart.Click += new System.EventHandler(this.button_SyncStart_Click);
+            // 
+            // button_AsyncStart
+            // 
+            this.button_AsyncStart.Location = new System.Drawing.Point(12, 41);
+            this.button_AsyncStart.Name = "button_AsyncStart";
+            this.button_AsyncStart.Size = new System.Drawing.Size(75, 23);
+            this.button_AsyncStart.TabIndex = 1;
+            this.button_AsyncStart.Text = "Async Start";
+            this.button_AsyncStart.UseVisualStyleBackColor = true;
+            this.button_AsyncStart.Click += new System.EventHandler(this.button_AsyncStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // button_AsyncStop
+            // 
+            this.button_AsyncStop.Location = new System.Drawing.Point(93, 41);
+            this.button_AsyncStop.Name = "button_AsyncStop";
+            this.button_AsyncStop.Size = new System.Drawing.Size(75, 23);
+            this.button_AsyncStop.TabIndex = 4;
+            this.button_AsyncStop.Text = "Async Stop";
+            this.button_AsyncStop.UseVisualStyleBackColor = true;
+            this.button_AsyncStop.Click += new System.EventHandler(this.button_AsyncStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button_AsyncStop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_AsyncStart);
+            this.Controls.Add(this.button_SyncStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_SyncStart;
+        private System.Windows.Forms.Button button_AsyncStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_AsyncStop;
+    }
+}
+
---
+++ TPL/Winform/Progress.Report Update UI/TPL_IProgress/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPL_IProgress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private CancellationTokenSource m_cts = new CancellationTokenSource();
+
+        private async void button_AsyncStart_Click(object sender, EventArgs e)
+        {
+            if (this.m_cts.IsCancellationRequested)
+            {
+                return;
+            }
+
+            label1.Text = "";
+            label2.Text = "";
+
+            var progress = new Progress<ProgressInfo>();
+
+            progress.ProgressChanged += (o, info) =>
+            {
+                label1.Text = info.Data.ToString();
+            };
+
+            var maskTask = await Task.Factory.StartNew(() => SumAsync(this.m_cts.Token, progress)); var status = string.Format("任務完成，完成狀態為：\r\nIsCanceled={0}\r\nIsCompleted={1}\r\nIsFaulted={2}\r\n",
+                      maskTask.IsCanceled,
+                      maskTask.IsCompleted,
+                      maskTask.IsFaulted);
+
+            if (!maskTask.IsCanceled && !maskTask.IsFaulted)
+            {
+                status += string.Format("\r\n計算結果:{0}", maskTask.Result);
+            }
+            label2.Text = status.ToString();
+        }
+
+        private async Task<long> SumAsync(CancellationToken cancellationToken, IProgress<ProgressInfo> progress)
+        {
+            long sum = 0;
+            var info = new ProgressInfo();
+            for (int i = 0; i < 1000; i++)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                }
+                sum++;
+
+                if (progress != null)
+                {
+                    info.Data = sum;
+                    progress.Report(info);
+                }
+
+                SpinWait.SpinUntil(() =>
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                    }
+                    return false;
+                }, 10);
+            }
+            return sum;
+        }
+
+        private Task<long> SumAsync1(CancellationToken cancellationToken, IProgress<ProgressInfo> progress)
+        {
+            var task = Task.Factory.StartNew(() =>
+            {
+                long sum = 0;
+                var info = new ProgressInfo();
+                for (int i = 0; i < 100; i++)
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                    }
+                    sum++;
+                    if (progress != null)
+                    {
+                        info.Data = sum;
+                        progress.Report(info);
+                    }
+                    SpinWait.SpinUntil(() =>
+                    {
+                        if (cancellationToken.IsCancellationRequested)
+                        {
+                            cancellationToken.ThrowIfCancellationRequested();
+                        }
+                        return false;
+                    }, 10);
+                }
+                return sum;
+            });
+            return task;
+        }
+
+        private void button_AsyncStop_Click(object sender, EventArgs e)
+        {
+            if (this.m_cts.IsCancellationRequested)
+            {
+                return;
+            }
+            this.m_cts.Cancel();
+            m_cts = new CancellationTokenSource();
+        }
+
+        private void button_SyncStart_Click(object sender, EventArgs e)
+        {
+            Sum();
+        }
+
+        private long Sum()
+        {
+            long sum = 0;
+            var info = new ProgressInfo();
+            for (int i = 0; i < 1000; i++)
+            {
+                sum++;
+                SpinWait.SpinUntil(() =>
+                {
+                    return false;
+                }, 10);
+            }
+            return sum;
+        }
+    }
+
+    internal class ProgressInfo
+    {
+        public long Data { get; internal set; }
+    }
+}
