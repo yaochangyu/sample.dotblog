f5cf8bdf1607f74c2b06bb1162a157d7d0da9261
余小章 <yao-chang.yu@outlook.com>
2020-02-01 02:30:38 +0800

    add dashboard

---
+++ Nancy/Lab.ServiceNet4/Lab.ServiceNet4/CustomBootstrapper.cs
@@ -0,0 +1,11 @@
+﻿using Nancy;
+using Nancy.Diagnostics;
+
+namespace Lab.ServiceNet4
+{
+    public class CustomBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override DiagnosticsConfiguration DiagnosticsConfiguration =>
+            new DiagnosticsConfiguration {Password = @"pass@w0rd1~"};
+    }
+}
