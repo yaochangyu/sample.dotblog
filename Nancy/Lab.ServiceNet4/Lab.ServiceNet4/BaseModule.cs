4ede2cb74b35bd62b6d27e5b9eaad3f1cd2d0147
余小章 <yao-chang.yu@outlook.com>
2020-02-02 11:57:49 +0800

    add pipeline

---
+++ Nancy/Lab.ServiceNet4/Lab.ServiceNet4/BaseModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Nancy;
+
+namespace Lab.ServiceNet4
+{
+    public class BaseModule : NancyModule
+    {
+        public BaseModule()
+        {
+            this.Before += p =>
+                           {
+                               Console.WriteLine("OnBefore");
+                               return null;
+                           };
+            this.After += p =>
+                          {
+                              Console.WriteLine("OnAfter");
+                          };
+            this.OnError += (p, ex) =>
+                            {
+                                Console.WriteLine(ex.ToString());
+                                return null;
+                            };
+        }
+    }
+}
---
+++ Nancy/Lab.ServiceNet4/Lab.ServiceNet4/GeneratorNancyModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Nancy;
+
+namespace Lab.ServiceNet4
+{
+    public class GeneratorNancyModule : NancyModule
+    {
+        public GeneratorNancyModule()
+        {
+            this.Before += p =>
+                           {
+                               Console.WriteLine("OnBefore");
+                               return null;
+                           };
+            this.After += p => { Console.WriteLine("OnAfter"); };
+            this.OnError += (p, ex) =>
+                            {
+                                Console.WriteLine(ex.ToString());
+                                return null;
+                            };
+            this.ModulePath    = "api/generator";
+            this.Get["/guid"]  = p => Guid.NewGuid().ToString();
+            this.Get["/error"] = p => throw new Exception("壞掉了唷");
+        }
+    }
+}
