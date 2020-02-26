a006c9512d507589a1b8abc1bbf82072779606aa
余小章 <yao-chang.yu@outlook.com>
2020-02-26 21:37:45 +0800

    add hangfire project

---
+++ Hangfire/Lab.HangfireManager/Lab.HangfireManagerJob/HangfireConfig.cs
@@ -0,0 +1,19 @@
+﻿using Hangfire;
+using Hangfire.Console;
+using Owin;
+
+namespace Lab.HangfireManagerJob
+{
+    internal class HangfireConfig
+    {
+        public static void Register(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration
+                               .UseSqlServerStorage("Hangfire")
+                               .UseConsole();
+ 
+            app.UseHangfireDashboard("/hangfire");
+            app.UseHangfireServer();
+        }
+    }
+}
---
+++ Hangfire/Lab.HangfireManager/Lab.HangfireManagerJob/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Owin;
+
+namespace Lab.HangfireManagerJob
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            var config = new HttpConfiguration();
+            HangfireConfig.Register(app);
+            config.Routes.MapHttpRoute("DefaultApi",
+                                       "api/{controller}/{id}",
+                                       new {id = RouteParameter.Optional}
+                                      );
+
+            app.UseWelcomePage("/");
+            app.UseWebApi(config);
+            app.UseErrorPage();
+        }
+    }
+}
