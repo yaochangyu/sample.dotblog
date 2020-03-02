28e2f15c9bd2d277b5424432963e658bc324cb3f
余小章 <yao-chang.yu@outlook.com>
2020-03-02 11:01:36 +0800

    add test project

---
+++ Hangfire/Lab.HangfireSqliteStorage/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static IDisposable s_webApp;
+        private const  string      HOST_ADDRESS = "http://localhost:9527";
+        static void Main(string[] args)
+        {
+            s_webApp = WebApp.Start<Startup>(HOST_ADDRESS);
+            Console.WriteLine("Hangfire service start...");
+            Console.ReadLine();
+        }
+    }
+}
