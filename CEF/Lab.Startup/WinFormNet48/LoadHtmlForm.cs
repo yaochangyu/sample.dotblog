09e8efc06819fcf0b609adfbe255a5117b525351
余小章 <yao-chang.yu@outlook.com>
2020-04-02 23:58:23 +0800

    add winform app at net 4.8

---
+++ CEF/Lab.Startup/WinFormNet48/LoadHtmlForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace WinFormNet48
+{
+    public partial class LoadHtmlForm : Form
+    {
+        private ChromiumWebBrowser _browser;
+
+        public LoadHtmlForm()
+        {
+            this.InitializeComponent();
+            this.InitBrowser();
+            this.FormClosed += this.HtmlForm_FormClosed;
+        }
+
+        public void InitBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            var fileName = Path.Combine(Directory.GetCurrentDirectory(), "Views/Index.html");
+
+            this._browser = new ChromiumWebBrowser(fileName)
+
+                {
+                    Dock = DockStyle.Fill
+                };
+            this.Controls.Add(this._browser);
+        }
+
+        private void HtmlForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Cef.Shutdown();
+            this._browser?.Dispose();
+        }
+    }
+}
