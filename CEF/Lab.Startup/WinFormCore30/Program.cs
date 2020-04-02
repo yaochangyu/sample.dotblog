96b45d95463a685926fdc1022890d51fb505eea9
余小章 <yao-chang.yu@outlook.com>
2020-04-03 00:52:55 +0800

    add net core 3.0 project

---
+++ CEF/Lab.Startup/WinFormCore30/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp.Internals;
+
+namespace WinFormCore30
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ CEF/Lab.Startup/WinFormCore31/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace WinFormCore31
+{
+    public partial class Form1 : Form
+    {
+        public ChromiumWebBrowser _browser;
+
+        public Form1()
+        {
+            this.InitializeComponent();
+            this.FormClosed += this.Form1_FormClosed;
+
+            this.InitBrowser();
+        }
+
+        public void InitBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            this._browser = new ChromiumWebBrowser("www.google.com")
+            {
+                Dock = DockStyle.Fill
+            };
+            this.Controls.Add(this._browser);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Cef.Shutdown();
+            this._browser?.Dispose();
+        }
+    }
+}
