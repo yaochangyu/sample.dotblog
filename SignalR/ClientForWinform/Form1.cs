b57725b4b93baea5b9a29135359a37ac87defc32
yaochang.yu <yao-chang.yu@outlook.com>
2020-05-26 10:56:05 +0800

    refactory

--- SignalR/ClientForWinform/Form1.cs
+++ SignalR/ClientForWinform/Form1.cs
@@ -7,48 +7,37 @@ namespace ClientForWinform
     public partial class Form1 : Form
     {
         private readonly string        _actionName = "Broadcast";
-        private readonly HubConnection _hubConnection;
         private readonly string        _hubName    = "BroadcastHub";
         private readonly string        _signalrUrl = @"https://localhost:44342/";
-        private          IHubProxy     _hubProxy;
+        private readonly HubConnection _hubConnection;
+        private readonly IHubProxy     _hubProxy;
 
         public Form1()
         {
             this.InitializeComponent();
-            if (this._hubConnection == null)
-            {
-                this._hubConnection = new HubConnection(this._signalrUrl);
-            }
-        }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+            this._hubConnection = new HubConnection(this._signalrUrl);
             this._hubProxy = this._hubConnection.CreateHubProxy(this._hubName);
-
             this._hubProxy.On("ShowMessage",
                               (string name, string country) =>
                               {
                                   this.Messages_TextBox
                                       .InvokeIfNecessary(() =>
                                                          {
-                                                             var msg = $"Hi, My name is {name}, I come from {country}\r\n";
+                                                             var msg =
+                                                                 $"Hi, My name is {name}, I come from {country}\r\n";
                                                              this.Messages_TextBox
                                                                  .AppendText(msg);
                                                              Console.WriteLine(msg);
                                                          });
                               });
 
-            //this._hubProxy.On("ShowMessage",
-            //                  (string name) =>
-            //                  {
-            //                      this.Messages_TextBox.InvokeIfNecessary(() =>
-            //                                                           {
-            //                                                               this.Messages_TextBox
-            //                                                                   .AppendText($"{name}\r\n");
-            //                                                           });
-            //                  });
-
             this._hubConnection.Start().Wait();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
 
         private async void SendMessageButton_Click(object sender, EventArgs e)
