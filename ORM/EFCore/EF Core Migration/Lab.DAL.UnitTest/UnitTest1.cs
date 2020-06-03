951b27b0fa26c3c5a562335f65edba89b51d75f8
yaochang.yu <yao-chang.yu@outlook.com>
2020-06-03 09:33:30 +0800

    add test case

--- ORM/EFCore/EF Core Migration/Lab.DAL.UnitTest/UnitTest1.cs
+++ ORM/EFCore/EF Core Migration/Lab.DAL.UnitTest/UnitTest1.cs
@@ -1,5 +1,5 @@
+using System;
 using System.Linq;
-using System.Linq.Expressions;
 using Lab.DAL.EntityModel;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,9 +12,28 @@ namespace Lab.DAL.UnitTest
         [TestMethod]
         public void TestMethod1()
         {
-            using (var dbContext=new LabEmployeeContext(DbOptionsFactory.DbContextOptions))
+            using (var dbContext = new LabEmployeeContext(DbOptionsFactory.DbContextOptions))
             {
                 var employees = dbContext.Employees.AsNoTracking().ToList();
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            using (var dbContext = new LabEmployeeContext(DbOptionsFactory.DbContextOptions))
+            {
+                var id = Guid.NewGuid();
+                var toDb = new Employee
+                {
+                    Id   = id,
+                    Name = "yao",
+                    Age  = 18,
+                };
+                dbContext.Employees.Add(toDb);
+                var count = dbContext.SaveChanges();
+                Assert.AreEqual(true, count           != 0);
+                Assert.AreEqual(true, toDb.SequenceId != 0);
             }
         }
     }
