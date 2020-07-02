30280ea91f5cd7bd82264e8ae8efbdb09e820a60
yaochang.yu <yao-chang.yu@outlook.com>
2020-07-02 17:39:24 +0800

    add core app

---
+++ ConsoleApp/Lab.ConsoleArgs/NETCore/Behaviors/CopyFileRequest.cs
@@ -0,0 +1,27 @@
+﻿using PowerArgs;
+
+namespace NETCore.Behaviors
+{
+    public class CopyFileRequest
+    {
+        public static string MethodName = "Copy";
+
+        [ArgRequired(PromptIfMissing = true)]
+        [ArgDescription("來源路徑")]
+        [ArgShortcut("-S")]
+        [ArgPosition(1)]
+        public string SourceFolder { get; set; }
+
+        [ArgRequired(PromptIfMissing = true)]
+        [ArgDescription("目標路徑")]
+        [ArgPosition(2)]
+        [ArgShortcut("-T")]
+        public string TargetFolder { get; set; }
+
+        [ArgRequired(PromptIfMissing = true)]
+        [ArgDescription("搜尋模式")]
+        [ArgPosition(3)]
+        [ArgShortcut("-SP")]
+        public string SearchPattern { get; set; }
+    }
+}
---
+++ ConsoleApp/Lab.ConsoleArgs/NETCore/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NETCore.Behaviors;
+using PowerArgs;
+
+namespace NETCore
+{
+    internal class Program
+    {
+        static Program()
+        {
+            var currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private static void Main(string[] args)
+        {
+            Args.InvokeAction<MultipleBehavior>(args);
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var exception = (Exception) e.ExceptionObject;
+
+            // todo:write log
+            Console.WriteLine(exception.ToString());
+        }
+    }
+}
