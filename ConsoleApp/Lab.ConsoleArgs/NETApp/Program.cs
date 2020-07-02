7774b5b29eb8651212b00519766f8a0cf674bfc3
yaochang.yu <yao-chang.yu@outlook.com>
2020-07-02 17:31:54 +0800

    add net project

---
+++ ConsoleApp/Lab.ConsoleArgs/NETApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NETApp.Behaviors;
+using PowerArgs;
+
+namespace NETApp
+{
+    internal class Program
+    {
+        static Program()
+        {
+            var currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        public static void Main(string[] args)
+        {
+            var copyFileRequest = Args.Parse<CopyFileRequest>(args);
+            
+            ////綁定單一行為
+            //Args.InvokeMain<SingleBehavior>(args);
+
+            ////綁定多個行為
+            //Args.InvokeAction<MultipleBehavior>(args);
+
+            ////解析行為
+            //var argAction = Args.ParseAction<MultipleBehavior>(args);
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var exception = (Exception) e.ExceptionObject;
+            Console.WriteLine(exception.Message);
+        }
+    }
+}
