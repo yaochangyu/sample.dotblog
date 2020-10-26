e537c11860bedebe420b0fa154cc5dd4c5b76899
yaochang.yu <yao-chang.yu@outlook.com>
2020-10-26 17:34:35 +0800

    fix

--- Log/Lab.MsLogging/WebApiNetCore31/Program.cs
+++ Log/Lab.MsLogging/WebApiNetCore31/Program.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,16 +6,25 @@ namespace WebApiNetCore31
 {
     public class Program
     {
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                       .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                       .ConfigureLogging((context, builder) =>
+                                         {
+                                             builder
+                                                 .AddFilter("Microsoft",        LogLevel.Warning)
+                                                 .AddFilter("System",           LogLevel.Warning)
+                                                 .AddFilter("WindowsFormsApp1", LogLevel.Debug)
+                                                 .AddConfiguration(context.Configuration.GetSection("Logging"))
+                                                 .AddConsole()
+                                                 ;
+                                         });
+        }
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
     }
 }
