9cac6306d3b13c3a006a136909bd9c6be0d04c93
yaochang.yu <yao-chang.yu@outlook.com>
2020-10-28 13:16:48 +0800

    refactory

---
+++ Log/Lab.MsLogging/WebApiNetCore31/LogEvents.cs
@@ -0,0 +1,17 @@
+namespace WebApiNetCore31
+{
+    public class LogEvents
+    {
+        public const int GenerateItems = 1000;
+        public const int ListItems     = 1001;
+        public const int GetItem       = 1002;
+        public const int InsertItem    = 1003;
+        public const int UpdateItem    = 1004;
+        public const int DeleteItem    = 1005;
+
+        public const int TestItem = 3000;
+
+        public const int GetItemNotFound    = 4000;
+        public const int UpdateItemNotFound = 4001;
+    }
+}
