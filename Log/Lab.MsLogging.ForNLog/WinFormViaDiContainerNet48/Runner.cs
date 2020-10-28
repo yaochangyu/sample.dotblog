04ec046be3a25c8209548b12b705fa0454df0475
yaochang.yu <yao-chang.yu@outlook.com>
2020-10-28 16:51:46 +0800

    fix

---
+++ Log/Lab.MsLogging.ForNLog/WinFormViaDiContainerNet48/Runner.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace WinFormViaDiContainerNet48
+{
+    public class Runner
+    {
+        private readonly ILogger<Runner> _logger;
+
+        public Runner(ILogger<Runner> logger)
+        {
+            this._logger = logger;
+        }
+
+        public void DoAction(string name)
+        {
+            this._logger.LogInformation(20, "Doing hard work! {Action}", name);
+        }
+    }
+}
