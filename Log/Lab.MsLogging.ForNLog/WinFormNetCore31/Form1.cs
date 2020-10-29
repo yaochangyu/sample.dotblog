9ee7e3f9859fba2094cc01f6f43c0b8717a41020
yaochang.yu <yao-chang.yu@outlook.com>
2020-10-29 11:24:03 +0800

    fix

--- Log/Lab.MsLogging.ForNLog/WinFormNetCore31/Form1.cs
+++ Log/Lab.MsLogging.ForNLog/WinFormNetCore31/Form1.cs
@@ -1,21 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Extensions.Logging;
 
 namespace WinFormNetCore31
 {
     public partial class Form1 : Form
     {
+        public Runner Runner { get; set; }
+
+        public ILogger<Form1> Logger { get; set; }
+
         public Form1()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
 
+        public Form1(ILogger<Form1> logger, Runner runner)
+        {
+            this.InitializeComponent();
+            this.Logger = logger;
+            this.Runner = runner;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var button = (Button) sender;
+            var name   = button.Name;
+
+            this.Logger.LogInformation(LogEvent.GenerateItem, "{name} 按鈕被按了", name);
+            this.Logger.LogInformation(LogEvent.UpdateItem,   "執行更新");
+
+            this.Runner.DoAction(name);
+
+            this.Logger.LogInformation(LogEvent.GenerateItem, "完成");
+        }
     }
 }
--- Log/Lab.MsLogging.ForNLog/WinFormNetCore31/Program.cs
+++ Log/Lab.MsLogging.ForNLog/WinFormNetCore31/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace WinFormNetCore31
 {
@@ -17,7 +21,36 @@ namespace WinFormNetCore31
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            var config = CreateConfig();
+            using (var serviceProvider = CreateServiceProvider(config))
+            {
+                var form = serviceProvider.GetService(typeof(Form1)) as Form;
+                Application.Run(form);
+            }
+        }
+        private static IConfiguration CreateConfig()
+        {
+            var config = new ConfigurationBuilder()
+                         .SetBasePath(System.IO.Directory
+                                            .GetCurrentDirectory()) //From NuGet Package Microsoft.Extensions.Configuration.Json
+                         .AddJsonFile("appsettings.json", true, true)
+                         .Build();
+            return config;
+        }
+
+        private static ServiceProvider CreateServiceProvider(IConfiguration config)
+        {
+            var serviceCollection = new ServiceCollection();
+            return serviceCollection.AddTransient<Form1>() // Runner is the custom class
+                                    .AddTransient<Runner>()
+                                    .AddLogging(loggingBuilder =>
+                                                {
+                                                    // configure Logging with NLog
+                                                    loggingBuilder.ClearProviders();
+                                                    loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+                                                    loggingBuilder.AddNLog(config);
+                                                })
+                                    .BuildServiceProvider();
         }
     }
 }
