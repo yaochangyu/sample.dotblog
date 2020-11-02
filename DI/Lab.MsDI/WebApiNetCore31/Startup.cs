d5fb701b342f010daf5f97f5296ddc6798b6e2f2
yaochang.yu <yao-chang.yu@outlook.com>
2020-11-02 14:50:18 +0800

    refactory

--- DI/Lab.MsDI/WebApiNetCore31/Startup.cs
+++ DI/Lab.MsDI/WebApiNetCore31/Startup.cs
@@ -34,6 +34,7 @@ namespace WebApiNetCore31
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddControllers();
             services.AddControllers(options => options.Filters.Add(new LogFilterAttribute()));
             
             services.AddTransient(p => new Worker(new LogMessager()));
