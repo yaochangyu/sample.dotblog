5f6c3d7213f18fcf3c3686a12860c518cc5ad050
yaochang.yu <yao-chang.yu@outlook.com>
2020-12-18 18:51:11 +0800

    add polly

--- Http Client/Lab.HttpClientFactory/Client.WinFormsNet48/Startup.cs
+++ Http Client/Lab.HttpClientFactory/Client.WinFormsNet48/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Net;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
 
 namespace Client.WinFormsNet48
 {
@@ -15,13 +18,24 @@ namespace Client.WinFormsNet48
             services.AddHttpClient("lab",
                                    p => { p.BaseAddress = new Uri(BaseAddress); });
             services.AddSingleton<LabService2>();
-            services.AddSingleton<Form2>();
 
             // LabService 注入 HttpClient
-            services.AddHttpClient<LabService>(client =>
-                                               {
-                                                   client.BaseAddress = new Uri(BaseAddress);
-                                               });
+            //services.AddHttpClient<LabService>(client => { client.BaseAddress = new Uri(BaseAddress); });
+
+            services.AddHttpClient<LabService>(client => { client.BaseAddress = new Uri(BaseAddress); })
+
+                    //Polly Retory
+                    .AddPolicyHandler(HttpPolicyExtensions
+
+                                      //HandleTransientHttpError 包含 5xx 及 408 錯誤
+                                      .HandleTransientHttpError()
+
+                                      //404錯誤
+                                      .OrResult(p => p.StatusCode == HttpStatusCode.NotFound)
+                                      .WaitAndRetryAsync(6, attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)))
+                                     );
+
+            services.AddSingleton<Form2>();
             services.AddSingleton<Form1>();
             services.AddSingleton<MainForm>();
             return services;
