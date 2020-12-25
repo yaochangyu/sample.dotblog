46ef105294b9e2ecc01ee3ffc7d638157bc0a26f
yaochang.yu <yao-chang.yu@outlook.com>
2020-12-25 17:18:02 +0800

    fix bug

--- DI/Lab.MsDI/Mvc5Net48/Properties/AssemblyInfo.cs
+++ DI/Lab.MsDI/Mvc5Net48/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mvc5Net48_1")]
+[assembly: AssemblyTitle("Mvc5Net48")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mvc5Net48_1")]
+[assembly: AssemblyProduct("Mvc5Net48")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
