166f3decf158a25817e34d200222dabc1e8e5dfb
yaochang.yu <yao-chang.yu@outlook.com>
2020-12-26 22:30:11 +0800

    refactory

--- DI/Lab.MsDI/WebApiNet48/Global.asax.cs
+++ DI/Lab.MsDI/WebApiNet48/Global.asax.cs
@@ -1,21 +1,13 @@
 using System.Web;
 using System.Web.Http;
 
-//[assembly: PreApplicationStartMethod(typeof(WebApiNet48.WebApiApplication), "InitModule")]
-
 namespace WebApiNet48
 {
     public class WebApiApplication : HttpApplication
     {
-        public static void InitModule()
-        {
-            //RegisterModule(typeof(ServiceScopeModule));
-        }
-
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            // using Microsoft.Extension.DependencyInjection here.
             GlobalConfiguration.Configure(DependencyInjectionConfig.Register);
         }
     }
