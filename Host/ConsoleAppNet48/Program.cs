4fb5cff107b54d29114ccd010eae3a181e615467
yaochang.yu <yao-chang.yu@outlook.com>
2021-01-07 00:42:01 +0800

    add project

---
+++ Host/ConsoleAppNet48/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleAppNet48
+{
+    public class Program
+    {
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                       .ConfigureServices((hostContext, services) =>
+                                          {
+                                              services.AddHostedService<LabHostedService>();
+                                          });
+        }
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+    }
+}
---
+++ Host/ConsoleAppNet5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppNet5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
