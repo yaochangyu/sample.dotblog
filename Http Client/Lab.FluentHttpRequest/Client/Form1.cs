5e38f7ee35e3621d183f8cab64f2c9977e620ecc
yaochang.yu <yao-chang.yu@outlook.com>
2021-01-09 19:30:34 +0800

    add web api project

---
+++ Http Client/Lab.FluentHttpRequest/Client/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        private static readonly string     BasicUrl = "https://localhost:44333/";
+        private static readonly HttpClient Client;
+
+        static Form1()
+        {
+            Client = new HttpClient
+            {
+                BaseAddress = new Uri(BasicUrl)
+            };
+        }
+
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var url      = "WeatherForecast?lessTemperature=11";
+            var response = await Client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            MessageBox.Show(content);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Http Client/Lab.FluentHttpRequest/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get(int lessTemperature)
+        {
+            var rng = new Random();
+            var sources = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                                             {
+                                                 Date         = DateTime.Now.AddDays(index),
+                                                 TemperatureC = rng.Next(-20, 55),
+                                                 Summary      = Summaries[rng.Next(Summaries.Length)]
+                                             })
+                                             .ToArray();
+            return sources.Where(p => p.TemperatureC < lessTemperature).ToList();
+        }
+    }
+}
