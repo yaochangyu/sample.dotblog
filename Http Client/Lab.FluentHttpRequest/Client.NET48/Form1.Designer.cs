615db6b4966a81f50fe9af99ca1412c2af8d970f
yaochang.yu <yao-chang.yu@outlook.com>
2021-01-11 11:15:08 +0800

    add .net 4.8 sample

--- Http Client/Lab.FluentHttpRequest/Client.NET48/Form1.Designer.cs
+++ Http Client/Lab.FluentHttpRequest/Client.NET48/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace Client.NET48
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
 
             // 
@@ -46,16 +47,30 @@ namespace Client.NET48
             this.button1.Click                   += new System.EventHandler(this.button1_Click);
 
             // 
+            // button2
+            // 
+            this.button2.Location                =  new System.Drawing.Point(38, 95);
+            this.button2.Name                    =  "button2";
+            this.button2.Size                    =  new System.Drawing.Size(75, 23);
+            this.button2.TabIndex                =  1;
+            this.button2.Text                    =  "button2";
+            this.button2.UseVisualStyleBackColor =  true;
+            this.button2.Click                   += new System.EventHandler(this.button2_Click);
+
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode       = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize          = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Button button2;
 
         private System.Windows.Forms.Button button1;
 
--- Http Client/Lab.FluentHttpRequest/Client.NET48/Form1.cs
+++ Http Client/Lab.FluentHttpRequest/Client.NET48/Form1.cs
@@ -1,18 +1,59 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 using System.Windows.Forms;
+using Flurl;
+using Flurl.Http;
 
 namespace Client.NET48
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private static readonly string     BasicUrl = "https://localhost:44333/";
+        private static readonly HttpClient Client;
+
+        static Form1()
         {
-            InitializeComponent();
+            Client = new HttpClient
+            {
+                BaseAddress = new Uri(BasicUrl)
+            };
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public Form1()
         {
-            throw new System.NotImplementedException();
+            this.InitializeComponent();
+            
+            ServicePointManager.FindServicePoint(new Uri(BasicUrl))
+                               .ConnectionLeaseTimeout = (int) TimeSpan.FromMinutes(10).TotalMilliseconds;
+            
+            ServicePointManager.DnsRefreshTimeout = (int)TimeSpan.FromMinutes(10).TotalMilliseconds;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var url      = "WeatherForecast?lessTemperature=11";
+            var response = await Client.GetAsync(url);
+
+            // response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            MessageBox.Show(content);
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            var url = "WeatherForecast";
+            var requestUrl = BasicUrl.AppendPathSegment(url)
+                                     .SetQueryParam("lessTemperature", 11);
+            try
+            {
+                var content = await requestUrl.GetAsync().ReceiveString();
+                MessageBox.Show(content);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
         }
     }
 }
