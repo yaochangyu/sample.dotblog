f8f9a655fe622b624d5296246017225cd4d98ed6
yaochang.yu <yao-chang.yu@outlook.com>
2021-03-09 00:35:39 +0800

    create web api 2 web site

---
+++ Coravel/Lab.CoravelScheduler/WebApi.NetFx48/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApi.NetFx48.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public async Task<IHttpActionResult> Get()
+        {
+            return this.Ok("OK");
+        }
+    }
+}
