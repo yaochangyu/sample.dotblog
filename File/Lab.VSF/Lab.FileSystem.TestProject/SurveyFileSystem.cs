7c3067432be79e71b1647158046cb62fa4c61aa8
yaochang.yu <yao-chang.yu@outlook.com>
2021-03-22 13:04:10 +0800

    add 列舉根路徑內的所有結構

--- File/Lab.VSF/Lab.FileSystem.TestProject/SurveyFileSystem.cs
+++ File/Lab.VSF/Lab.FileSystem.TestProject/SurveyFileSystem.cs
@@ -11,6 +11,62 @@ namespace Lab.FileSystem.TestProject
     public class SurveyFileSystem
     {
         [TestMethod]
+        public void 列舉根路徑內的所有結構()
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            var rootPath          = Path.GetDirectoryName(executingAssembly.Location);
+            var subPath           = "TestFolder";
+            var subPath1          = $"{subPath}/1";
+            var subPath1_1        = $"{subPath}/1/1_1";
+            var subPath1_1_1      = $"{subPath}/1/1_1/1_1_1";
+            var subPath2          = $"{subPath}/2";
+            var content           = "This is test string";
+            var contentBytes      = Encoding.UTF8.GetBytes(content);
+
+            Lexical.FileSystem.FileSystem fileSystem = null;
+            try
+            {
+                fileSystem = new Lexical.FileSystem.FileSystem(rootPath);
+                if (fileSystem.Exists(subPath1_1_1) == false)
+                {
+                    fileSystem.CreateDirectory(subPath1_1_1);
+                }
+
+                if (fileSystem.Exists(subPath2) == false)
+                {
+                    fileSystem.CreateDirectory(subPath2);
+                }
+
+                var file1 = $"{subPath1_1}/1_1.text";
+                if (fileSystem.Exists(file1) == false)
+                {
+                    fileSystem.CreateFile(file1, contentBytes);
+                }
+
+                var file2 = Path.Combine(rootPath, subPath1_1_1, "1_1_1.txt");
+                if (fileSystem.Exists(file2) == false)
+                {
+                    using var stream =
+                        fileSystem.Open(file2, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+                    stream.Write(contentBytes, 0, contentBytes.Length);
+                }
+
+                var lines = fileSystem.VisitTree(subPath);
+                foreach (var line in lines)
+                {
+                    Console.WriteLine($"{line.Path}");
+                }
+                
+                fileSystem.PrintTo(Console.Out, subPath);
+            }
+            finally
+            {
+                //還原 
+                fileSystem.Delete(subPath, true);
+            }
+        }
+
+        [TestMethod]
         public void 列舉根路徑內的子資料夾()
         {
             var executingAssembly = Assembly.GetExecutingAssembly();
