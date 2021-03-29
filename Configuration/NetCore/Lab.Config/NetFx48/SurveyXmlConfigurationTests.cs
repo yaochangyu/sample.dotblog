ccf944d42e705b88dd0b028fa782a832ae7e5e58
yaochang.yu <yao-chang.yu@outlook.com>
2021-03-30 01:03:24 +0800

    add read xml file

---
+++ Configuration/NetCore/Lab.Config/NetFx48/SurveyXmlConfigurationTests.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetFx48
+{
+    [TestClass]
+    public class SurveyXmlConfigurationTests
+    {
+        [TestMethod]
+        public void 手動實例化ConfigurationBuilder()
+        {
+            var configBuilder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddXmlFile("appsettings.xml");
+            var configRoot = configBuilder.Build();
+
+            //讀取組態
+
+            Console.WriteLine($"AppId = {configRoot["AppId"]}");
+            Console.WriteLine($"AppId = {configRoot["Player:AppId"]}");
+            Console.WriteLine($"Key = {configRoot["Player:Key"]}");
+            Console.WriteLine($"Connection String = {configRoot["ConnectionStrings:DefaultConnectionString"]}");
+        }
+
+
+
+    }
+}
