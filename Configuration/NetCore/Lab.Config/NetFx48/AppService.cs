03fb61f448236767d0f91da1b2ae139bf5793254
yaochang.yu <yao-chang.yu@outlook.com>
2021-03-30 15:47:01 +0800

    di appservice

---
+++ Configuration/NetCore/Lab.Config/NetFx48/AppService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace NetFx48
+{
+    public class AppService
+    {
+        private readonly IConfiguration _config;
+
+        public AppService(IConfiguration config)
+        {
+            this._config = config;
+        }
+
+        public string GetPlayerId()
+        {
+            return this._config.GetSection("Player:AppId").Value;
+        }
+    }
+}
