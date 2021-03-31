c069b373210a97d1fc4e1de17dc9d684fa5a1fc1
yaochang.yu <yao-chang.yu@outlook.com>
2021-03-31 12:10:52 +0800

    add SurveyKeyPerFileConfigurationTests

---
+++ Configuration/NetCore/Lab.Config/NetFx48/SurveyKeyPerFileConfigurationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetFx48
+{
+    [TestClass]
+    public class SurveyKeyPerFileConfigurationTests
+    {
+        [TestMethod]
+        public void 手動實例化ConfigurationBuilder()
+        {
+            var expected     = "我是檔案內容";
+            var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "keys/aws/web");
+            var configBuilder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddKeyPerFile(folderPath,false);
+            var config = configBuilder.Build();
+
+            //讀取組態
+            var actual = config["NewFile1.txt"];
+            Console.WriteLine($"NewFile1.txt = {actual}");
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
