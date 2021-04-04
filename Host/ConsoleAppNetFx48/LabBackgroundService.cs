5ce55a5ca040a9343bf8841c25780045d2fa373e
yaochang.yu <yao-chang.yu@outlook.com>
2021-04-04 16:45:55 +0800

    refactor

---
+++ Host/ConsoleAppNetFx48/LabBackgroundService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleAppNetFx48
+{
+    public class LabBackgroundService : BackgroundService
+    {
+        private readonly ILogger<LabBackgroundService> _logger;
+
+        public LabBackgroundService(ILogger<LabBackgroundService> logger)
+        {
+            this._logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                this._logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
