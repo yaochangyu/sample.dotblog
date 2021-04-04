e48a42edd245d24bfb933ce79d8801e550a4a1c1
yaochang.yu <yao-chang.yu@outlook.com>
2021-04-04 16:45:55 +0800

    refactor

---
+++ Host/ConsoleAppNetFx48/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppNetFx48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Host/NetFx48/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetFx48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
