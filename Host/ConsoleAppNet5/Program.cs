30d488483cb238ddcb5ece695795b7a1cdf42bff
yaochang.yu <yao-chang.yu@outlook.com>
2021-04-06 18:00:48 +0800

    refactor

--- Host/ConsoleAppNet5/Program.cs
+++ Host/ConsoleAppNet5/Program.cs
@@ -2,9 +2,9 @@
 
 namespace ConsoleAppNet5
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
