b15680708df3d1ff7cf3d3aecea35df2a0adf232
yaochang.yu <yao-chang.yu@outlook.com>
2021-04-07 18:48:52 +0800

    add batch file

--- Host/Lab.WorkerService/ConsoleAppNetFx48/CallSafeCreateService.bat
+++ Host/Lab.WorkerService/ConsoleAppNetFx48/CallSafeCreateService.bat
@@ -3,13 +3,13 @@ set batchFolder=%~dp0
 set serviceName=ConsoleAppNetFx48
 set serviceDisplayName=ConsoleAppNetFx48
 set serviceDescription="ด๚ธี"
-set serviceLaunchPath=%batchFolder%ConsoleAppNetFx48.exe
+set serviceLaunchPath=%batchFolder%bin\ConsoleAppNetFx48.exe
 set serviceLogonId=.\setup
-set serviceLogonPassword=password
-::set serverName=\\YAO-S658RF
+set serviceLogonPassword=pass@w0rd1~
+::set serverName=\\Computer Name
 set serverName=
 Call SafeStopService %serviceName% %serverName%
 Call SafeDeleteService %serviceName% %serverName%
 Call SafeCreateService %serviceName% %serviceDisplayName% %serviceDescription% %serviceLaunchPath% %serviceLogonId% %serviceLogonPassword% %serverName%
-::Call SafeStartService %serviceName% %serverName%
+Call SafeStartService %serviceName% %serverName%
 
