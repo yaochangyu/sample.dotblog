2adb7066d4b830a74b30b13ffde9aa1f71ea3d34
yaochang.yu <yao-chang.yu@outlook.com>
2021-04-12 09:17:40 +0800

    add

---
+++ ORM/EFCore/Lab.VirtualDb/Lab.DAL/DomainModel/Employee/InsertRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab.DAL.DomainModel.Employee
+{
+    public class InsertRequest
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int? Age { get; set; }
+
+        [Required]
+        public string Account { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string Remark { get; set; }
+    }
+}
