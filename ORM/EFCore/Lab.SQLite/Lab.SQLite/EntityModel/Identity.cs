83bde0613ce471ff36c6501fa2f0d94c90c83c32
yaochang.yu <yao-chang.yu@outlook.com>
2021-04-15 23:55:44 +0800

    first add

---
+++ ORM/EFCore/Lab.SQLite/Lab.SQLite/EntityModel/Identity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab.SQLite.EntityModel
+{
+    [Table("Identity")]
+    public class Identity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid Employee_Id { get; set; }
+
+        [Required]
+        public string Account { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long SequenceId { get; set; }
+
+        public string Remark { get; set; }
+
+        [Required]
+        public DateTime CreateAt { get; set; }
+
+        [Required]
+        public string CreateBy { get; set; }
+
+        [ForeignKey("Employee_Id")]
+        public virtual Employee Employee { get; set; }
+    }
+}
