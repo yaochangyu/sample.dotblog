bd4a15fdf1131b45fbe1226e450013f4e268affa
yaochang.yu <yao-chang.yu@outlook.com>
2021-04-16 15:44:56 +0800

    add web api project

---
+++ ORM/EFCore/Lab.VirtualDb/Lab.WebApi/ServiceModel/Employee/NewRequest.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab.WebApi.ServiceModel.Employee
+{
+    public class NewRequest
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public int? Age { get; set; }
+
+        [Required]
+        public string Account { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string Remark { get; set; }
+    }
+}
