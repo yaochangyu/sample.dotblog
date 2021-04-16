ab8fbbb347cb80c70501145b6f13df8ccc5d0096
yaochang.yu <yao-chang.yu@outlook.com>
2021-04-16 15:44:56 +0800

    add web api project

---
+++ ORM/EFCore/Lab.VirtualDb/Lab.WebApi/Controllers/DefaultController.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Lab.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ServiceModel = Lab.WebApi.ServiceModel;
+using DomainModel = Lab.DAL.DomainModel;
+
+namespace Lab.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DefaultController : ControllerBase
+    {
+        private readonly ILogger<DefaultController> _logger;
+        private readonly EmployeeRepository         _repository;
+
+        public DefaultController(ILogger<DefaultController> logger, EmployeeRepository repository)
+        {
+            this._logger     = logger;
+            this._repository = repository;
+        }
+
+        [HttpGet]
+        [Produces(typeof(ServiceModel.Employee.FilterResponse))]
+        public async Task<IActionResult> Get(CancellationToken cancel = default)
+        {
+            var repository = this._repository;
+            var record     = await repository.GetAllAsync(cancel);
+            return this.Ok(record);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(ServiceModel.Employee.NewRequest request,
+                                              string                           accessId,
+                                              CancellationToken                cancel = default)
+        {
+            var repository = this._repository;
+            var count = await repository.NewAsync(new DomainModel.Employee.NewRequest()
+            {
+                Account  = request.Account,
+                Age      = request.Age,
+                Name     = request.Name,
+                Password = request.Password,
+                Remark   = request.Remark
+            }, accessId, cancel);
+            if (count == 2)
+            {
+                return this.Ok();
+            }
+            else
+            {
+                return this.NoContent();
+            }
+        }
+    }
+}
---
+++ ORM/EFCore/Lab.VirtualDb/Lab.WebApi/ServiceModel/Employee/FilterResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab.WebApi.ServiceModel.Employee
+{
+    //for doc
+    public class FilterResponse
+    {
+        public string Name { get; set; }
+
+        public int? Age { get; set; }
+
+        public string Account { get; set; }
+
+        public string Password { get; set; }
+
+        public string Remark { get; set; }
+    }
+}
