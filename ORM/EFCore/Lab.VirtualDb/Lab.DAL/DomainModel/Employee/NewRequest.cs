4c2eec954cc598f091bbcd75f67b060e67c11a52
yaochang.yu <yao-chang.yu@outlook.com>
2021-04-16 11:21:15 +0800

    refactor

--- ORM/EFCore/Lab.VirtualDb/Lab.DAL/DomainModel/Employee/NewRequest.cs
+++ ORM/EFCore/Lab.VirtualDb/Lab.DAL/DomainModel/Employee/NewRequest.cs
@@ -5,9 +5,6 @@ namespace Lab.DAL.DomainModel.Employee
 {
     public class NewRequest
     {
-        [Key]
-        public Guid Id { get; set; }
-
         [Required]
         public string Name { get; set; }
 
