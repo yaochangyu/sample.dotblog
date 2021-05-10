0f4597434c337b123fcbb9335416491d305f8f54
yaochang.yu <yao-chang.yu@outlook.com>
2021-05-10 14:49:32 +0800

    fix: DefaultDependencyResolver.Dispose() 不會調用物件的 Dispose()

--- DI/Lab.MsDI/WebApiNet48/Message/MachineMessager.cs
+++ DI/Lab.MsDI/WebApiNet48/Message/MachineMessager.cs
@@ -5,5 +5,10 @@ namespace WebApiNet48
     internal class MachineMessager : IMessager
     {
         public string OperationId { get; } = $"機器-{Guid.NewGuid()}";
+
+        public void Dispose()
+        {
+            Console.WriteLine($"{nameof(MachineMessager)} GC");
+        }
     }
 }
