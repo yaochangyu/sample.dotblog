01173170845dd94657b00f90447cf5e120d188ba
yaochang.yu <yao-chang.yu@outlook.com>
2021-05-10 15:11:32 +0800

    refactor: 觀察 asp.net core 專案的 DI 有沒有正常的呼叫 IMessage.Dispose

--- DI/Lab.MsDI/WebApiNetCore31/Message/LogMessager.cs
+++ DI/Lab.MsDI/WebApiNetCore31/Message/LogMessager.cs
@@ -5,5 +5,10 @@ namespace WebApiNetCore31
     internal class LogMessager : IMessager
     {
         public string OperationId { get; } = $"日誌-{Guid.NewGuid()}";
+
+        public void Dispose()
+        {
+            Console.WriteLine($"{nameof(LogMessager)} GC");
+        }
     }
 }
--- DI/Lab.MsDI/WebApiNetCore31/Message/MultiMessager.cs
+++ DI/Lab.MsDI/WebApiNetCore31/Message/MultiMessager.cs
@@ -5,5 +5,10 @@ namespace WebApiNetCore31
     public class MultiMessager : IScopeMessager, ISingleMessager, ITransientMessager
     {
         public string OperationId { get; } = $"多個接口-{Guid.NewGuid()}";
+
+        public void Dispose()
+        {
+            Console.WriteLine($"{nameof(MultiMessager)} GC");
+        }
     }
 }
