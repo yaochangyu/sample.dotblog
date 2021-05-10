1b077c19c31f13451c8a1476841f9d1f47f21fae
yaochang.yu <yao-chang.yu@outlook.com>
2021-05-10 15:07:02 +0800

    refactor: 觀察 mvc5 專案的 DI 有沒有正常的呼叫 IMessage.Dispose

--- DI/Lab.MsDI/Mvc5Net48/Message/LogMessager.cs
+++ DI/Lab.MsDI/Mvc5Net48/Message/LogMessager.cs
@@ -5,5 +5,10 @@ namespace Mvc5Net48.Message
     internal class LogMessager : IMessager
     {
         public string OperationId { get; } = $"日誌-{Guid.NewGuid()}";
+
+        public void Dispose()
+        {
+            Console.WriteLine($"{nameof(LogMessager)} GC");
+        }
     }
 }
