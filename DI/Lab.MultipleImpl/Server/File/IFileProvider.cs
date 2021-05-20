5880d375386359b75462271081df9d17bcacffca
yaochang.yu <yao-chang.yu@outlook.com>
2021-05-20 10:22:55 +0800

    feat: 完成 func name 注入

---
+++ DI/Lab.MultipleImpl/Server/File/IFileProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Server
+{
+    public interface IFileProvider
+    {
+        string Print();
+    }
+}
---
+++ DI/Lab.MultipleImpl/Server/ServiceProviderExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Server
+{
+    public static class ServiceProviderExtension
+    {
+        public static T GetService<T>(this IServiceProvider provider, string name)
+        {
+            var pool = (Func<string, IFileProvider>) provider.GetService(typeof(Func<string, IFileProvider>));
+            return (T) pool(name);
+        }
+    }
+
+}
