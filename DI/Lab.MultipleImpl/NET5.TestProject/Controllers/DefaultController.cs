3735188b5bcfa09d2518c055822e1f52d442891c
yaochang.yu <yao-chang.yu@outlook.com>
2021-05-21 11:17:32 +0800

    add NET5.TestProject.csproj

---
+++ DI/Lab.MultipleImpl/NET5.TestProject/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NET5.TestProject.File;
+
+namespace NET5.TestProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FuncController : ControllerBase
+    {
+        private readonly ILogger<FuncController> _logger;
+
+        public FuncController(ILogger<FuncController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{type}")]
+        public IActionResult Get(string type)
+        {
+            var fileProvider = this.HttpContext.RequestServices.GetService<IFileProvider>(type);
+            var result       = fileProvider.Print();
+            return this.Ok(result);
+        }
+    }
+}
---
+++ DI/Lab.MultipleImpl/NET5.TestProject/ServiceProviderExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NET5.TestProject.File;
+
+namespace NET5.TestProject
+{
+    public static class ServiceProviderExtension
+    {
+        public static T GetService<T>(this IServiceProvider provider, string name)
+        {
+            var pool = (Func<string, IFileProvider>) provider.GetService(typeof(Func<string, IFileProvider>));
+            return (T) pool(name);
+        }
+    }
+
+}
