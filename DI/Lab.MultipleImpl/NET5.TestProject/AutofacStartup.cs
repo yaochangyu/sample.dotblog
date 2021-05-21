07111d57f9a360aebd642a1450733972bbee60cc
yaochang.yu <yao-chang.yu@outlook.com>
2021-05-21 11:17:32 +0800

    add NET5.TestProject.csproj

---
+++ DI/Lab.MultipleImpl/NET5.TestProject/AutofacStartup.cs
@@ -0,0 +1,53 @@
+﻿using Autofac;
+using Autofac.Features.AttributeFilters;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NET5.TestProject.Controllers;
+using NET5.TestProject.File;
+
+namespace NET5.TestProject
+{
+    public class AutofacStartup
+    {
+        public AutofacStartup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterType<FileProvider>().Keyed<IFileProvider>("file");
+            builder.RegisterType<ZipFileProvider>().Keyed<IFileProvider>("zip");
+            builder.RegisterType<AutofacController>().WithAttributeFiltering();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                    .AddControllersAsServices();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
