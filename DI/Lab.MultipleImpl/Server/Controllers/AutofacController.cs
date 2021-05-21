31cf8cef69bc18bf6285938f8552ff54bf3794fc
yaochang.yu <yao-chang.yu@outlook.com>
2021-05-21 10:46:28 +0800

    feat: 新增 Autofac test test

---
+++ DI/Lab.MultipleImpl/Server/Controllers/AutofacController.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Autofac.Features.AttributeFilters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AutofacController : ControllerBase
+    {
+        private readonly IFileProvider _fileProvider;
+
+        private readonly ILogger<AutofacController> _logger;
+
+        // public AutofacDefaultController(ILogger<AutofacDefaultController> logger)
+        // {
+        //     this._logger = logger;
+        // }
+
+        public AutofacController(ILogger<AutofacController> logger,
+                                        [KeyFilter("zip")] IFileProvider  fileProvider)
+        {
+            this._logger       = logger;
+            this._fileProvider = fileProvider;
+            this._fileProvider.Print();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Ok(this._fileProvider.Print());
+        }
+    }
+}
