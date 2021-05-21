ff8e202538de87327086e6f99e9a09e6fcd24e8c
yaochang.yu <yao-chang.yu@outlook.com>
2021-05-21 13:42:05 +0800

    new manual di register

---
+++ DI/Lab.MultipleImpl/NET5.TestProject/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NET5.TestProject.File;
+
+namespace NET5.TestProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DefaultController : ControllerBase
+    {
+        private readonly ILogger<DefaultController> _logger;
+        private readonly IFileProvider              _fileProvider;
+
+        public DefaultController(ILogger<DefaultController> logger, 
+                                 IFileProvider fileProvider)
+        {
+            this._logger       = logger;
+            this._fileProvider = fileProvider;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // var fileProvider = this.HttpContext.RequestServices.GetService<IFileProvider>();
+            var fileProvider = this._fileProvider;
+            var result       = fileProvider.Print();
+            return this.Ok(result);
+        }
+    }
+}
