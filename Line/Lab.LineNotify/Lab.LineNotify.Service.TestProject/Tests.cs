8c1c30d61d07d74f6c8b3fef75a5a08a80be53c5
yaochang.yu <yao-chang.yu@outlook.com>
2021-08-19 22:44:44 +0800

    refactory

---
+++ Line/Lab.LineNotify/Lab.LineNotify.Service.TestProject/Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab.LineNotify.Service.TestProject
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void AAA()
+        {
+        }
+    }
+}
