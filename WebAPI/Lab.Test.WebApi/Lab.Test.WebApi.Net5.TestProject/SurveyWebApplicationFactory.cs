0aae0677a4c497c92766f2d6c52bc1b63c15ca26
yao <yao-chang.yu@outlook.com>
2021-10-23 15:01:01 +0800

    add project

---
+++ WebAPI/Lab.Test.WebApi/Lab.Test.WebApi.Net5.TestProject/SurveyWebApplicationFactory.cs
@@ -0,0 +1,32 @@
+using System;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab.Test.WebApi.Net5.TestProject
+{
+    [TestClass]
+    public class SurveyWebApplicationFactory
+    {
+        [TestMethod]
+        public void CustomTestServer()
+        {
+            var server = new CustomTestServer();
+            var httpClient = server.CreateClient();
+            var url = "demo/file";
+            var response = httpClient.GetAsync(url).Result;
+            var result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(result);
+        }
+
+        [TestMethod]
+        public void WebApplicationFactory基本用法()
+        {
+            var server = new WebApplicationFactory<Startup>();
+            var httpClient = server.CreateClient();
+            var url = "demo/file";
+            var response = httpClient.GetAsync(url).Result;
+            var result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ WebAPI/Lab.Test.WebApi/Lab.Test.WebApi.Net5/Controllers/DemoController.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Lab.Test.WebApi.Net5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+        private readonly IFileProvider _fileProvider;
+        private readonly ILogger<DemoController> _logger;
+
+        public DemoController(ILogger<DemoController> logger,
+                              IFileProvider fileProvider)
+        {
+            this._logger = logger;
+            this._fileProvider = fileProvider;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(QueryResponse))]
+        public IActionResult Get(CancellationToken cancel)
+        {
+            return this.Ok(new QueryResponse
+            {
+                Message = "Hello"
+            });
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(QueryResponse))]
+        [Route("file")]
+        public IActionResult GetFile(CancellationToken cancel)
+        {
+            return this.Ok(new QueryResponse
+            {
+                Message = this._fileProvider.Name()
+            });
+        }
+    }
+
+    public class QueryResponse
+    {
+        public string Message { get; set; }
+    }
+}
