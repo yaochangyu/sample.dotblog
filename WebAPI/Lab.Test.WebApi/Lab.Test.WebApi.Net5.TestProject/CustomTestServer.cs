0c60abb38afdb7a54db4db73eced495ad049149c
yao <yao-chang.yu@outlook.com>
2021-10-23 15:01:01 +0800

    add project

---
+++ WebAPI/Lab.Test.WebApi/Lab.Test.WebApi.Net5.TestProject/CustomTestServer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+
+namespace Lab.Test.WebApi.Net5.TestProject
+{
+    public class CustomTestServer : WebApplicationFactory<Startup>
+    {
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped(p =>
+            {
+                var fileProvider = Substitute.For<IFileProvider>();
+                fileProvider.Name().Returns("Fake FileProfile");
+                return fileProvider;
+            });
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(this.ConfigureServices);
+        }
+    }
+}
