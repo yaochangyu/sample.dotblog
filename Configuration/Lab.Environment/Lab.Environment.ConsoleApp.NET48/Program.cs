637a997a3a24114adc451ac51f4c1872a5684ebe
yao <yao-chang.yu@outlook.com>
2021-12-08 19:28:19 +0800

    feat: add project

---
+++ Configuration/Lab.Environment/Lab.Environment.ConsoleApp.NET48/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace Lab.Environment.ConsoleApp.NET48
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var appEnv = System.Environment.GetEnvironmentVariable("APP_ENV");
+            var scoopPath = System.Environment.GetEnvironmentVariable("scoop");
+
+            if (string.IsNullOrWhiteSpace(appEnv) == false)
+            {
+                Console.WriteLine(appEnv);
+            }
+        }
+    }
+}
