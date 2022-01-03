ee8f6fa5107c004627f154c89003029c424fde55
yao <yao-chang.yu@outlook.com>
2022-01-03 22:46:02 +0800

    feat: add specflow project

---
+++ Test/Lab.AllureReport/Lab.AllureReport4Specflow/計算機Step.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace Lab.AllureReport4Specflow;
+
+[Binding]
+public class 計算機Step : Steps
+{
+    [Given(@"the first number is (.*)")]
+    public void GivenTheFirstNumberIs(double firstNumber)
+    {
+        this.ScenarioContext.Set(firstNumber, "firstNumber");
+    }
+
+    [Given(@"the second number is (.*)")]
+    public void GivenTheSecondNumberIs(double secondNumber)
+    {
+        this.ScenarioContext.Set(secondNumber, "secondNumber");
+    }
+
+    [Then(@"the result should be (.*)")]
+    public void ThenTheResultShouldBe(int expected)
+    {
+        var actual = this.ScenarioContext.Get<double>("actual");
+        Assert.AreEqual(expected, actual);
+    }
+
+    [When(@"the two numbers are added")]
+    public void WhenTheTwoNumbersAreAdded()
+    {
+        var firstNumber = this.ScenarioContext.Get<double>("firstNumber");
+        var secondNumber = this.ScenarioContext.Get<double>("secondNumber");
+        var calculation = new Calculation();
+        var actual = calculation.Add(firstNumber, secondNumber);
+        this.ScenarioContext.Set(actual, "actual");
+    }
+}
