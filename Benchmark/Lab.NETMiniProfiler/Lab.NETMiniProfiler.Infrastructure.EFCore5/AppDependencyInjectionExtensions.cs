41fa6a043c48af667dc172100e3a59665ef898a5
yao <yao-chang.yu@outlook.com>
2022-01-16 13:03:19 +0800

    feat: add 大象資料庫

--- Benchmark/Lab.NETMiniProfiler/Lab.NETMiniProfiler.Infrastructure.EFCore5/AppDependencyInjectionExtensions.cs
+++ Benchmark/Lab.NETMiniProfiler/Lab.NETMiniProfiler.Infrastructure.EFCore5/AppDependencyInjectionExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Lab.NETMiniProfiler.Infrastructure.EFCore5.EntityModel;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer.Infrastructure.Internal;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure.Internal;
 
 namespace Lab.NETMiniProfiler.Infrastructure.EFCore5;
 
@@ -9,44 +11,45 @@ public static class AppDependencyInjectionExtensions
 {
     public static void AddAppEnvironment(this IServiceCollection services)
     {
-        services.AddLogging(builder =>
-        {
-            builder.AddConsole();
-        });
+        services.AddLogging(builder => { builder.AddConsole(); });
         services.AddSingleton<AppEnvironmentOption>();
     }
 
     public static void AddEntityFramework(this IServiceCollection services)
     {
+        // services.AddPooledDbContextFactory<EmployeeDbContext>((provider, optionsBuilder) =>
+        // {
+        //     var option = provider.GetService<AppEnvironmentOption>();
+        //     var connectionString = option.EmployeeDbConnectionString;
+        //     var loggerFactory = provider.GetService<ILoggerFactory>();
+        //     optionsBuilder.UseSqlServer(connectionString)
+        //                   .UseLoggerFactory(loggerFactory)
+        //         ;
+        // });
+
         services.AddPooledDbContextFactory<EmployeeDbContext>((provider, optionsBuilder) =>
         {
-            var option = provider.GetService<AppEnvironmentOption>();
-            var connectionString = option.EmployeeDbConnectionString;
+            // var mssqlOptions = optionsBuilder.Options.FindExtension<SqlServerOptionsExtension>();
+            // var npgsqlOptions = optionsBuilder.Options.FindExtension<NpgsqlOptionsExtension>();
+
+            var appOption = provider.GetService<AppEnvironmentOption>();
             var loggerFactory = provider.GetService<ILoggerFactory>();
-            optionsBuilder.UseSqlServer(connectionString)
+            var connectionString = appOption.EmployeeDbConnectionString;
+            // optionsBuilder.UseSqlServer(connectionString)
+            //               .UseLoggerFactory(loggerFactory);
+
+            optionsBuilder.UseNpgsql(
+                              connectionString, //只會呼叫一次
+                              builder =>
+                                  builder.EnableRetryOnFailure(
+                                      10,
+                                      TimeSpan.FromSeconds(30),
+                                      new List<string> { "57P01" }))
+
+                          // .UseLazyLoadingProxies()
+                          // .UseSnakeCaseNamingConvention()
                           .UseLoggerFactory(loggerFactory)
                 ;
         });
-
-        ;
-
-        // services.AddPooledDbContextFactory<EmployeeDbContext>((provider, options) =>
-        // {
-        //     var option = provider.GetService<AppEnvironmentOption>();
-        //     var loggerFactory = provider.GetService<ILoggerFactory>();
-        //     options.UseNpgsql(
-        //                option.MemberDbConnectionString, //只會呼叫一次
-        //                builder =>
-        //                    builder.EnableRetryOnFailure(
-        //                        10,
-        //                        TimeSpan.FromSeconds(30),
-        //                        new List<string> { "57P01" }))
-        //
-        //            //  .UseLazyLoadingProxies()
-        //            .UseSnakeCaseNamingConvention()
-        //            .UseLoggerFactory(loggerFactory)
-        //         ;
-        // });
     }
-
 }
