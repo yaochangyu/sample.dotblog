e50cf1678ea7806ea111d0d39559d96e7afd0823
yao <yao-chang.yu@outlook.com>
2022-01-16 10:45:20 +0800

    add files

---
+++ Benchmark/Lab.NETMiniProfiler/Lab.NETMiniProfiler.Infrastructure.EFCore5/AppEnvironmentOption.cs
@@ -0,0 +1,31 @@
+﻿namespace Lab.NETMiniProfiler.Infrastructure.EFCore5;
+
+public class AppEnvironmentOption
+{
+    public string EmployeeDbConnectionString
+    {
+        get
+        {
+            if (string.IsNullOrWhiteSpace(this._employeeDbConnectionString))
+            {
+                this._employeeDbConnectionString =
+                    EnvironmentAssistant.GetEnvironmentVariable(this.EMPLOYEE_DB_CONN_STR);
+            }
+
+            return this._employeeDbConnectionString;
+        }
+        set
+        {
+            this._employeeDbConnectionString = value;
+            Environment.SetEnvironmentVariable(this.EMPLOYEE_DB_CONN_STR, value);
+        }
+    }
+
+    private string _employeeDbConnectionString;
+    private readonly string EMPLOYEE_DB_CONN_STR = "EMPLOYEE_DB_CONNECTION_STR";
+
+    public void Initial()
+    {
+        var memberDbConnectionString = this.EmployeeDbConnectionString;
+    }
+}
---
+++ Benchmark/Lab.NETMiniProfiler/Lab.NETMiniProfiler.Infrastructure.EFCore5/EntityModel/OrderHistory.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab.NETMiniProfiler.Infrastructure.EFCore5.EntityModel
+{
+    [Table("OrderHistory")]
+    public class OrderHistory
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public Guid? Employee_Id { get; set; }
+
+        public string Remark { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long SequenceId { get; set; }
+
+        public string Product_Id { get; set; }
+
+        public string Product_Name { get; set; }
+
+        [Required]
+        public DateTime CreateAt { get; set; }
+
+        [Required]
+        public string CreateBy { get; set; }
+    }
+}
