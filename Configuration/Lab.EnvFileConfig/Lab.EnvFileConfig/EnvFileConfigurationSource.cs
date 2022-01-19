23bd21a9c729910bc8f7d39ea89799a7d83cc521
yao <yao-chang.yu@outlook.com>
2022-01-20 00:46:40 +0800

    feat: Env File Config

---
+++ Configuration/Lab.EnvFileConfig/Lab.EnvFileConfig/EnvFileConfigurationSource.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Lab.EnvFileConfig
+{
+    public class EnvFileConfigurationSource : IConfigurationSource
+    {
+        private readonly string _envFile;
+
+        public EnvFileConfigurationSource(string envFile)
+        {
+            this._envFile = envFile;
+        }
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new EnvFileConfigurationProvider(this._envFile);
+        }
+    }
+}
---
+++ Configuration/Lab.EnvFileConfig/Lab.EnvFileConfig/Program.cs
@@ -0,0 +1,32 @@
+﻿// using System.Diagnostics;
+// using Lab.EnvFileConfig;
+// using Microsoft.Extensions.Configuration;
+//
+// namespace App
+// {
+//     public class Program
+//     {
+//         private static void Main()
+//         {
+//             var initialSettings = new Dictionary<string, string>
+//             {
+//                 ["Gender"] = "Male",
+//                 ["Age"] = "18",
+//                 ["ContactInfo:EmailAddress"] = "foobar@outlook.com",
+//                 ["ContactInfo:PhoneNo"] = "123456789"
+//             };
+//
+//             var profile = new ConfigurationBuilder()
+//                           .AddJsonFile("appSettings.json")
+//                           .AddDatabase("DefaultDb", initialSettings)
+//                           .Build()
+//                           .Get<Profile>();
+//
+//             Debug.Assert(profile.Gender == Gender.Male);
+//             Debug.Assert(profile.Age == 18);
+//             Debug.Assert(profile.ContactInfo.EmailAddress == "foobar@outlook.com");
+//             Debug.Assert(profile.ContactInfo.PhoneNo == "123456789");
+//         }
+//     }
+// }
+
