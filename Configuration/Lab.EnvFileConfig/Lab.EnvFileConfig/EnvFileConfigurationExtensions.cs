2655433d721a6050862d64bf9665959c69819a27
yao <yao-chang.yu@outlook.com>
2022-01-20 00:46:40 +0800

    feat: Env File Config

---
+++ Configuration/Lab.EnvFileConfig/Lab.EnvFileConfig/EnvFileConfigurationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Lab.EnvFileConfig
+{
+    public static class EnvFileConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddEnvFile(this IConfigurationBuilder builder, string envFile)
+        {
+            var source = new EnvFileConfigurationSource(envFile);
+            builder.Add(source);
+            builder.AddEnvironmentVariables();
+            return builder;
+        }
+    }
+}
