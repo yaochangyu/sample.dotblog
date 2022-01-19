8630ea161733d2840c3956d3767bea7707718ffa
yao <yao-chang.yu@outlook.com>
2022-01-19 15:13:16 +0800

    refactor

--- Configuration/Lab.ConfigBind/Lab.ConfigBind.TestProject/來源為字典集合.cs
+++ Configuration/Lab.ConfigBind/Lab.ConfigBind.TestProject/來源為字典集合.cs
@@ -8,6 +8,29 @@ namespace Lab.ConfigBind.TestProject;
 public class 來源為字典集合
 {
     [TestMethod]
+    public void 綁定字典()
+    {
+        var source = new Dictionary<string, string>
+        {
+            ["a:id"] = "9527",
+            ["a:profile:gender"] = "Male",
+            ["a:profile:age"] = "18",
+            ["a:profile:address"] = "Taipei",
+            ["b:id"] = "9528",
+            ["b:profile:gender"] = "Male",
+            ["b:profile:age"] = "19",
+            ["b:profile:address"] = "Taipei",
+        };
+
+        var builder = new ConfigurationBuilder();
+        var configRoot = builder.AddInMemoryCollection(source).Build();
+        var member = configRoot.Get<Dictionary<string, Member>>();
+
+        Assert.AreEqual("9527", member["a"].Id);
+        Assert.AreEqual("9528", member["b"].Id);
+    }
+
+    [TestMethod]
     public void 綁定集合()
     {
         var source = new Dictionary<string, string>
@@ -49,29 +72,6 @@ public class 來源為字典集合
         Assert.AreEqual(18, member.Profile.Age);
         Assert.AreEqual("Taipei", member.Profile.Address);
         Assert.AreEqual(Gender.Male, member.Profile.Gender);
-    }
-
-    [TestMethod]
-    public void 綁定環境變數()
-    {
-        var source = new Dictionary<string, string>
-        {
-            ["a:id"] = "9527",
-            ["a:profile:gender"] = "Male",
-            ["a:profile:age"] = "18",
-            ["a:profile:address"] = "Taipei",
-            ["b:id"] = "9528",
-            ["b:profile:gender"] = "Male",
-            ["b:profile:age"] = "19",
-            ["b:profile:address"] = "Taipei",
-        };
-        var builder = new ConfigurationBuilder();
-        builder.AddEnvironmentVariables();
-        var configRoot = builder.AddInMemoryCollection(source).Build();
-        var member = configRoot.Get<IList<Member>>();
-
-        Assert.AreEqual("9527", member[0].Id);
-        Assert.AreEqual("9528", member[1].Id);
     }
 
     private enum Gender
