34c0f23e0bea84e36aa51655d94dd80bd9d70a70
yao <yao-chang.yu@outlook.com>
2022-01-19 10:14:10 +0800

    feat:字典集合綁定

--- Configuration/Lab.ConfigBind/Lab.ConfigBind.TestProject/來源為字典集合.cs
+++ Configuration/Lab.ConfigBind/Lab.ConfigBind.TestProject/來源為字典集合.cs
@@ -4,31 +4,30 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Lab.ConfigBind.TestProject;
 
-
-
 [TestClass]
-public class UnitTest1
+public class 來源為字典集合
 {
-    public enum Gender
+    [TestMethod]
+    public void 綁定集合()
     {
-        Male,
-        Female
-    }
+        var source = new Dictionary<string, string>
+        {
+            ["a:id"] = "9527",
+            ["a:profile:gender"] = "Male",
+            ["a:profile:age"] = "18",
+            ["a:profile:address"] = "Taipei",
+            ["b:id"] = "9528",
+            ["b:profile:gender"] = "Male",
+            ["b:profile:age"] = "19",
+            ["b:profile:address"] = "Taipei",
+        };
 
-    public class Member
-    {
-        public string Id { get; set; }
+        var builder = new ConfigurationBuilder();
+        var configRoot = builder.AddInMemoryCollection(source).Build();
+        var member = configRoot.Get<IList<Member>>();
 
-        public Profile Profile { get; set; }
-    }
-
-    public class Profile
-    {
-        public Gender? Gender { get; set; }
-
-        public int? Age { get; set; }
-
-        public string Address { get; set; }
+        Assert.AreEqual("9527", member[0].Id);
+        Assert.AreEqual("9528", member[1].Id);
     }
 
     [TestMethod]
@@ -41,39 +40,16 @@ public class UnitTest1
             ["profile:age"] = "18",
             ["profile:address"] = "Taipei",
         };
-        
+
         var builder = new ConfigurationBuilder();
         var configRoot = builder.AddInMemoryCollection(source).Build();
         var member = configRoot.Get<Member>();
-        
+
         Assert.AreEqual("9527", member.Id);
         Assert.AreEqual(18, member.Profile.Age);
         Assert.AreEqual("Taipei", member.Profile.Address);
         Assert.AreEqual(Gender.Male, member.Profile.Gender);
     }
-    [TestMethod]
-        public void 綁定集合()
-        {
-            var source = new Dictionary<string, string>
-            {
-                ["a:id"] = "9527",
-                ["a:profile:gender"] = "Male",
-                ["a:profile:age"] = "18",
-                ["a:profile:address"] = "Taipei",
-                ["b:id"] = "9528",
-                ["b:profile:gender"] = "Male",
-                ["b:profile:age"] = "19",
-                ["b:profile:address"] = "Taipei",
-    
-            };
-            
-            var builder = new ConfigurationBuilder();
-            var configRoot = builder.AddInMemoryCollection(source).Build();
-            var member = configRoot.Get<IList<Member>>();
-            
-            Assert.AreEqual("9527", member[0].Id);
-            Assert.AreEqual("9528", member[1].Id);
-        }
 
     [TestMethod]
     public void 綁定環境變數()
@@ -88,15 +64,35 @@ public class UnitTest1
             ["b:profile:gender"] = "Male",
             ["b:profile:age"] = "19",
             ["b:profile:address"] = "Taipei",
-
         };
         var builder = new ConfigurationBuilder();
         builder.AddEnvironmentVariables();
         var configRoot = builder.AddInMemoryCollection(source).Build();
         var member = configRoot.Get<IList<Member>>();
-        
+
         Assert.AreEqual("9527", member[0].Id);
         Assert.AreEqual("9528", member[1].Id);
     }
- 
+
+    private enum Gender
+    {
+        Male,
+        Female
+    }
+
+    private class Member
+    {
+        public string Id { get; set; }
+
+        public Profile Profile { get; set; }
+    }
+
+    private class Profile
+    {
+        public Gender? Gender { get; set; }
+
+        public int? Age { get; set; }
+
+        public string Address { get; set; }
+    }
 }
