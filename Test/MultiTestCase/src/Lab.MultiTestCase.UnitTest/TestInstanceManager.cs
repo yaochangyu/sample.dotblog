da6e29d6a817243d9697008738e0261b0d5f3e8f
yao <yao-chang.yu@outlook.com>
2022-02-13 20:01:04 +0800

    add project

---
+++ Test/MultiTestCase/src/Lab.MultiTestCase.UnitTest/TestInstanceManager.cs
@@ -0,0 +1,34 @@
+﻿// using System;
+// using Lab.MultiTestCase.EntityModel;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.Extensions.DependencyInjection;
+//
+// namespace Lab.MultiTestCase.UnitTest;
+//
+// internal class TestInstanceManager
+// {
+//     private static IServiceProvider _serviceProvider;
+//
+//     public static IDbContextFactory<EmployeeDbContext> EmployeeDbContextFactory =>
+//         _serviceProvider.GetService<IDbContextFactory<EmployeeDbContext>>();
+//
+//     static TestInstanceManager()
+//     {
+//         var services = new ServiceCollection();
+//         ConfigureTestServices(services);
+//     }
+//
+//     public static void ConfigureTestServices(IServiceCollection services)
+//     {
+//         services.AddAppEnvironment();
+//         services.AddEntityFramework();
+//         _serviceProvider = services.BuildServiceProvider();
+//     }
+//
+//     public static void SetTestEnvironmentVariable()
+//     {
+//         var option = _serviceProvider.GetService<AppEnvironmentOption>();
+//         option.EmployeeDbConnectionString =
+//             "Data Source=localhost;Initial Catalog=EmployeeDb;Integrated Security=false;User ID=sa;Password=pass@w0rd1~;MultipleActiveResultSets=True;TrustServerCertificate=True";
+//     }
+// }
