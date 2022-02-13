cdc61186aeca7c07531759ffdfd0bfef9d8676a9
yao <yao-chang.yu@outlook.com>
2022-02-13 20:14:21 +0800

    feat: add project

---
+++ Property Change Tracking/ChangeTracking/src/Lab.ChangeTracking.Domain/EmployeeAggregate/Entity/Employee.cs
@@ -0,0 +1,29 @@
+﻿namespace Lab.ChangeTracking.Domain.EmployeeAggregate.Entity;
+
+public record Employee
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int? Age { get; set; }
+
+    public string Remark { get; set; }
+
+    public DateTimeOffset CreateAt { get; set; }
+
+    public string CreateBy { get; set; }
+
+    public Employee SetName(string name)
+    {
+        this.Name = name;
+        return this;
+    }
+
+    public Employee SetAge(int age)
+    {
+        this.Age = age;
+        return this;
+    }
+    
+}
