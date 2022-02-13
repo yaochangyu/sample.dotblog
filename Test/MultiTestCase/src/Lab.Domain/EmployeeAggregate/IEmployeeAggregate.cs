dffd83101d4678e8ccca177bdd350c1fdcd96517
yao <yao-chang.yu@outlook.com>
2022-02-13 20:01:04 +0800

    add project

---
+++ Test/MultiTestCase/src/Lab.Domain/EmployeeAggregate/IEmployeeAggregate.cs
@@ -0,0 +1,8 @@
+﻿using Lab.Domain.Entity;
+
+namespace Lab.Domain;
+
+public interface IEmployeeAggregate
+{
+    Employee InsertAsync(Employee employee, CancellationToken cancel = default);
+}
---
+++ Test/MultiTestCase/src/Lab.Domain/EmployeeAggregate/Repository/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Lab.Domain.Entity;
+
+namespace Lab.Domain.Repository;
+
+public interface IEmployeeRepository
+{
+    Task<int> InsertAsync(Employee employee, CancellationToken cancel = default);
+}
+
+class EmployeeRepository : IEmployeeRepository
+{
+    public Task<int> InsertAsync(Employee employee, CancellationToken cancel = default)
+    {
+        throw new NotImplementedException();
+    }
+}
