15aea806db76def14108a11d8157ef8823f4f6d2
yao <yao-chang.yu@outlook.com>
2022-02-17 13:11:24 +0800

    feat: 追蹤異動

--- Property Change Tracking/ChangeTracking/src/Lab.ChangeTracking.Domain.UnitTest/MsTestHook.cs
+++ Property Change Tracking/ChangeTracking/src/Lab.ChangeTracking.Domain.UnitTest/MsTestHook.cs
@@ -1,31 +1,31 @@
-﻿// using Microsoft.VisualStudio.TestTools.UnitTesting;
-//
-// namespace Lab.MultiTestCase.UnitTest;
-//
-// [TestClass]
-// public class MsTestHook
-// {
-//     [AssemblyCleanup]
-//     public static void Cleanup()
-//     {
-//         TestInstanceManager.SetTestEnvironmentVariable();
-//         var db = TestInstanceManager.EmployeeDbContextFactory.CreateDbContext();
-//         if (db.Database.CanConnect())
-//         {
-//             db.Database.EnsureDeleted();
-//         }
-//     }
-//
-//     [AssemblyInitialize]
-//     public static void Setup(TestContext context)
-//     {
-//         TestInstanceManager.SetTestEnvironmentVariable();
-//         var db = TestInstanceManager.EmployeeDbContextFactory.CreateDbContext();
-//         if (db.Database.CanConnect())
-//         {
-//             db.Database.EnsureDeleted();
-//         }
-//
-//         db.Database.EnsureCreated();
-//     }
-// }
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab.MultiTestCase.UnitTest;
+
+[TestClass]
+public class MsTestHook
+{
+    [AssemblyCleanup]
+    public static void Cleanup()
+    {
+        TestAssistants.SetTestEnvironmentVariable();
+        var db = TestAssistants.EmployeeDbContextFactory.CreateDbContext();
+        if (db.Database.CanConnect())
+        {
+            db.Database.EnsureDeleted();
+        }
+    }
+
+    [AssemblyInitialize]
+    public static void Setup(TestContext context)
+    {
+        TestAssistants.SetTestEnvironmentVariable();
+        var db = TestAssistants.EmployeeDbContextFactory.CreateDbContext();
+        if (db.Database.CanConnect())
+        {
+            db.Database.EnsureDeleted();
+        }
+
+        db.Database.EnsureCreated();
+    }
+}
