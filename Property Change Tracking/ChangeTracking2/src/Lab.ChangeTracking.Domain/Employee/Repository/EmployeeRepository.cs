bf5d823d707491dafdc43aef806cc1a3498af6c9
yao <yao-chang.yu@outlook.com>
2022-03-06 01:10:28 +0800

    feat: 狀態追蹤套用 EF Core

---
+++ Property Change Tracking/ChangeTracking2/src/Lab.ChangeTracking.Domain/Employee/Repository/EmployeeRepository.cs
@@ -0,0 +1,30 @@
+﻿using Lab.ChangeTracking.Infrastructure.DB.EntityModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab.ChangeTracking.Domain;
+
+public class EmployeeRepository : RepositoryBase, IEmployeeRepository
+{
+    private readonly IDbContextFactory<EmployeeDbContext> _memberContextFactory;
+
+    public EmployeeRepository(IDbContextFactory<EmployeeDbContext> memberContextFactory)
+    {
+        this._memberContextFactory = memberContextFactory;
+    }
+
+    public async Task<int> SaveChangeAsync(EmployeeEntity srcEmployee,
+                                           CancellationToken cancel = default)
+    {
+        await using var dbContext = await this._memberContextFactory.CreateDbContextAsync(cancel);
+        var destEmployee = srcEmployee.To();
+        this.ApplyChange(dbContext, srcEmployee, destEmployee, new List<string>
+            {
+                "Identity",
+                "Addresses"
+            }
+        );
+        this.ApplyChange(dbContext, srcEmployee.Identity, destEmployee.Identity);
+        this.ApplyChanges(dbContext, srcEmployee.Addresses, destEmployee.Addresses);
+        return await dbContext.SaveChangesAsync(cancel);
+    }
+}
