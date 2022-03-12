6a94eb0a1747f8edaf129fd0be0af3e53803ac42
yao <yao-chang.yu@outlook.com>
2022-03-12 12:24:34 +0800

    add swagger doc project

---
+++ WebAPI/Swagger/Lab.SwaggerDoc/Lab.Swashbuckle.AspNetCore6/Program.cs
@@ -0,0 +1,67 @@
+using System.Reflection;
+using System.Xml.XPath;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+void IncludeXmlComments(Assembly assembly, SwaggerGenOptions swaggerGenOptions)
+{
+    var directory = AppDomain.CurrentDomain.BaseDirectory;
+    if (assembly != null)
+    {
+        foreach (var name in assembly.GetManifestResourceNames()
+                     .Where(x => x.ToUpper()
+                                .EndsWith(".XML"))
+                )
+        {
+            try
+            {
+                var xPath = new XPathDocument(assembly.GetManifestResourceStream(name));
+                swaggerGenOptions.IncludeXmlComments((Func<XPathDocument>)(() => xPath));
+            }
+            catch
+            {
+            }
+        }
+    }
+
+    if (string.IsNullOrEmpty(directory))
+    {
+        return;
+    }
+
+    foreach (var file in Directory.GetFiles(directory, "*.XML", SearchOption.AllDirectories))
+    {
+        swaggerGenOptions.IncludeXmlComments(file);
+    }
+}
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    var assembly = typeof(Program).Assembly;
+
+    IncludeXmlComments(assembly, c);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ WebAPI/Swagger/Lab.SwaggerDoc/Lab.Swashbuckle.AspNetCore6/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Lab.Swashbuckle.AspNetCore6;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(this.TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
