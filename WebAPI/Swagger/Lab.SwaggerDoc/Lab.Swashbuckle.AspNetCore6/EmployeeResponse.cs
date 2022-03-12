b763c2bba3f55cfec9ddc1a4476e6588c3a4afa8
yao <yao-chang.yu@outlook.com>
2022-03-12 17:40:40 +0800

    refactor

---
+++ WebAPI/Swagger/Lab.SwaggerDoc/Lab.Swashbuckle.AspNetCore6/EmployeeResponse.cs
@@ -0,0 +1,12 @@
+namespace Lab.Swashbuckle.AspNetCore6;
+
+public class EmployeeResponse
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public string Remark { get; set; }
+}
---
+++ WebAPI/Swagger/Lab.SwaggerDoc/Lab.Swashbuckle.AspNetCore6/QueryEmployeeRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab.Swashbuckle.AspNetCore6;
+
+public class QueryEmployeeRequest
+{
+    /// <summary>
+    /// 姓名
+    /// </summary>
+    /// <example>小章</example>
+    [Required]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 年齡
+    /// </summary>
+    /// <example>18</example>
+    public int Age { get; set; }
+}
