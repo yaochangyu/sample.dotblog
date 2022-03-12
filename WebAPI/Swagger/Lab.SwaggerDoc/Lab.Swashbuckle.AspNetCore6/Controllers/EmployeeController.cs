c1b795fb1a987b27226115c8f1cbe2ba3171def8
yao <yao-chang.yu@outlook.com>
2022-03-12 17:40:40 +0800

    refactor

---
+++ WebAPI/Swagger/Lab.SwaggerDoc/Lab.Swashbuckle.AspNetCore6/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.Swashbuckle.AspNetCore6.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EmployeeController : ControllerBase
+{
+    private static readonly string[] Summaries =
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<EmployeeController> _logger;
+
+    public EmployeeController(ILogger<EmployeeController> logger)
+    {
+        this._logger = logger;
+    }
+
+    // [HttpGet(Name = "GetEmployee")]
+    [HttpGet]
+    public async Task<IActionResult> Get(QueryEmployeeRequest request)
+    {
+        if (this.ModelState.IsValid == false)
+        {
+            return this.BadRequest();
+        }
+
+        return this.Ok(new List<EmployeeResponse>
+        {
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "yao",
+                Age = 20
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "小章",
+                Age = 18,
+                Remark = "說明"
+            }
+        });
+    }
+}
--- WebAPI/Swagger/Lab.SwaggerDoc/Lab.Swashbuckle.AspNetCore6/Program.cs
+++ WebAPI/Swagger/Lab.SwaggerDoc/Lab.Swashbuckle.AspNetCore6/Program.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System.Xml.XPath;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 void IncludeXmlComments(Assembly assembly, SwaggerGenOptions swaggerGenOptions)
@@ -42,11 +43,28 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+builder.Services.AddSwaggerGen(options =>
 {
-    var assembly = typeof(Program).Assembly;
-
-    IncludeXmlComments(assembly, c);
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+    
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
 
 var app = builder.Build();
