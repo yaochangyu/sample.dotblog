5769ad027b233154982cf478bc2b06fb36bd3198
yao <yao-chang.yu@outlook.com>
2022-05-22 20:19:15 +0800

    feat: 訪問不需要授權的服務

--- WebAPI/Security/Lab.AspNetCore.Security/Lab.AspNetCore.Security.BasicAuthenticationSite/Controllers/DemoController.cs
+++ WebAPI/Security/Lab.AspNetCore.Security/Lab.AspNetCore.Security.BasicAuthenticationSite/Controllers/DemoController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lab.AspNetCore.Security.BasicAuthenticationSite.Controllers;
@@ -13,6 +14,7 @@ public class DemoController : ControllerBase
         this._logger = logger;
     }
 
+    [AllowAnonymous]
     public async Task<IActionResult> Get()
     {
         return this.Ok("好");
