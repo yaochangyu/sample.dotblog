1edfd775e28110790bfbf1d8fb4d069478b54624
yao <yao-chang.yu@outlook.com>
2022-06-18 23:02:04 +0800

    feat: add unit test

---
+++ WebAPI/Security/Lab.AspNetCore.Security/Lab.AspNetCore.Security.BasicAuthenticationSite.IntegrateTest/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Lab.AspNetCore.Security.BasicAuthenticationSite.IntegrateTest.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    private readonly ILogger<TestController> _logger;
+
+    public TestController(ILogger<TestController> logger)
+    {
+        this._logger = logger;
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return this.Ok("好");
+    }
+}
