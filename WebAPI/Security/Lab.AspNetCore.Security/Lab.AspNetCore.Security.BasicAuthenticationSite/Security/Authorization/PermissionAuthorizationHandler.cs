b65a67b818e4d63cf385ac6817cc8597cc5449b4
yao <yao-chang.yu@outlook.com>
2022-06-20 21:56:37 +0800

    fix

---
+++ WebAPI/Security/Lab.AspNetCore.Security/Lab.AspNetCore.Security.BasicAuthenticationSite/Security/Authorization/PermissionAuthorizationHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Lab.AspNetCore.Security.BasicAuthenticationSite.Security.Authorization;
+
+public class PermissionAuthorizationHandler : AuthorizationHandler<PermissionAuthorizationRequirement>
+{
+
+    public override Task HandleAsync(AuthorizationHandlerContext context)
+    {
+        return base.HandleAsync(context);
+    }
+
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context,
+        PermissionAuthorizationRequirement requirement)
+    {
+        if (context.User.Identity.IsAuthenticated==false)
+        {
+            
+            // return Task.FromResult(false);
+        }
+
+        
+
+        // Task.FromResult(false);
+    }
+    
+}
---
+++ WebAPI/Security/Lab.AspNetCore.Security/Lab.AspNetCore.Security.BasicAuthenticationSite/Security/Authorization/PermissionAuthorizationMiddlewareResultHandler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+
+namespace Lab.AspNetCore.Security.BasicAuthenticationSite.Security.Authorization;
+
+public class PermissionAuthorizationMiddlewareResultHandler : IAuthorizationMiddlewareResultHandler
+{
+    public async Task HandleAsync(RequestDelegate next, 
+        HttpContext context,
+        AuthorizationPolicy policy,
+        PolicyAuthorizationResult authorizeResult)
+    {
+        await next(context);
+    }
+}
