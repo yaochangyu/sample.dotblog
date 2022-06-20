80b3c9ae694a6f6593f292885676f38738543726
yao <yao-chang.yu@outlook.com>
2022-06-20 21:56:37 +0800

    fix

---
+++ WebAPI/Security/Lab.AspNetCore.Security/Lab.AspNetCore.Security.BasicAuthenticationSite/Security/Authorization/PermissionAuthorizationRequirement.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Authorization;
+
+namespace Lab.AspNetCore.Security.BasicAuthenticationSite.Security.Authorization;
+
+public class PermissionAuthorizationRequirement : IAuthorizationRequirement
+{
+}
