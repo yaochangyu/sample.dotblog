5a669bd52128b839e1873a8b6376c3dcb242d80b
yao <yao-chang.yu@outlook.com>
2022-06-21 23:39:49 +0800

    refactor

--- WebAPI/Security/Lab.AspNetCore.Security/Lab.AspNetCore.Security.BasicAuthenticationSite/Security/Authorization/Permission.cs
+++ WebAPI/Security/Lab.AspNetCore.Security/Lab.AspNetCore.Security.BasicAuthenticationSite/Security/Authorization/Permission.cs
@@ -4,8 +4,8 @@ public class Permission
 {
     public class Operation
     {
-        public const string Write = $"{nameof(Operation)}.{nameof(Write)}";
-        public const string Read = $"{nameof(Operation)}.{nameof(Read)}";
+        public const string Write = $"{nameof(Permission)}.{nameof(Operation)}:{nameof(Write)}";
+        public const string Read = $"{nameof(Permission)}.{nameof(Operation)}:{nameof(Read)}";
 
         private static readonly Lazy<Dictionary<string, Type>> s_values
             = new(() =>
