1f325465b84a87275350dc29f9ee1dec2b312dff
yao <yao-chang.yu@outlook.com>
2022-06-26 23:21:04 +0800

    feat: add other temp

---
+++ Template/Lab.RazorTemplate/Lab.RazorTemplate/EnvironmentType.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab.RazorTemplate;
+
+public class EnvironmentName
+{
+    public const string Dev = "Dev";
+    public const string Test = "Test";
+    public const string QA = "QA";
+    public const string Production = "Prod";
+}
