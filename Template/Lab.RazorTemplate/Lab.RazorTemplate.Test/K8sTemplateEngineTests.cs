2d7cf36b2bebac8b19fc00fa406e1c7223085372
yao <yao-chang.yu@outlook.com>
2022-06-26 23:21:04 +0800

    feat: add other temp

--- Template/Lab.RazorTemplate/Lab.RazorTemplate.Test/K8sTemplateEngineTests.cs
+++ Template/Lab.RazorTemplate/Lab.RazorTemplate.Test/K8sTemplateEngineTests.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Text;
+
 namespace Lab.RazorTemplate.Test;
 
 [TestClass]
@@ -21,6 +24,33 @@ public class K8sTemplateEngineTests
             ["Value2"] = "2",
             ["K8S_COMMON_SERVICE_NAME"] = "3",
         };
+
+        var engine = new K8sTemplateEngine();
+        var result = await engine.RenderAsync(templatePath, k8sValue, k8sDynamicValues);
+        Console.WriteLine(result);
+    }
+
+    [TestMethod]
+    public async Task 替換範本aa()
+    {
+        var templatePath = "EnvTemplate.cshtml";
+        var k8sValue = new K8sValue()
+        {
+            Common = new Common
+            {
+                ProjectName = "member-service-api",
+                Namespace = "member-service",
+            },
+        };
+        var k8sDynamicValues = new Dictionary<string, object>
+        {
+            ["Market"] = "TW",
+            ["Environment"] = "Dev",
+        };
+        var response = new HttpResponseMessage();
+        response.StatusCode = HttpStatusCode.NoContent;
+        response.Content = new StringContent("[]", Encoding.UTF8);
+
         var engine = new K8sTemplateEngine();
         var result = await engine.RenderAsync(templatePath, k8sValue, k8sDynamicValues);
         Console.WriteLine(result);
---
+++ Template/Lab.RazorTemplate/Lab.RazorTemplate/MarketType.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab.RazorTemplate;
+
+public class MarketName
+{
+    public const string TW = "TW";
+    public const string MY = "MY";
+    public const string HK = "HK";
+}
