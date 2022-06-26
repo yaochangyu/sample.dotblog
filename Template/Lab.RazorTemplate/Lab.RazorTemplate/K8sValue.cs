3499b66d07fb4d1e00582697039d2b2a73201eb7
yao <yao-chang.yu@outlook.com>
2022-06-26 13:53:00 +0800

    feat: 套用範本

---
+++ Template/Lab.RazorTemplate/Lab.RazorTemplate/K8sValue.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab.RazorTemplate;
+
+public class K8sValue
+{
+    public Common Common { get; set; }
+
+    public Resource Resource { get; set; }
+}
+
+public class Common
+{
+    public string ProjectName { get; set; }
+
+    public string Namespace { get; set; }
+}
+
+public class Resource
+{
+    public uint CPU { get; set; }
+
+    public uint Memory { get; set; }
+}
