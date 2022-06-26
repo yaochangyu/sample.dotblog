0bbc9efa59f3c943d93b9746224a14e78c1cda30
yao <yao-chang.yu@outlook.com>
2022-06-26 13:53:00 +0800

    feat: 套用範本

---
+++ Template/Lab.RazorTemplate/Lab.RazorTemplate/K8sTemplateEngine.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace Lab.RazorTemplate;
+
+public class K8sTemplateEngine
+{
+    public async Task<string> RenderAsync(string templatePath,
+        K8sValue k8sValue,
+        Dictionary<string, object> k8sDynamicValue)
+    {
+        return await Razor.Templating.Core.RazorTemplateEngine.RenderAsync(templatePath,
+            k8sValue,
+            k8sDynamicValue);
+    }
+}
