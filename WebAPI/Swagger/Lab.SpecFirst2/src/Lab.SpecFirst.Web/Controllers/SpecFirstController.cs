13d3994c8313339d16d589f597592d38d3c2d82d
yao <yao-chang.yu@outlook.com>
2022-07-19 00:59:26 +0800

    from fork lab.specfirst

---
+++ WebAPI/Swagger/Lab.SpecFirst2/src/Lab.SpecFirst.Web/Controllers/SpecFirstController.cs
@@ -0,0 +1,38 @@
+﻿namespace Lab.SpecFirst.Web.Controllers;
+
+class SpecFirstController : ISpecFirstContractController
+{
+    public async Task<ICollection<Pet>> ListPetsAsync(int? limit)
+    {
+        return new List<Pet>()
+        {
+            new()
+            {
+                Id = 1,
+                Name = "yao",
+                Tag = "dog",
+                AdditionalProperties = new Dictionary<string, object>()
+                {
+                }
+            },
+        };
+    }
+
+    public async Task CreatePetsAsync()
+    {
+        
+    }
+
+    public async Task<Pet> ShowPetByIdAsync(string petId)
+    {
+        return new()
+        {
+            Id = 1,
+            Name = "yao",
+            Tag = "dog",
+            AdditionalProperties = new Dictionary<string, object>()
+            {
+            }
+        };
+    }
+}
