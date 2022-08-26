af30d60b739800e34ba3a56f14de48f9c73d6248
yao <yao-chang.yu@outlook.com>
2022-08-27 00:20:11 +0800

    切換設定

--- Host/Lab.Host.Env/src/Lab.Host.Env.WebApi/Program.cs
+++ Host/Lab.Host.Env/src/Lab.Host.Env.WebApi/Program.cs
@@ -1,13 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
+var environmentName = builder.Environment.EnvironmentName;
+var configRoot = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
+    ;
+
+builder.Configuration.AddConfiguration(configRoot);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +26,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+var extension = app.Configuration.GetSection("Extension:Version");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
