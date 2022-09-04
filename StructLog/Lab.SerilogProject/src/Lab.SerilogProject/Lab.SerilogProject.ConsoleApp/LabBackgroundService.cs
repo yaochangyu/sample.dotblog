79a8635c24b11d609acaef5281a2d3ba6963e08c
yao <yao-chang.yu@outlook.com>
2022-09-04 15:07:59 +0800

    add console project

---
+++ StructLog/Lab.SerilogProject/src/Lab.SerilogProject/Lab.SerilogProject.ConsoleApp/LabBackgroundService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Lab.SerilogProject.ConsoleApp;
+
+public class LabBackgroundService : BackgroundService
+{
+    private readonly ILogger<LabBackgroundService> _logger;
+
+    public LabBackgroundService(ILogger<LabBackgroundService> logger)
+    {
+        this._logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        this._logger.LogInformation(new EventId(2000, "Trace"), "Start {ClassName}.{MethodName}...",
+            nameof(LabBackgroundService), nameof(this.ExecuteAsync));
+
+        var sensorInput = new { Latitude = 25, Longitude = 134 };
+        this._logger.LogInformation("Processing {@SensorInput}", sensorInput);
+    }
+}
