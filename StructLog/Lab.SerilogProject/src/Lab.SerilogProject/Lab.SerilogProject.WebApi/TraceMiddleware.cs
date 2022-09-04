b044daceb6961466e0fb7bd3d5a57306da5d11bb
yao <yao-chang.yu@outlook.com>
2022-09-04 13:12:21 +0800

    add TraceMiddleware

---
+++ StructLog/Lab.SerilogProject/src/Lab.SerilogProject/Lab.SerilogProject.WebApi/TraceMiddleware.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab.SerilogProject.WebApi;
+
+public class TraceMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public TraceMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context, ILogger<TraceMiddleware> logger)
+    {
+        using (logger.BeginScope(new Dictionary<string, object>
+        {
+            ["UserId"] = "svrooij",
+            ["OperationType"] = "update",
+        }))
+        {
+            await this._next.Invoke(context);
+        }
+    
+        // using (logger.BeginScope("{_rid}", Guid.NewGuid()))
+        // {
+        //     await this._next.Invoke(context);
+        // }
+    }
+}
