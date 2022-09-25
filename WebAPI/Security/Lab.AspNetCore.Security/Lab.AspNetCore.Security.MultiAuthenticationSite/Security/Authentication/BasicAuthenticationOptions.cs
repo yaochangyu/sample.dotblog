54bf38e2716b04236ce988945fc41fe849108201
yao <yao-chang.yu@outlook.com>
2022-09-25 21:34:22 +0800

    feat: add project

---
+++ WebAPI/Security/Lab.AspNetCore.Security/Lab.AspNetCore.Security.MultiAuthenticationSite/Security/Authentication/BasicAuthenticationOptions.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Lab.AspNetCore.Security.MultiAuthenticationSite.Security.Authentication;
+
+public class BasicAuthenticationOptions : AuthenticationSchemeOptions
+{
+    public string Realm { get; set; }
+}
---
+++ WebAPI/Security/Lab.AspNetCore.Security/Lab.AspNetCore.Security.MultiAuthenticationSite/Security/Authentication/IBasicAuthenticationProvider.cs
@@ -0,0 +1,6 @@
+﻿namespace Lab.AspNetCore.Security.MultiAuthenticationSite.Security.Authentication;
+
+public interface IBasicAuthenticationProvider
+{
+    Task<bool> IsValidateAsync(string user, string password, CancellationToken cancel);
+}
