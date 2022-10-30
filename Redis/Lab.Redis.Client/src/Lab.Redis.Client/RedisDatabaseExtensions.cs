a8e2287cc30b80ccdc61f74433dbd319a4fb6c6b
yao <yao-chang.yu@outlook.com>
2022-10-30 11:59:00 +0800

    增加Connection Pool

---
+++ Redis/Lab.Redis.Client/src/Lab.Redis.Client/RedisDatabaseExtensions.cs
@@ -0,0 +1,30 @@
+using System.Text.Json;
+using StackExchange.Redis;
+
+namespace TestProject1;
+
+public static class RedisDatabaseExtensions
+{
+    public static bool IsExist(this IDatabase db, string key)
+    {
+        return db.KeyExists(key);
+    }
+
+    public static void Set<T>(this IDatabase db, string key, T value,
+        TimeSpan? expiry = default,
+        When when = When.Always,
+        CommandFlags flags = CommandFlags.None)
+    {
+        db.StringSet(key, JsonSerializer.Serialize(value), expiry, when, flags);
+    }
+
+    public static T Get<T>(this IDatabase db, string key)
+    {
+        if (db.IsExist(key))
+        {
+            return JsonSerializer.Deserialize<T>(db.StringGet(key));
+        }
+
+        return default;
+    }
+}
