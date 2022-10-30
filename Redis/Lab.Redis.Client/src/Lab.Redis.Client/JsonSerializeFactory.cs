2b38ca33b9bbaa2300daf22802682533cf85da8b
yao <yao-chang.yu@outlook.com>
2022-10-30 15:53:32 +0800

    refactor

---
+++ Redis/Lab.Redis.Client/src/Lab.Redis.Client/JsonSerializeFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
+
+namespace Lab.Redis.Client;
+
+public class JsonSerializeFactory
+{
+    public static JsonSerializerOptions CreateDefault()
+    {
+        return new JsonSerializerOptions
+        {
+            Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.CjkUnifiedIdeographs),
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        };
+    }
+}
