4d74ea9e1118ace7c4bbbcab800183938ae2b3f4
yao <yao-chang.yu@outlook.com>
2022-12-11 11:16:14 +0800

    feat: 加入 Context DI Container Project

---
+++ Test/Specflow3/Lab.StepDependencyInjection/TestProject1Lab.StepDependencyInjection.Test/Features/Demo1.feature
@@ -0,0 +1,5 @@
+﻿Feature: Demo1
+
+    Scenario: 測試步驟注入
+        When 取得檔案路徑
+        Then 預期得到 "File Provider"
---
+++ Test/Specflow3/Lab.StepDependencyInjection/TestProject1Lab.StepDependencyInjection.Test/Steps/Demo1Steps.cs
@@ -0,0 +1,36 @@
+﻿using BoDi;
+using TechTalk.SpecFlow;
+
+namespace TestProject1Lab.StepDependencyInjection.Test.Steps;
+
+[Binding]
+[Scope(Feature = "Demo1")]
+public class Demo1Steps : TechTalk.SpecFlow.Steps
+{
+    private readonly IObjectContainer objectContainer;
+
+    private ScenarioContext ScenarioContext { get; }
+
+    public Demo1Steps(IObjectContainer objectContainer,
+        ScenarioContext scenarioContext)
+    {
+        objectContainer.RegisterInstanceAs(new FileProvider(), nameof(FileProvider));
+        this.objectContainer = objectContainer;
+        this.ScenarioContext = scenarioContext;
+    }
+
+    [When(@"取得檔案路徑")]
+    public void When取得檔案路徑()
+    {
+        var target = this.objectContainer.Resolve<FileProvider>(nameof(FileProvider));
+        var path = target.GetPath();
+        this.ScenarioContext.Set(path, "actual");
+    }
+
+    [Then(@"預期得到 ""(.*)""")]
+    public void Then預期得到(string expected)
+    {
+        var actual = this.ScenarioContext.Get<string>("actual");
+        Assert.AreEqual(expected, actual);
+    }
+}
