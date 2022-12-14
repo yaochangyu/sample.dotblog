de3c92840610527d156a11554fa025c33fb19d22
yao <yao-chang.yu@outlook.com>
2022-12-14 22:35:40 +0800

    add project

---
+++ Test/Specflow3/Lab.StepDependencyInjectionAndDbContext/Lab.StepDependencyInjection.Test/EntityModel/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab.StepDependencyInjection.Test.EntityModel
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int? Age { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long SequenceId { get; set; }
+
+        public string Remark { get; set; }
+
+        [Required]
+        public DateTimeOffset CreateAt { get; set; }
+
+        [Required]
+        public string CreateBy { get; set; }
+    }
+}
