c22cf01c1361f57f97ed4956f35e37de633a24be
yao <yao-chang.yu@outlook.com>
2022-12-14 22:35:40 +0800

    add project

---
+++ Test/Specflow3/Lab.StepDependencyInjectionAndDbContext/Lab.StepDependencyInjection.Test/InstanceManager.cs
@@ -0,0 +1,42 @@
+﻿using Lab.StepDependencyInjection.Test.EntityModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Lab.StepDependencyInjection.Test;
+
+internal class InstanceManager
+{
+    public static IServiceCollection InitializeServices()
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton<FileProvider>();
+
+        services.AddLogging(p => p.AddConsole());
+        services.AddDbContextFactory<EmployeeDbContext>((p, options) =>
+        {
+            var loggerFactory = p.GetService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger("測試");
+            logger.LogInformation("選用測試專案的的注入設定");
+            var connectionString =
+                "Host=localhost;Port=5432;Database=employee_test;Username=postgres;Password=guest;";
+
+            // var connectionString = sp.GetService<MEMBER_SERVICE_DB_CONN_STR>().Value;
+            options.UseNpgsql(connectionString, //只會呼叫一次
+                    builder => builder.EnableRetryOnFailure(
+                        10,
+                        TimeSpan.FromSeconds(30),
+                        new List<string> { "57P01" })
+                )
+
+                //  .UseLazyLoadingProxies()
+                .UseSnakeCaseNamingConvention()
+                .EnableSensitiveDataLogging()
+                .UseLoggerFactory(loggerFactory)
+                ;
+
+            //.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        });
+        return services;
+    }
+}
---
+++ Test/Specflow3/Lab.StepDependencyInjectionAndDbContext/Lab.StepDependencyInjection.Test/SysFileProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab.StepDependencyInjection.Test;
+
+public class SysFileProvider
+{
+    private readonly string _name;
+
+    public SysFileProvider(string name)
+    {
+        this._name = name;
+    }
+
+    public string GetPath()
+    {
+        return $"{this._name}:SysFileProvider";
+    }
+}
