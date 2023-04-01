82b51167707a17527217eebdb2cd9acf97ae5143
yao <yao-chang.yu@outlook.com>
2023-04-01 12:40:15 +0800

    add project

---
+++ Jetbrains HttpClient/Jetbrains HttpClient/WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+
+    [HttpGet]
+    [Route("get:id")]
+    public ActionResult GetId()
+    {
+        return this.Ok(new
+        {
+            Id = new Random().Next(1, 5)
+        });
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public ActionResult Get(int id)
+    {
+        var data = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Id = index,
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        var result = data.FirstOrDefault(p => p.Id == id);
+        return this.Ok(result);
+    }
+}
