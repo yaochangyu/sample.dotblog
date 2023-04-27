222544e1fa65e2f3b5fdcf7e3db70846fff3258b
yao <yao-chang.yu@outlook.com>
2023-04-27 13:33:01 +0800

    add project

---
+++ Args/Lab.SpectreConsole/Lab.SpectreConsole/CancellableAsyncCommand.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.Logging;
+using Spectre.Console.Cli;
+
+namespace Lab.SpectreConsole;
+
+public abstract class CancellableAsyncCommand<TSettings> : AsyncCommand<TSettings>
+    where TSettings : CommandSettings
+{
+    private readonly ILogger<CancellableAsyncCommand<TSettings>> _logger;
+
+    protected CancellableAsyncCommand(ILogger<CancellableAsyncCommand<TSettings>> logger)
+    {
+        this._logger = logger;
+    }
+
+    public abstract Task<int> ExecuteAsync(CommandContext context, TSettings settings, CancellationToken cancellation);
+
+    public override async Task<int> ExecuteAsync(CommandContext context, TSettings settings)
+    {
+        using var cancellationSource = new CancellationTokenSource();
+
+        Console.CancelKeyPress += OnCancelKeyPress;
+        AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
+
+        using var _ = cancellationSource.Token.Register(
+            () =>
+            {
+                AppDomain.CurrentDomain.ProcessExit -= OnProcessExit;
+                Console.CancelKeyPress -= OnCancelKeyPress;
+            }
+        );
+        int exitCode = -1;
+        try
+        {
+            this._logger.LogInformation("執行任務中...");
+            var executeTask = this.ExecuteAsync(context, settings, cancellationSource.Token);
+            exitCode = await executeTask;
+            this._logger.LogInformation("執行完成!!!");
+            AppDomain.CurrentDomain.ProcessExit -= OnProcessExit;
+            Console.CancelKeyPress -= OnCancelKeyPress;
+        }
+        catch (OperationCanceledException)
+        {
+            exitCode = 0;
+        }
+        catch (Exception e)
+        {
+            this._logger.LogError(e, "執行命令時發生非預期的錯誤");
+        }
+
+        return exitCode;
+
+        void OnCancelKeyPress(object? sender, ConsoleCancelEventArgs e)
+        {
+            // NOTE: cancel event, don't terminate the process
+            e.Cancel = true;
+
+            cancellationSource.Cancel();
+        }
+
+        void OnProcessExit(object? sender, EventArgs e)
+        {
+            if (cancellationSource.IsCancellationRequested)
+            {
+                // NOTE: SIGINT (cancel key was pressed, this shouldn't ever actually hit however, as we remove the event handler upon cancellation of the `cancellationSource`)
+                return;
+            }
+
+            cancellationSource.Cancel();
+        }
+    }
+}
---
+++ Args/Lab.SpectreConsole/Lab.SpectreConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Lab.SpectreConsole;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Templates;
+using Spectre.Console.Extensions.Hosting;
+
+// var formatter = new CompactJsonFormatter();
+var formatter = new ExpressionTemplate(
+    "{ {_t: @t, _msg: @m, _props: @p} }\n");
+Log.Logger = new LoggerConfiguration()
+    // .MinimumLevel.Information()
+    .Enrich.FromLogContext()
+    .WriteTo.Console(formatter)
+    .WriteTo.File(formatter, "logs/host-.txt", rollingInterval: RollingInterval.Hour)
+    .CreateBootstrapLogger();
+
+var currentDomain = AppDomain.CurrentDomain;
+currentDomain.UnhandledException += (_, eventArgs) =>
+{
+    var e = (Exception)eventArgs.ExceptionObject;
+    Log.Error(e, "執行命令時發生非預期的錯誤");
+};
+
+try
+{
+    Log.Information("程序開始");
+    await Host.CreateDefaultBuilder(args)
+            .UseSerilog()
+            .UseConsoleLifetime()
+            .UseSpectreConsole(config => { config.AddCommand<FileSizeAsyncCommand>("filesize"); })
+            .RunConsoleAsync()
+        ;
+    Console.WriteLine("程序結束");
+    return Environment.ExitCode;
+}
+catch (Exception e)
+{
+    Log.Error(e, "執行命令時發生非預期的錯誤");
+    return -1;
+}
