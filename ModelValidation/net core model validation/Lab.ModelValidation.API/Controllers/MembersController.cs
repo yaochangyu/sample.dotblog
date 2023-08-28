71c5f18ef99f212413e365cb0868541bc11de7de
yao <yao-chang.yu@outlook.com>
2023-08-28 08:54:24 +0800

    自訂驗證錯誤回傳訊息

---
+++ ModelValidation/net core model validation/Lab.ModelValidation.API/Controllers/MembersController.cs
@@ -0,0 +1,35 @@
+﻿using Lab.ModelValidation.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.ModelValidation.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MembersController : GenericController
+{
+    private readonly ILogger<MembersController> _logger;
+
+    public MembersController(ILogger<MembersController> logger)
+    {
+        this._logger = logger;
+    }
+
+    [HttpPost(Name = "CreateData")]
+    public ActionResult Post(CreateMemberRequest request)
+    {
+        return this.NoContent();
+    }
+
+    [HttpGet("{id}", Name = "GetData")]
+    public ActionResult Get(int id)
+    {
+        var service = new MemberService();
+        var (failure, _) = service.GetMember(id);
+        if (failure != null)
+        {
+            return this.GenericFailure(failure);
+        }
+
+        return this.NoContent();
+    }
+}
