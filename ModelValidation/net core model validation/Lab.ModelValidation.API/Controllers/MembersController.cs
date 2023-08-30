8b591342077549b6a993d717b5f61dc5ff75fc0c
yao <yao-chang.yu@outlook.com>
2023-08-30 13:56:22 +0800

    在 member service 增加驗證

--- ModelValidation/net core model validation/Lab.ModelValidation.API/Controllers/MembersController.cs
+++ ModelValidation/net core model validation/Lab.ModelValidation.API/Controllers/MembersController.cs
@@ -1,4 +1,4 @@
-﻿using Lab.ModelValidation.API.Filters;
+﻿using FluentValidation;
 using Lab.ModelValidation.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,23 +9,36 @@ namespace Lab.ModelValidation.API.Controllers;
 public class MembersController : GenericController
 {
     private readonly ILogger<MembersController> _logger;
+    private readonly IValidator<CreateMemberRequest> _validator;
+    private readonly MemberService _memberService;
 
-    public MembersController(ILogger<MembersController> logger)
+    public MembersController(ILogger<MembersController> logger,
+        IValidator<CreateMemberRequest> validator,
+        MemberService memberService)
     {
         this._logger = logger;
+        this._validator = validator;
+        this._memberService = memberService;
     }
 
     // [ModelValidation()]
     [HttpPost(Name = "CreateData")]
-    public ActionResult Post(CreateMemberRequest request)
+    public async Task<ActionResult> Post(CreateMemberRequest request,
+        CancellationToken cancel)
     {
+        var createMemberResult = await this._memberService.CreateMemberAsync(request, cancel);
+        if (createMemberResult.Failure != null)
+        {
+            return this.GenericFailure(createMemberResult.Failure);
+        }
+
         return this.NoContent();
     }
 
     [HttpGet("{id}", Name = "GetData")]
     public ActionResult Get(int id)
     {
-        var service = new MemberService();
+        var service = new MemberServiceTemp();
         var (failure, _) = service.GetMember(id);
         if (failure != null)
         {
