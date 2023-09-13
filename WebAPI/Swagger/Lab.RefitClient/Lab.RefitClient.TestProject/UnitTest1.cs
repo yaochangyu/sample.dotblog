9fd98dbc7d4fae10306e94f3b9523433c985bd66
yao <yao-chang.yu@outlook.com>
2023-09-13 09:04:02 +0800

    移除 header

--- WebAPI/Swagger/Lab.RefitClient/Lab.RefitClient.TestProject/UnitTest1.cs
+++ WebAPI/Swagger/Lab.RefitClient/Lab.RefitClient.TestProject/UnitTest1.cs
@@ -7,6 +7,17 @@ using Refit;
 
 namespace Lab.RefitClient.TestProject;
 
+public static class HttpClientExtensions
+{
+    public static void AddHeaders(this HttpRequestMessage requestMessage, Dictionary<string, string> headers)
+    {
+        foreach (var header in headers)
+        {
+            requestMessage.Headers.Add(header.Key, header.Value);
+        }
+    }
+}
+
 [TestClass]
 public class UnitTest1
 {
@@ -15,9 +26,11 @@ public class UnitTest1
     {
         var server = new PetStoreTestServer();
         var httpClient = server.CreateClient();
+        httpClient.DefaultRequestHeaders.Add("x-idempotency-key", "1234567890");
         httpClient.BaseAddress = new Uri(httpClient.BaseAddress, "api/v3");
         var client = RestService.For<ISwaggerPetstoreOpenAPI30>(httpClient);
         var username = "yao";
+
         var response = await client.GetUserByName(username);
         var content = response.Content;
         Assert.AreEqual(username, content.Username);
