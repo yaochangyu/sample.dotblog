ca3a8485b14f6e5eb18dabcf61c72051f9648d55
yao <yao-chang.yu@outlook.com>
2023-09-13 09:04:02 +0800

    移除 header

--- WebAPI/Swagger/Lab.RefitClient/Lab.RefitClient.WebAPI/Controllers/PetstoreControllerImpl.cs
+++ WebAPI/Swagger/Lab.RefitClient/Lab.RefitClient.WebAPI/Controllers/PetstoreControllerImpl.cs
@@ -2,88 +2,111 @@
 
 public class PetStoreControllerImpl : IPetStoreController
 {
-    public Task<Pet> UpdatePetAsync(Pet body)
+    private IHttpContextAccessor _httpContextAccessor;
+
+    private HttpContext _httpContent;
+
+    public PetStoreControllerImpl(IHttpContextAccessor httpContextAccessor)
+    {
+        this._httpContextAccessor = httpContextAccessor;
+    }
+
+    // public PetStoreControllerImpl(HttpContextAccessor httpContextAccessor)
+    // {
+    //     this._httpContextAccessor = httpContextAccessor;
+    // }
+
+    public Task<Pet> UpdatePetAsync(Pet body, CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task<Pet> AddPetAsync(Pet body)
+    public Task<Pet> AddPetAsync(Pet body, CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task<ICollection<Pet>> FindPetsByStatusAsync(Status status)
+    public Task<ICollection<Pet>> FindPetsByStatusAsync(Status status, CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task<ICollection<Pet>> FindPetsByTagsAsync(IEnumerable<string> tags)
+    public Task<ICollection<Pet>> FindPetsByTagsAsync(IEnumerable<string> tags, CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task<Pet> GetPetByIdAsync(long petId)
+    public Task<Pet> GetPetByIdAsync(long petId, CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task UpdatePetWithFormAsync(long petId, string name, string status)
+    public Task UpdatePetWithFormAsync(long petId, string name, string status,
+        CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task DeletePetAsync(string api_key, long petId)
+    public Task DeletePetAsync(string api_key, long petId, CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task<ApiResponse> UploadFileAsync(long petId, string additionalMetadata, IFormFile body)
+    public Task<ApiResponse> UploadFileAsync(long petId, string additionalMetadata, IFormFile body,
+        CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task<IDictionary<string, int>> GetInventoryAsync()
+    public Task<IDictionary<string, int>> GetInventoryAsync(CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task<Order> PlaceOrderAsync(Order body)
+    public Task<Order> PlaceOrderAsync(Order body, CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task<Order> GetOrderByIdAsync(long orderId)
+    public Task<Order> GetOrderByIdAsync(long orderId, CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task DeleteOrderAsync(long orderId)
+    public Task DeleteOrderAsync(long orderId, CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task<User> CreateUserAsync(User body)
+    public Task<User> CreateUserAsync(User body, CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task<User> CreateUsersWithListInputAsync(IEnumerable<User> body)
+    public Task<User> CreateUsersWithListInputAsync(IEnumerable<User> body, CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task<string> LoginUserAsync(string username, string password)
+    public Task<string> LoginUserAsync(string username, string password, CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task LogoutUserAsync()
+    public Task LogoutUserAsync(CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public async Task<User> GetUserByNameAsync(string username)
+    public Task<User> GetUserByNameAsync(string username, string x_api_key,
+        CancellationToken cancellationToken = default(CancellationToken))
     {
+        throw new NotImplementedException();
+    }
+
+    public async Task<User> GetUserByNameAsync(string username, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var headers = this._httpContextAccessor.HttpContext.Request.Headers;
         return new User
         {
             Id = 0,
@@ -98,12 +121,12 @@ public class PetStoreControllerImpl : IPetStoreController
         };
     }
 
-    public Task UpdateUserAsync(string username, User body)
+    public Task UpdateUserAsync(string username, User body, CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
 
-    public Task DeleteUserAsync(string username)
+    public Task DeleteUserAsync(string username, CancellationToken cancellationToken = default(CancellationToken))
     {
         throw new NotImplementedException();
     }
