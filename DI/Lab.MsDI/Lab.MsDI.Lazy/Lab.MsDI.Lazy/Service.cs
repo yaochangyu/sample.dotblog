67272bbf6711efb59c791d42640e01c59c3e1304
yao <yao-chang.yu@outlook.com>
2023-10-01 13:30:52 +0800

    add lazy di project

---
+++ DI/Lab.MsDI/Lab.MsDI.Lazy/Lab.MsDI.Lazy/Service.cs
@@ -0,0 +1,54 @@
+﻿namespace Lab.MsDI.Lazy;
+
+public interface IService
+{
+    string Get();
+}
+
+public class Service : IService
+{
+    private readonly IServiceA _serviceA;
+    private readonly IServiceB _serviceB;
+
+    public Service(IServiceA serviceA,
+        IServiceB serviceB)
+    {
+        this._serviceA = serviceA;
+        this._serviceB = serviceB;
+    }
+
+    public string Get()
+    {
+        var random = new Random().Next(1, 10);
+        if (random % 2 == 0)
+        {
+            return this._serviceB.Get();
+        }
+
+        return this._serviceA.Get();
+    }
+}
+
+public interface IServiceA
+{
+    string Get();
+}
+
+public class ServiceA : IServiceA
+{
+    public ServiceA() => Console.WriteLine("Create instance for ServiceA");
+
+    public string Get() => "ServiceA";
+}
+
+public interface IServiceB
+{
+    string Get();
+}
+
+public class ServiceB : IServiceB
+{
+    public ServiceB() => Console.WriteLine("Create instance for ServiceB");
+
+    public string Get() => "ServiceB";
+}
---
+++ DI/Lab.MsDI/Lab.MsDI.Lazy/Lab.MsDI.Lazy/ServiceLazy.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab.MsDI.Lazy;
+
+public class ServiceLazy : IService
+{
+    private readonly Lazy<IServiceA> _serviceA;
+    private readonly Lazy<IServiceB> _serviceB;
+
+    public ServiceLazy(Lazy<IServiceA> serviceA,
+        Lazy<IServiceB> serviceB)
+    {
+        this._serviceA = serviceA;
+        this._serviceB = serviceB;
+    }
+
+    public string Get()
+    {
+        var random = new Random().Next(1, 10);
+        if (random % 2 == 0)
+        {
+            return this._serviceB.Value.Get();
+        }
+
+        return this._serviceA.Value.Get();
+    }
+}
