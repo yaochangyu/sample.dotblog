93aabf065e6f9eb3b5020535ebf67aeb71d482d7
yao <yao-chang.yu@outlook.com>
2023-10-08 12:40:31 +0800

    refactor

--- Trace/Lab.Context.Trace/Lab.Context.Trace.WebAPI/Models/FailureCode.cs
+++ Trace/Lab.Context.Trace/Lab.Context.Trace.WebAPI/Models/FailureCode.cs
@@ -2,16 +2,5 @@ namespace Lab.Context.Trace.WebAPI.Models;
 
 public enum FailureCode
 {
-    UnknownError = 0,
-    InputInvalid = 1,
-    MemberNotFound,
-    MemberAlreadyExist,
-    ServerError,
-    DataConflict,
-    DataConcurrency,
-    DataNotFound,
-    DbError,
-    S3Error,
-    CellphoneFormatInvalid,
     Unauthorized
 }
