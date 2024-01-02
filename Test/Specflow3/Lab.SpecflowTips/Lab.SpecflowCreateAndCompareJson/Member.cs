51e8a1e0bc9106c6a85fc497ec4bb18c6a61abc1
yao <yao-chang.yu@outlook.com>
2024-01-02 23:07:10 +0800

    feat: specflow 比對 json

---
+++ Test/Specflow3/Lab.SpecflowTips/Lab.SpecflowCreateAndCompareJson/Member.cs
@@ -0,0 +1,35 @@
+﻿namespace Lab.SpecflowCreateAndCompareJson;
+
+public class Member
+{
+    public string Id { get; set; }
+
+    public int Age { get; set; }
+
+    public Name Name { get; set; }
+
+    public State State { get; set; }
+
+    public List<string> IpData { get; set; }
+
+    public List<Order> Orders { get; set; }
+}
+
+public enum State
+{
+    None,
+    Active,
+    Inactive,
+}
+
+public class Order
+{
+    public string Id { get; set; }
+}
+
+public class Name
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+}
