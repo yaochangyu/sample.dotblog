b8204796c25d3ff6f15f70a3305095109ea6891c
yao <yao-chang.yu@outlook.com>
2024-01-15 00:30:14 +0800

    refactor

---
+++ Shnapshot/Lab.Snapshot/Lab.Snapshot.DB/MemberDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Lab.Snapshot.DB;
+
+public class MemberDbContextFactory : IDesignTimeDbContextFactory<MemberDbContext>
+{
+    public MemberDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<MemberDbContext>();
+        optionsBuilder.UseNpgsql("Host=localhost;Database=member;Username=postgres;Password=guest");
+
+        return new MemberDbContext(optionsBuilder.Options);
+    }
+}
