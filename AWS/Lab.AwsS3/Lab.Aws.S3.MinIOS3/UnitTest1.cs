a7874192c4f732d0aec254d6b608a7d5b309e6b1
yao <yao-chang.yu@outlook.com>
2024-02-01 19:41:14 +0800

    增加批次上傳範例

--- AWS/Lab.AwsS3/Lab.Aws.S3.MinIOS3/UnitTest1.cs
+++ AWS/Lab.AwsS3/Lab.Aws.S3.MinIOS3/UnitTest1.cs
@@ -1,6 +1,6 @@
 using System.Collections.Concurrent;
 using System.Reflection;
-using Amazon;
+using Amazon.Runtime;
 using Amazon.S3;
 using Amazon.S3.Model;
 
@@ -71,16 +71,75 @@ public class UnitTest1
     [TestMethod]
     public async Task 新增一個儲存桶()
     {
-        var s3Config = new AmazonS3Config()
-        {
-            RegionEndpoint = RegionEndpoint.USEast1,
-            ServiceURL = "http://localhost:9000",
-            ForcePathStyle = true
-        };
-        var s3Client = new AmazonS3Client(s3Config);
+        var s3Client = CreateS3Client();
         var response = await s3Client.PutBucketAsync(new PutBucketRequest
         {
             BucketName = "test-bucket",
         });
+    }
+
+    [TestMethod]
+    public async Task 分批上傳檔案()
+    {
+        await WriteObjectDataAsync("上傳.csv");
+    }
+    [TestMethod]
+    public async Task 分批下載檔案()
+    {
+        await WriteObjectDataAsync("上傳.csv");
+    }
+
+    static async Task WriteObjectDataAsync(string filePath)
+    {
+        var s3Client = CreateS3Client();
+        var writerStream = new MemoryStream();
+        var writer = new StreamWriter(writerStream)
+        {
+            AutoFlush = true
+        };
+        try
+        {
+            await using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            using var reader = new StreamReader(fileStream);
+            var lineCount = 0;
+            var lineLimit = 10;
+            var putRequest = new PutObjectRequest()
+            {
+                BucketName = "test-bucket",
+                Key = $"test.csv",
+                UseChunkEncoding = true,
+                AutoCloseStream = false,
+                AutoResetStreamPosition = true,
+            };
+            while (await reader.ReadLineAsync() is { } line)
+            {
+                // todo:可以處理一整批後再寫入到 s3
+                await writer.WriteLineAsync(line);
+                putRequest.InputStream = writerStream;
+                var response = await s3Client.PutObjectAsync(putRequest);
+                lineCount++;
+            }
+        }
+        catch (AmazonS3Exception e)
+        {
+            Console.WriteLine("Error encountered on server. Message:'{0}' when writing object", e.Message);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing object", e.Message);
+        }
+    }
+    
+    static AmazonS3Client CreateS3Client()
+    {
+        var credentials = new BasicAWSCredentials("AKIAIOSFODNN7EXAMPLE",
+            "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY");
+
+        var s3Config = new AmazonS3Config
+        {
+            ServiceURL = "http://localhost:9000",
+            ForcePathStyle = true
+        };
+        return new AmazonS3Client(credentials, s3Config);
     }
 }
