4197882e0edc66c22a9c7cd4bec02135ce657e61
yao <yao-chang.yu@outlook.com>
2024-03-03 11:55:04 +0800

    add app2

---
+++ Rate Limit/Lab.ClientRateLimitAndRedis/Lab.ClientRateLimitAndRedis2/RateLimitHttpMessageHandler.cs
@@ -0,0 +1,58 @@
+﻿namespace Lab.ClientRateLimitAndRedis2;
+
+public class RateLimitHttpMessageHandler : DelegatingHandler
+{
+    private readonly List<DateTimeOffset> _callLog =
+        new List<DateTimeOffset>();
+    private readonly TimeSpan _limitTime;
+    private readonly int _limitCount;
+
+    public RateLimitHttpMessageHandler(int limitCount, TimeSpan limitTime)
+    {
+        this._limitCount = limitCount;
+        this._limitTime = limitTime;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        var now = DateTimeOffset.UtcNow;
+
+        lock (this._callLog)
+        {
+            this._callLog.Add(now);
+
+            while (this._callLog.Count > this._limitCount)
+                this._callLog.RemoveAt(0);
+        }
+
+        await this.LimitDelay(now);
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+
+    private async Task LimitDelay(DateTimeOffset now)
+    {
+        if (this._callLog.Count < this._limitCount)
+            return;
+
+        var limit = now.Add(-this._limitTime);
+
+        var lastCall = DateTimeOffset.MinValue;
+        var shouldLock = false;
+
+        lock (this._callLog)
+        {
+            lastCall = this._callLog.FirstOrDefault();
+            shouldLock = this._callLog.Count(x => x >= limit) >= this._limitCount;
+        }
+
+        var delayTime = shouldLock && (lastCall > DateTimeOffset.MinValue)
+            ? (limit - lastCall)
+            : TimeSpan.Zero;
+
+        if (delayTime > TimeSpan.Zero)
+            await Task.Delay(delayTime);
+    }
+}
