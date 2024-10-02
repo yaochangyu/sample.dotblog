b5c24a0cc0e2c5875a5c7dc0255bb8d11c9ba46a
yao <yao-chang.yu@outlook.com>
2024-10-02 17:43:41 +0800

    完成環境變數產生器

---
+++ Configuration/Lab.EnvGenerator/Lab.EnvGeneratorCli/Program.cs
@@ -0,0 +1,19 @@
+﻿using Spectre.Console.Cli;
+
+namespace Lab.EnvGeneratorCli;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var app = new CommandApp();
+        app.Configure(config =>
+        {
+            config.AddCommand<ConvertEnvCommand>("convert")
+                .WithDescription("convert a file.")
+                .WithExample("convert", "--env", "qa");
+        });
+
+        app.Run(args);
+    }
+}
