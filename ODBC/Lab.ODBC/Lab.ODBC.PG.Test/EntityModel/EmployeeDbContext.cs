a0caca996f03215a90352a627e32d00fae76d17f
yao <yao-chang.yu@outlook.com>
2024-10-03 16:10:38 +0800

    完成 Dapper + ODBC 讀取 PostgreSql

---
+++ ODBC/Lab.ODBC/Lab.ODBC.PG.Test/EntityModel/EmployeeDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab.ODBC.PG.Test.EntityModel
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(p =>
+            {
+                p.HasKey(e => e.Id);
+                p.HasIndex(e => e.SequenceId)
+                    .IsUnique();
+                p.Property(p => p.Remark).IsRequired(false);
+            });
+        }
+    }
+}
