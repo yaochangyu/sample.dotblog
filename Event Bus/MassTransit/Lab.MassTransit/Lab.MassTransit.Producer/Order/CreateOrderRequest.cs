b7281decb0c2b25ff6712db69bad3ca1061cc5d6
yao <yao-chang.yu@outlook.com>
2024-10-13 17:01:00 +0800

    建立生產者、消費者專案

---
+++ Event Bus/MassTransit/Lab.MassTransit/Lab.MassTransit.Producer/Order/CreateOrderRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Lab.MassTransit.Producer.Order;
+
+// 訂單請求
+public class CreateOrderRequest
+{
+    public decimal TotalAmount { get; set; }
+}
---
+++ Event Bus/MassTransit/Lab.MassTransit/Producer/MessageSenderService.cs
@@ -0,0 +1,37 @@
+﻿using MassTransit;
+using Message;
+using Microsoft.Extensions.Hosting;
+
+namespace Producer;
+
+public class MessageSenderService : IHostedService
+{
+    private readonly ISendEndpointProvider _sendEndpointProvider;
+
+    public MessageSenderService(ISendEndpointProvider sendEndpointProvider)
+    {
+        this._sendEndpointProvider = sendEndpointProvider;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        // 取得 SendEndpoint 並發送訊息到指定佇列
+        var sendEndpoint =
+
+            // await this._sendEndpointProvider.GetSendEndpoint(new Uri("rabbitmq://localhost/order-submitted-queue"));
+            await this._sendEndpointProvider.GetSendEndpoint(new Uri("rabbitmq://localhost/order-submitted-queue"));
+
+        await sendEndpoint.Send(new OrderSubmitted
+        {
+            OrderId = Guid.NewGuid(),
+            Timestamp = DateTime.UtcNow
+        }, cancellationToken);
+
+        Console.WriteLine("OrderSubmitted event sent to order-submitted-queue.");
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
