a24eedbae1b41d65fd15de428590d948120e6c09
余小章 <yao-chang.yu@outlook.com>
2024-10-18 19:31:18 +0800

    add file

---
+++ 專案範本/Lab.Context.Trace/JobBank1111.Job.WebAPI/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+using JobBank1111.Infrastructure.TraceContext;
+using JobBank1111.Job.WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobBank1111.Job.WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DemoController : ControllerBase
+{
+    private readonly ILogger<DemoController> _logger;
+    private readonly IContextGetter<AuthContext?> _authContextGetter;
+
+    public DemoController(ILogger<DemoController> logger,
+        IContextGetter<AuthContext?> authContextGetter)
+    {
+        this._logger = logger;
+        this._authContextGetter = authContextGetter;
+    }
+
+    [HttpGet(Name = "GetDemo")]
+    public ActionResult Get()
+    {
+        var authContext = this._authContextGetter.Get();
+        var userId = authContext.UserId;
+        // 由 Context 取得 UserId
+        var member = Member.GetFakeMembers().FirstOrDefault(p => p.UserId == userId);
+
+        this._logger.LogInformation(2000, "found {@Data}", member);
+
+        return this.Ok(member);
+    }
+}
---
+++ 專案範本/Lab.Context.Trace/JobBank1111.Job.WebAPI/SysHeaderNames.cs
@@ -0,0 +1,6 @@
+namespace JobBank1111.Job.WebAPI;
+
+public abstract class SysHeaderNames
+{
+    public const string TraceId = "x-trace-id";
+}
