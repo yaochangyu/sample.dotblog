dafcabb466e9aadb09fdf4d9cd59934049fb604c
余小章 <yao-chang.yu@outlook.com>
2024-10-18 13:26:57 +0800

    增加使用場景

--- Event Bus/MassTransit/Lab.MassTransit/Consumer/OrderSubmittedConsumer.cs
+++ Event Bus/MassTransit/Lab.MassTransit/Consumer/OrderSubmittedConsumer.cs
@@ -7,14 +7,14 @@ public class OrderSubmittedConsumer : IConsumer<OrderSubmitted>
 {
     public async Task Consume(ConsumeContext<OrderSubmitted> context)
     {
-        var destinationAddress = new Uri("rabbitmq://localhost/order-submitted-queue");
-        var command = new OrderSubmitted()
-        {
-            OrderId = context.Message.OrderId,
-            Timestamp = context.Message.Timestamp
-        };
-
-        await context.Send(destinationAddress, command);
+        // var destinationAddress = new Uri("rabbitmq://localhost/order-submitted-queue");
+        // var command = new OrderSubmitted()
+        // {
+        //     OrderId = context.Message.OrderId,
+        //     Timestamp = context.Message.Timestamp
+        // };
+        //
+        // await context.Send(destinationAddress, command);
 
         // var endpoint = await context.GetSendEndpoint(destinationAddress);
         // await endpoint.Send(command);
--- Event Bus/MassTransit/Lab.MassTransit/Producer/MessagePublishService2.cs
+++ Event Bus/MassTransit/Lab.MassTransit/Producer/MessagePublishService2.cs
@@ -13,15 +13,15 @@ public class MessagePublishService2 : IHostedService
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        // 使用 Publish 發佈事件，所有訂閱者都能接收此事件
         var orderSubmitted = new OrderSubmitted
         {
             OrderId = Guid.NewGuid(),
             Timestamp = DateTime.UtcNow
         };
         // ===Publish===
-        // await this._bus.Publish(orderSubmitted, cancellationToken);
-        // Console.WriteLine("OrderSubmitted event published.");
+        // 使用 Publish 發佈事件，所有訂閱者都能接收此事件
+        await this._bus.Publish(orderSubmitted, cancellationToken);
+        Console.WriteLine("OrderSubmitted event published.");
         
         // ===Send===
         // EndpointConvention.Map<OrderSubmitted>(new Uri("queue:order-submitted-queue"));
