2bd5e49a75541d50817f8709d9592d5c9a9b024f
yao <yao-chang.yu@outlook.com>
2024-10-31 17:05:42 +0800

    增加 RequestScopedDynamicDbContextFactory

---
+++ ORM/EFCore/Lab.Sharding/src/Lab.Sharding.DB/RequestScopedDynamicDbContextFactory.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Concurrent;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lab.Sharding.DB;
+
+public static class HttpContextExtensions
+{
+	public static T GetOrCreate<T>(this IDictionary<object, object?> items, object key, Func<T> factory)
+		where T : class
+	{
+		if (items.TryGetValue(key, out var existingValue) && existingValue is T existing)
+		{
+			return existing;
+		}
+
+		var result = factory();
+		items[key] = result;
+		return result;
+	}
+}
+
+public class RequestScopedDynamicDbContextFactory<TContext>(
+	IConnectionStringProvider connectionStringProvider,
+	IHttpContextAccessor httpContextAccessor)
+	: IDynamicDbContextFactory<TContext>
+	where TContext : DynamicDbContext
+{
+	public TContext CreateDbContext(string serverName, string databaseName, string tablePostfix)
+	{
+		var httpContext = httpContextAccessor.HttpContext;
+		if (httpContext == null)
+		{
+			throw new InvalidOperationException("HttpContext is not available");
+		}
+
+		var name = (typeof(RequestScopedDynamicDbContextFactory<TContext>)).Name;
+		var contextDictionary = httpContext.Items.GetOrCreate(
+			name,
+			() => new ConcurrentDictionary<string, TContext>());
+
+		var requestId = httpContext.Items.GetOrCreate("RequestId", () => Guid.NewGuid().ToString());
+
+		return contextDictionary.GetOrAdd(requestId, key =>
+		{
+			var connectionString = connectionStringProvider.GetConnectionString(serverName, databaseName);
+
+			// var scope = serviceScopeFactory.CreateScope();
+			var scope = httpContextAccessor.HttpContext.RequestServices.CreateScope();
+
+			// 在請求結束時釋放資源
+			httpContext.Response.RegisterForDispose(scope);
+
+			var options = new DbContextOptionsBuilder<TContext>()
+				.UseSqlServer(connectionString)
+				.Options;
+
+			return (TContext)Activator.CreateInstance(
+				typeof(TContext),
+				connectionString,
+				tablePostfix);
+		});
+	}
+}
--- ORM/EFCore/Lab.Sharding/src/Lab.Sharding.Infrastructure/PaginatedList.cs
+++ ORM/EFCore/Lab.Sharding/src/Lab.Sharding.Infrastructure/PaginatedList.cs
@@ -2,32 +2,32 @@
 
 enum MyEnum
 {
-    AA = 0,
-    BB
+	AA = 0,
+	BB
 }
 
 public class PaginatedList<T>
 {
-    public List<T> Items { get; }
+	public List<T> Items { get; set; }
 
-    public int PageIndex { get; }
+	public int PageIndex { get; set; }
 
-    public int TotalPages { get; }
+	public int TotalPages { get; set; }
 
-    public bool HasPreviousPage => this.PageIndex > 1;
+	public bool HasPreviousPage => this.PageIndex > 1;
 
-    public bool HasNextPage => this.PageIndex < this.TotalPages;
+	public bool HasNextPage => this.PageIndex < this.TotalPages;
 
-    public PaginatedList()
-    {
-    }
+	public PaginatedList()
+	{
+	}
 
-    public PaginatedList(List<T> items, int pageIndex, int pageSize, int totalCount)
-    {
-        var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+	public PaginatedList(List<T> items, int pageIndex, int pageSize, int totalCount)
+	{
+		var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
 
-        this.Items = items;
-        this.PageIndex = pageIndex;
-        this.TotalPages = totalPages;
-    }
+		this.Items = items;
+		this.PageIndex = pageIndex;
+		this.TotalPages = totalPages;
+	}
 }
