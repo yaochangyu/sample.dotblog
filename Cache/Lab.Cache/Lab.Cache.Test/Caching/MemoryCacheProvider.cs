7c91c5fcee4bab1e9d41f63fec20baa1c549bce8
yao <yao-chang.yu@outlook.com>
2024-11-03 22:52:52 +0800

    加入專案

---
+++ Cache/Lab.Cache/Lab.Cache.Test/Caching/MemoryCacheProvider.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Lab.Cache.Test.Caching;
+
+public class MemoryCacheProvider : ICacheProvider
+{
+    private readonly IMemoryCache _memoryCache;
+    private readonly CacheProviderOptions _options;
+
+    public MemoryCacheProvider(IMemoryCache memoryCache,
+                               CacheProviderOptions options = null)
+    {
+        this._memoryCache = memoryCache;
+        this._options = options;
+    }
+
+    public Task<T?> GetAsync<T>(string key)
+    {
+        this._memoryCache.TryGetValue(key, out T? value);
+        return Task.FromResult(value);
+    }
+
+    public Task SetAsync<T>(string key,
+                            T value,
+                            CacheProviderOptions options)
+    {
+        var cacheOptions = new MemoryCacheEntryOptions();
+
+        if (options.SlidingExpiration.HasValue)
+        {
+            cacheOptions.SetSlidingExpiration(options.SlidingExpiration.Value);
+        }
+
+        if (options.AbsoluteExpiration.HasValue)
+        {
+            cacheOptions.SetAbsoluteExpiration(options.AbsoluteExpiration.Value);
+        }
+
+        this._memoryCache.Set(key, value, cacheOptions);
+        return Task.CompletedTask;
+    }
+
+    public Task SetAsync<T>(string key,
+                            T value)
+    {
+        return this.SetAsync(key, value, this._options);
+    }
+
+    public Task RemoveAsync(string key)
+    {
+        this._memoryCache.Remove(key);
+        return Task.CompletedTask;
+    }
+
+    public Task<bool> ExistsAsync(string key)
+    {
+        return Task.FromResult(this._memoryCache.TryGetValue(key, out _));
+    }
+}
---
+++ Cache/Lab.Cache/Lab.Cache.Test/Caching/RedisCacheProvider.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Lab.Cache.Test.Caching;
+
+public class RedisCacheProvider : ICacheProvider
+{
+    private readonly IDistributedCache _distributedCache;
+    private readonly CacheProviderOptions _options;
+
+    public RedisCacheProvider(IDistributedCache distributedCache,
+                              CacheProviderOptions options = null)
+    {
+        this._distributedCache = distributedCache;
+        this._options = options;
+    }
+
+    public async Task<T?> GetAsync<T>(string key)
+    {
+        var jsonData = await this._distributedCache.GetStringAsync(key);
+        if (string.IsNullOrEmpty(jsonData))
+        {
+            return default;
+        }
+
+        return JsonSerializer.Deserialize<T>(jsonData);
+    }
+
+    public async Task SetAsync<T>(string key,
+                                  T value,
+                                  CacheProviderOptions options)
+    {
+        var distributedOptions = new DistributedCacheEntryOptions();
+
+        if (options.SlidingExpiration.HasValue)
+        {
+            distributedOptions.SetSlidingExpiration(options.SlidingExpiration.Value);
+        }
+
+        if (options.AbsoluteExpiration.HasValue)
+        {
+            distributedOptions.SetAbsoluteExpiration(options.AbsoluteExpiration.Value);
+        }
+
+        var jsonData = JsonSerializer.Serialize(value);
+        await this._distributedCache.SetStringAsync(key, jsonData, distributedOptions);
+    }
+
+    public Task SetAsync<T>(string key,
+                            T value)
+    {
+        return this.SetAsync(key, value, this._options);
+    }
+
+    public async Task RemoveAsync(string key)
+    {
+        await this._distributedCache.RemoveAsync(key);
+    }
+
+    public async Task<bool> ExistsAsync(string key)
+    {
+        var value = await this._distributedCache.GetStringAsync(key);
+        return !string.IsNullOrEmpty(value);
+    }
+}
