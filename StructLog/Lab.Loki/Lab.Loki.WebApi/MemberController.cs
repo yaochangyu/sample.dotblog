c127c13e7807d33cf8f0683fdce6d12dc1ff4316
yao <yao-chang.yu@outlook.com>
2024-11-07 23:28:30 +0800

    加入專案

---
+++ StructLog/Lab.Loki/Lab.Loki.WebApi/MemberController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.Loki.WebApi;
+
+[ApiController]
+public class MemberV1Controller(ILogger<MemberV1Controller> Logger) : ControllerBase
+{
+    [HttpGet]
+    [Route("api/v1/members", Name = "GetMember")]
+    public async Task<ActionResult> GetMemberCursor(
+        CancellationToken cancel = default)
+    {
+        Logger.LogInformation(new EventId(2000, "Trace"), "Start {ControllerName}.{MethodName}...",
+                                    nameof(MemberV1Controller), nameof(GetMemberCursor));
+        
+        var sensorInput = new { Latitude = 25, Longitude = 134 };
+        Logger.LogInformation("Processing {@SensorInput}", sensorInput);
+        return this.Ok();
+    }
+}
