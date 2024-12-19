433a5169953c26dca87e15ee67fae5505b69a27e
yao <yao-chang.yu@outlook.com>
2024-12-19 23:21:58 +0800

    add i18n project

---
+++ i18N/Lab.i18N.WebApi/HeaderRequestCultureProvider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Primitives;
+
+namespace Lab.i18N.WebApi;
+
+public class HeaderRequestCultureProvider : RequestCultureProvider
+{
+    public override async Task<ProviderCultureResult?> DetermineProviderCultureResult(HttpContext httpContext)
+    {
+        // 檢查是否有 Accept-Language Header
+        if (!httpContext.Request.Headers.TryGetValue("Accept-Language", out var acceptLanguage))
+        {
+            return await Task.FromResult<ProviderCultureResult?>(null);
+        }
+
+        // 解析 Header 值，取第一個語言
+        var languages = acceptLanguage.ToString()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(lang => lang.Split(';').FirstOrDefault())
+                .ToList()
+            ;
+
+        if (languages.Any())
+        {
+            var languageSegments = languages.Select(lang => new StringSegment(lang)).ToList();
+            return await Task.FromResult(new ProviderCultureResult(languageSegments));
+        }
+
+        return await Task.FromResult<ProviderCultureResult?>(null);
+    }
+}
