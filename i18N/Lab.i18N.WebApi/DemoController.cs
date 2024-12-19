b1662ef1f2add62b90f11b5b0f808b25b28e0e04
yao <yao-chang.yu@outlook.com>
2024-12-19 23:21:58 +0800

    add i18n project

---
+++ i18N/Lab.i18N.WebApi/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace Lab.i18N.WebApi;
+
+[ApiController]
+public class DemoController(IStringLocalizer<DemoController> localizer) : ControllerBase
+{
+    [HttpGet]
+    [Route("api/v1/demo", Name = "GetDemo")]
+    public IActionResult Get()
+    {
+        var desc = localizer["about.description"];
+
+        return Ok(new
+        {
+            desc
+        });
+    }
+}
