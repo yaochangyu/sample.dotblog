e3a4cd544411d30e7981f50682f4f2cc09080651
yao <yao-chang.yu@outlook.com>
2025-01-11 10:39:01 +0800

    ✨ feat: 添加 Web API 项目及其相关配置文件

---
+++ Nginx/WebApi/WebApi/Program.cs
@@ -0,0 +1,49 @@
+namespace WebApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        // Configure the HTTP request pipeline.
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                                                                 new WeatherForecast
+                                                                 {
+                                                                     Date = DateOnly.FromDateTime(
+                                                                         DateTime.Now.AddDays(index)),
+                                                                     TemperatureC = Random.Shared.Next(-20, 55),
+                                                                     Summary = summaries[
+                                                                         Random.Shared.Next(summaries.Length)]
+                                                                 })
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+        app.UseStaticFiles();
+        app.Run();
+    }
+}
