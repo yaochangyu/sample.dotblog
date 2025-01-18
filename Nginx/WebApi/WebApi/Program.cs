6e4cd6e17d8a914c8c3bf2a54d070920c1034285
yao <yao-chang.yu@outlook.com>
2025-01-18 09:58:23 +0800

    ✨ feat(Nginx/WebApi): 更新 index.html 的輸出目錄設置為 Never，並啟用預設檔案服務功能。

--- Nginx/WebApi/WebApi/Program.cs
+++ Nginx/WebApi/WebApi/Program.cs
@@ -43,6 +43,7 @@ public class Program
             })
             .WithName("GetWeatherForecast")
             .WithOpenApi();
+        app.UseDefaultFiles();
         app.UseStaticFiles();
         app.Run();
     }
