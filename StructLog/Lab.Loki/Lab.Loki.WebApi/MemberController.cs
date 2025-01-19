d430ee66bccc793526296192d1b38f148069581b
yao <yao-chang.yu@outlook.com>
2025-01-19 20:42:58 +0800

    🚨 fix(MemberController): 增加日誌記錄以捕捉不同級別的錯誤和警告訊息

--- StructLog/Lab.Loki/Lab.Loki.WebApi/MemberController.cs
+++ StructLog/Lab.Loki/Lab.Loki.WebApi/MemberController.cs
@@ -14,12 +14,19 @@ public class MemberV1Controller(ILogger<MemberV1Controller> log) : ControllerBas
         log.LogInformation("OKK");
         log.LogInformation(
             "API Request completed {@RequestDetails}",
-            new { 
+            new
+            {
                 Path = Request.Path,
                 Method = Request.Method,
                 StatusCode = Response.StatusCode,
+
                 // Duration = stopwatch.ElapsedMilliseconds
             });
+        log.LogError(2000, new Exception("完蛋了!!!"), "慘了");
+        log.LogCritical(2001, new Exception("完蛋了!!!"), "慘了");
+        log.LogWarning(3000, "警告");
+        log.LogTrace(3001, "追蹤");
+        log.LogDebug(3002, "除錯");
         return this.Ok();
     }
 }
