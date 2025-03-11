fca2d42d9b8ed59355c7e10a024aecf0d34fdf8e
余小章 <yao-chang.yu@outlook.com>
2025-03-11 12:54:16 +0800

    ✨ feat(GracefulShutdown): 新增優雅關閉服務，實現服務啟動與停止的日誌紀錄功能

---
+++ Graceful Shutdown/Lab.GracefulShutdownWithSellMode/Lab.GracefulShutdown.Net8/GracefulShutdownService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Lab.GracefulShutdown.Net8;
+
+internal class GracefulShutdownService : IHostedService
+{
+    private readonly IHostApplicationLifetime _appLifetime;
+    private Task _backgroundTask;
+    private bool _stop;
+    private ILogger<GracefulShutdownService> _logger;
+
+    public GracefulShutdownService(IHostApplicationLifetime appLifetime,
+        ILogger<GracefulShutdownService> logger)
+    {
+        this._appLifetime = appLifetime;
+        this._logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancel)
+    {
+        this._logger.LogInformation($"{DateTime.Now} 服務啟動中...");
+
+        this._backgroundTask = Task.Run(async () => { await this.ExecuteAsync(cancel); }, cancel);
+
+        return Task.CompletedTask;
+    }
+
+    public async Task StopAsync(CancellationToken cancel)
+    {
+        this._logger.LogInformation($"{DateTime.Now} 服務停止中...");
+
+        this._stop = true;
+        await this._backgroundTask;
+        
+        this._logger.LogInformation($"{DateTime.Now} 服務已停止");
+    }
+
+    private async Task ExecuteAsync(CancellationToken cancel)
+    {
+        this._logger.LogInformation($"{DateTime.Now} 服務已啟動!");
+
+        while (!this._stop)
+        {
+            this._logger.LogInformation($"{DateTime.Now} 1.服務運行中...");
+            this._logger.LogInformation($"1.IsCancel={cancel.IsCancellationRequested}");
+            await Task.Delay(TimeSpan.FromSeconds(30), cancel);
+            this._logger.LogInformation($"2.IsCancel={cancel.IsCancellationRequested}");
+            this._logger.LogInformation($"{DateTime.Now} 2.服務運行中...");
+        }
+
+        this._logger.LogInformation($"{DateTime.Now} 服務已完美的停止(Graceful Shutdown)");
+    }
+}
---
+++ Graceful Shutdown/Lab.GracefulShutdownWithSellMode/Lab.GracefulShutdown.Net8/GracefulShutdownService1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Lab.GracefulShutdown.Net8;
+
+class GracefulShutdownService1 : BackgroundService
+{
+    private readonly ILogger<GracefulShutdownService1> _logger;
+
+    public GracefulShutdownService1(ILogger<GracefulShutdownService1> logger)
+    {
+        this._logger = logger;
+    }
+    private int _count = 1;
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        this._logger.LogInformation($"{DateTime.Now} 服務已啟動!");
+
+        while (true)
+        {
+            this._logger.LogInformation($"{DateTime.Now} ，執行次數：{_count++}");
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            this._logger.LogInformation($"{DateTime.Now} 1.服務運行中...");
+            this._logger.LogInformation($"1.IsCancel={stoppingToken.IsCancellationRequested}");
+            await Task.Delay(TimeSpan.FromSeconds(30), stoppingToken);
+            this._logger.LogInformation($"2.IsCancel={stoppingToken.IsCancellationRequested}");
+            this._logger.LogInformation($"{DateTime.Now} 2.服務運行中...");
+        }
+        this._logger.LogInformation($"{DateTime.Now} 服務已完美的停止(Graceful Shutdown)");
+    }
+}
