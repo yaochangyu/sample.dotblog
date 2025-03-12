ff9fce2995516b14b61d3847a68a7dd8195d4d8e
余小章 <yao-chang.yu@outlook.com>
2025-03-12 15:35:13 +0800

    ✨ feat(Dockerfile): 更新入口點設置以啟用應用程式啟動
    ✨ feat(GracefulShutdownService1.cs): 修改服務啟動與停止邏輯以加強日誌記錄

--- Graceful Shutdown/Lab.GracefulShutdownWithSellMode/Lab.GracefulShutdown.Net8/GracefulShutdownService1.cs
+++ Graceful Shutdown/Lab.GracefulShutdownWithSellMode/Lab.GracefulShutdown.Net8/GracefulShutdownService1.cs
@@ -14,10 +14,14 @@ class GracefulShutdownService1 : BackgroundService
 
     private int _count = 1;
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    public override Task StartAsync(CancellationToken cancellationToken)
     {
         this._logger.LogInformation($"{DateTime.Now} 服務已啟動!");
+        return base.StartAsync(cancellationToken);
+    }
 
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
         while (true)
         {
             if (stoppingToken.IsCancellationRequested)
@@ -28,7 +32,11 @@ class GracefulShutdownService1 : BackgroundService
             this._logger.LogInformation($"{DateTime.Now} ，執行次數：{_count++}");
             await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
         }
+    }
 
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
         this._logger.LogInformation($"{DateTime.Now} 完成!");
+        return base.StopAsync(cancellationToken);
     }
 }
