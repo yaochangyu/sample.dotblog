0dd19a7d0d20baccf1e1346a82c4cd96838e8f52
yao <yao-chang.yu@outlook.com>
2025-03-31 22:03:38 +0800

    feat: initialize Python project with basic structure
    Added pyproject.toml configuration and main.py entry point for lab-py-testcontainer project. Set up Python 3.13+ requirement and basic Hello World functionality.

---
+++ Test/Lab.Py.TestContainer/test.py
@@ -0,0 +1,47 @@
+from testcontainers.postgres import PostgresContainer
+import psycopg2
+
+class TestDatabases:
+    def test_postgres_connection(self):
+        _image = "postgres:latest"
+        _user = "postgres"
+        _password = "postgres"
+        _dbname = "test_db"
+        # 啟動 PostgresSQL 容器
+        postgres_container = PostgresContainer(
+            image=_image,
+            user=_user,
+            password=_password,
+            dbname=_dbname
+        )
+        try:
+            # 啟動容器
+            postgres_container.start()
+
+            host = postgres_container.get_container_host_ip()
+            port = postgres_container.get_exposed_port(5432)
+            # 建立數據庫連接
+            conn = psycopg2.connect(
+                host=host,
+                port=port,
+                user=_user,
+                password=_password,
+                dbname=_dbname
+            )
+            cursor = conn.cursor()
+
+            # 測試數據庫操作
+            cursor.execute("CREATE TABLE test (id serial PRIMARY KEY, name VARCHAR);")
+            cursor.execute("INSERT INTO test (name) VALUES (%s)", ("test_name",))
+            conn.commit()
+            cursor.execute("SELECT * FROM test;")
+            result = cursor.fetchone()
+
+            assert result[1] == "test_name"
+
+            # 清理
+            cursor.close()
+            conn.close()
+
+        finally:
+            postgres_container.stop()
