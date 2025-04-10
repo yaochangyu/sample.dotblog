5aff15ef6c2dec8f909b1807f44df424ae60e33c
yao <yao-chang.yu@outlook.com>
2025-04-10 21:50:58 +0800

    refactor: rename database repository files and update imports for consistency

--- Test/Lab.Py.ApiTest/app/main.py
+++ Test/Lab.Py.ApiTest/app/main.py
@@ -1,25 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
 from app.api import api_router
-from dotenv import load_dotenv
-import os
-from app.db.member_memory_repository import member_memory_repository
-from app.db.member_repository import MemberRepositoryInterface
+from app.di.container import Container
 
-# 讀取環境變數
-# load_dotenv()
+# 创建依赖注入容器
+container = Container()
 
-# # 根據環境變數決定使用哪個 repository
-# if os.getenv("USE_POSTGRES", "false").lower() == "true":
-#     from app.db.postgres_db import postgres_member_repository
-#
-#     # 全局替換 repository 實例
-#     member_db_repository = postgres_member_repository
 app = FastAPI(
     title="Member API",
-    description="RESTful API for managing members with Memory DB",
+    description="RESTful API for managing members with DI Container",
     version="0.1.0"
 )
+
+app.state.container = container
 
 app.include_router(api_router)
 
