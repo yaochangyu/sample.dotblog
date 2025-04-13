805a852bdc15c9e175b8e5536f0a830a8db09050
yao <yao-chang.yu@outlook.com>
2025-04-13 10:39:00 +0800

    feat: refactor database configuration to use a dedicated config module and improve repository selection logic

--- Test/Lab.Py.ApiTest/app/di/container.py
+++ Test/Lab.Py.ApiTest/app/di/container.py
@@ -1,17 +1,19 @@
 from dependency_injector import containers, providers
 from app.db.member_memory_repository import MemoryMemberRepository
 from app.db.member_postgres_repository import PostgresMemberRepository
-import os
 from dotenv import load_dotenv
+from app.config import USE_POSTGRES
 
 load_dotenv()
 
 
 class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
+    # 環境變數配置
 
+    env = USE_POSTGRES
     member_repository = providers.Singleton(
-        MemoryMemberRepository
-        if os.getenv("USE_POSTGRES", "false").lower() != "true"
-        else PostgresMemberRepository
+        PostgresMemberRepository
+        if env
+        else MemoryMemberRepository
     )
