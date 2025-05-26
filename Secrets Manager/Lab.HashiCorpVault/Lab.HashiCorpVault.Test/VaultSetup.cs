d8734c50ed4fa1d6cf663290d55b57252abfdc33
余小章 <yao-chang.yu@outlook.com>
2025-05-26 16:00:12 +0800

    ✨ feat(VaultSetup.cs): 新增團隊政策與測試讀取秘密功能，支援 dev 與 prod 環境。

--- Secrets Manager/Lab.HashiCorpVault/Lab.HashiCorpVault.Test/VaultSetup.cs
+++ Secrets Manager/Lab.HashiCorpVault/Lab.HashiCorpVault.Test/VaultSetup.cs
@@ -15,23 +15,37 @@ public class VaultSetup
 
     private readonly Dictionary<string, string> _policies = new()
     {
+        ["team-dev"] = """
+                       path "dev/*" {
+                         capabilities = ["list"]
+                       }
+
+                       path "dev/data/db/connection/*" {
+                         capabilities = ["read","list"]
+                       }
+                       """,
+        ["team-prod"] = """
+                        path "prod/data/db/connection/*" {
+                          capabilities = ["read","list"]
+                        }
+                        """,
         ["team-1"] = """
                      #path "secret/*" {
                      #  capabilities = ["read","list"]
                      #}
 
-                     path "secret/db/*" {
-                       capabilities = ["create", "read", "update", "delete", "list"]
+                     #path "secret/db/*" {
+                     #  capabilities = ["create", "read", "update", "delete", "list"]
+                     #}
+
+                     path "secret/data/db/connection" {
+                       capabilities = ["read"]
                      }
 
-                     #path "secret/data/db/connection" {
-                     #  capabilities = ["read"]
-                     #}
 
-                     #
-                     #path "secret/data/db/connection/data" {
-                     #  capabilities = ["read"]
-                     #}
+                     path "secret/data/db/connection/data" {
+                       capabilities = ["read"]
+                     }
                      """,
         ["team-2"] = """
                      path "secret/data/db/connection" {
@@ -64,10 +78,12 @@ public class VaultSetup
 
     private readonly List<(string Name, string Policy)> _tokens = new()
     {
+        ("team-dev", "team-dev"),
+        ("team-prod", "team-prod"),
         ("team-1", "team-1"),
         ("team-2", "team-2"),
         ("team-3", "team-3"),
-        ("team-admin", "team-admin")
+        ("team-admin", "team-admin"),
     };
 
     public async Task SetupVaultAsync()
@@ -80,7 +96,9 @@ public class VaultSetup
         try
         {
             Console.WriteLine("Enabling kv v2 secret engine...");
-            await ExecuteVaultCommandAsync("secrets enable -path=secret kv-v2");
+            //await ExecuteVaultCommandAsync("secrets enable -path=secret kv-v2");
+            await ExecuteVaultCommandAsync("secrets enable -path=dev kv-v2");
+            await ExecuteVaultCommandAsync("secrets enable -path=prod kv-v2");
         }
         catch (Exception e)
         {
@@ -101,13 +119,16 @@ public class VaultSetup
         Console.WriteLine("Writing secrets to kv v2...");
         await ExecuteVaultCommandAsync("kv put secret/db/connection key-1=123456 key-2=77889 key-3=0806449");
 
+        await ExecuteVaultCommandAsync("kv put dev/db/connection/identity Account=user Password=1111");
+        await ExecuteVaultCommandAsync("kv put prod/db/connection/identity Account=user Password=111111");
+
         // 建立 Tokens
         Console.WriteLine("Creating tokens...");
         foreach (var (tokenName, policyName) in _tokens)
         {
             // child token 的 ttl 跟著 root token 設定(Max Lease TTL)
             var result = await ExecuteVaultCommandAsync($"token create -policy={policyName} -format=json -ttl=360d");
-            /* 
+            /*
             列舉目前的 token：vault list auth/token/accessors
             查看特定 token：vault token lookup -accessor <accessor-id>
             */
@@ -142,6 +163,38 @@ public class VaultSetup
             Environment.SetEnvironmentVariable("VAULT_TOKEN", teamToken);
             var readResult = await ExecuteVaultCommandAsync("kv get -mount=secret db/connection");
             Console.WriteLine("Read result with team-1 token:");
+            Console.WriteLine(readResult);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        // 使用 team-dev token 測試讀取秘密
+        try
+        {
+            Console.WriteLine("Testing read access with team-dev token...");
+            var teamToken = await File.ReadAllTextAsync("team-dev-token.txt");
+            Environment.SetEnvironmentVariable("VAULT_TOKEN", teamToken);
+            var readResult = await ExecuteVaultCommandAsync("kv get -format=json dev/db/connection/identity");
+            Console.WriteLine("Read result with team-dev token:");
+            Console.WriteLine(readResult);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+
+        // 使用 team-prod token 測試讀取秘密
+        try
+        {
+            Console.WriteLine("Testing read access with team-prod token...");
+            var teamToken = await File.ReadAllTextAsync("team-prod-token.txt");
+            Environment.SetEnvironmentVariable("VAULT_TOKEN", teamToken);
+            var readResult = await ExecuteVaultCommandAsync("kv get -mount=prod db/connection/identity");
+            Console.WriteLine("Read result with team-prod token:");
             Console.WriteLine(readResult);
         }
         catch (Exception e)
