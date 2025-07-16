5fffb88892268b051380215be458527ad1c6aa3e
余小章 <yao-chang.yu@outlook.com>
2025-07-16 17:20:48 +0800

    ✨ feat(controllers): 新增 XmlSerializerController 以支援 XML 序列化測試功能 ✨ feat(webapi): 新增 Web API 應用程式及其設定檔 ✨ feat(test): 增加性能測試腳本以測試 Web API ✨ feat(models): 新增 WeatherForecast 模型以提供天氣預報資訊 ✨ chore(sln): 更新解決方案以包含新的 Web API 專案 ✨ chore(json): 新增應用程式設定檔以配置日誌記錄級別

---
+++ Xml/Lab.XmlSerializerPerformance/Lab.XmlSerializer.WebApi/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.XmlSerializer.WebApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+        builder.Services.AddControllers();
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+        app.MapControllers();
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        {
+                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            TemperatureC = Random.Shared.Next(-20, 55),
+                            Summary = summaries[Random.Shared.Next(summaries.Length)]
+                        })
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast");
+
+        app.Run();
+    }
+}
