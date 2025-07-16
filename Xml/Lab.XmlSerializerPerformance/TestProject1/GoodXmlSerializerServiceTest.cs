dc67cc2927c2864c6c3ad5b1335766d37dc18720
余小章 <yao-chang.yu@outlook.com>
2025-07-16 19:37:35 +0800

    ✨ feat(TestProject1): 新增 GoodXmlSerializerService 測試案例以驗證效能改善 🔧 fix(XmlSerializerController): 修正 XmlSerializer 實作以提升序列化效能 📦 chore(TestProject1): 更新專案參考，加入 Lab.XmlSerializer.WebApi 參考

---
+++ Xml/Lab.XmlSerializerPerformance/TestProject1/GoodXmlSerializerServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using Lab.XmlSerializer.WebApi.Controllers;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class GoodXmlSerializerServiceTest
+    {
+        int Count = 1000000;
+
+        [TestMethod]
+        public async Task BadTest()
+        {
+            var service = new BadXmlSerializerService();
+            var person = new TestPerson
+            {
+                Name = null,
+                Age = 0,
+                Email = null,
+                CreatedDate = default
+            };
+            for (int i = 0; i < Count; i++)
+            {
+                service.Serialize(person);
+            }
+        }
+
+        [TestMethod]
+        public async Task GoodTest()
+        {
+            var service = new GoodXmlSerializerService();
+            var person = new TestPerson
+            {
+                Name = null,
+                Age = 0,
+                Email = null,
+                CreatedDate = default
+            };
+            for (int i = 0; i < Count; i++)
+            {
+                service.Serialize(person);
+            }
+        }
+    }
+}
