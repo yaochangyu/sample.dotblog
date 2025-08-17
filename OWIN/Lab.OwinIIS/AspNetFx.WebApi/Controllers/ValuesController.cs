2be0dcdfed8ac7626abe5034d2333c778f275ba5
yao <yao-chang.yu@outlook.com>
2025-08-17 17:44:29 +0800

    Add initial ASP.NET Web API project structure and configuration files.

---
+++ OWIN/Lab.OwinIIS/AspNetFx.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AspNetFx.WebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            var serverVariables = new HttpContextProvider().GetServerVariables();
+            var results = serverVariables.Select(p => p.Key + "=" + p.Value)
+                .ToList();
+                
+            return results;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
