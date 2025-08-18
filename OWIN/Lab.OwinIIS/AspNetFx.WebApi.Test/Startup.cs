16a3a61f49a0b8eb0e2d450fb17297c7f53744da
yao <yao-chang.yu@outlook.com>
2025-08-18 21:56:52 +0800

    Add AspNetFx.WebApi.Test project with initial configuration and tests.

---
+++ OWIN/Lab.OwinIIS/AspNetFx.WebApi.Test/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace AspNetFx.WebApi.Test
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var configuration = new HttpConfiguration();
+            WebApiConfig.Register(configuration);
+            //app.UseErrorPage();
+            //app.UseWelcomePage("/Welcome");
+            app.UseWebApi(configuration);
+        }
+    }
+}
