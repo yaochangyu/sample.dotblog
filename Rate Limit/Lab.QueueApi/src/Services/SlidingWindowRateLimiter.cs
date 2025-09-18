1ba1f2d505f6db8192dea0f6c30e29c8dae75753
余小章 <yao-chang.yu@outlook.com>
2025-09-18 14:30:21 +0800

    ✨ feat(QueuedRequest): 將 ApiRequest 類別重命名為 CreateQueueRequest ✨ feat(Program): 調整 SlidingWindowRateLimiter 的時間窗口至 10 秒 ✨ feat(QueueController): 重命名 QueuedApiController 為 QueueController ✨ feat(QueueController): 更新 ProcessRequest 方法以使用 CreateQueueRequest ✨ feat(RequestProcessorService): 將 BackgroundRequestProcessor 重新命名為 RequestProcessorService ✨ feat(SlidingWindowRateLimiter): 將預設時間窗口調整為 30 秒

--- Rate Limit/Lab.QueueApi/src/Services/SlidingWindowRateLimiter.cs
+++ Rate Limit/Lab.QueueApi/src/Services/SlidingWindowRateLimiter.cs
@@ -12,7 +12,7 @@ public class SlidingWindowRateLimiter : IRateLimiter
     public SlidingWindowRateLimiter(int maxRequests = 2, TimeSpan? timeWindow = null)
     {
         _maxRequests = maxRequests;
-        _timeWindow = timeWindow ?? TimeSpan.FromMinutes(1);
+        _timeWindow = timeWindow ?? TimeSpan.FromSeconds(30);
     }
 
     public bool IsRequestAllowed()
