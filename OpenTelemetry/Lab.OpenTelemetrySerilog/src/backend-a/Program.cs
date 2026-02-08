19ff8327eaa25ea37e86a66e7658f1b721a8e631
yao <yao-chang.yu@outlook.com>
2026-02-08 18:42:54 +0800

    ✨ feat(opentelemetry): 更新 backend-a 和 backend-b 的 Serilog 整合

--- OpenTelemetry/Lab.OpenTelemetrySerilog/src/backend-a/Program.cs
+++ OpenTelemetry/Lab.OpenTelemetrySerilog/src/backend-a/Program.cs
@@ -1,34 +1,56 @@
-using System.Net.Http;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Information()
+        .Enrich.WithProperty("application", "backend-a")
+        .Enrich.FromLogContext()
+        .WriteTo.Seq("http://localhost:5341")
+        .WriteTo.Console()
+        .WriteTo.File("logs/host-.txt", rollingInterval: RollingInterval.Day)
+        .CreateBootstrapLogger()
+    ;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddHttpClient();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog();
+
+    // Add services to the container.
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+    builder.Services.AddHttpClient();
+
+    var app = builder.Build();
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.MapGet("/weatherforecast", async (HttpClient client) =>
+        {
+            try
+            {
+                var response = await client.GetStringAsync("http://localhost:5200/weatherforecast");
+                return Results.Content(response, "application/json");
+            }
+            catch (HttpRequestException ex)
+            {
+                return Results.Problem($"Error calling Backend-B: {ex.Message}");
+            }
+        })
+        .WithName("GetWeatherForecast");
+
+    app.Run();
 }
-
-
-
-app.MapGet("/weatherforecast", async (HttpClient client) =>
+catch (Exception ex)
 {
-    try
-    {
-        var response = await client.GetStringAsync("http://localhost:5200/weatherforecast");
-        return Results.Content(response, "application/json");
-    }
-    catch (HttpRequestException ex)
-    {
-        return Results.Problem($"Error calling Backend-B: {ex.Message}");
-    }
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
